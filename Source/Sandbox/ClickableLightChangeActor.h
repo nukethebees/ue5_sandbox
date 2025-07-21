65f2cd0604ddf285cf3f08d6ebe69d4530794061
Matthew Love <matthewlove94@gmail.com>
2025-07-21 20:04:19 +0100

    Add rotating box

---
+++ Source/Sandbox/ClickableLightChangeActor.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <array>
+
+#include "Clickable.h"
+#include "Components/PointLightComponent.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "ClickableLightChangeActor.generated.h"
+
+UCLASS()
+class SANDBOX_API AClickableLightChangeActor
+    : public AActor
+    , public IClickable {
+    GENERATED_BODY()
+  public:
+    AClickableLightChangeActor();
+  protected:
+    virtual void BeginPlay() override;
+  public:
+    virtual void Tick(float DeltaTime) override;
+    virtual void OnClicked() override;
+  private:
+    UPointLightComponent* point_light{nullptr};
+
+    static inline std::array<FLinearColor, 4> colours{
+        FLinearColor::Red, FLinearColor::Green, FLinearColor::Blue, FLinearColor::Yellow};
+    int32 current_colour_index{0};
+};
