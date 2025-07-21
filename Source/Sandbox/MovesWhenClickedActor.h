4cdd5caea1adb1438ce0b440d1142b371b60c139
Matthew Love <matthewlove94@gmail.com>
2025-07-21 18:50:53 +0100

    Implement moving actor

---
+++ Source/Sandbox/MovesWhenClickedActor.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Clickable.h"
+
+#include "MovesWhenClickedActor.generated.h"
+
+UCLASS()
+class SANDBOX_API AMovesWhenClickedActor
+    : public AActor
+    , public IClickable {
+    GENERATED_BODY()
+  public:
+    AMovesWhenClickedActor();
+  protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+  public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+    virtual void OnClicked() override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement");
+    float move_speed{300.0f};
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement");
+    float acceleration{100.0f};
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement");
+    float move_duration{2.0f};
+  private:
+    bool is_moving{false};
+    float move_timer{0};
+};
