21d6b095f55799519c34ffa0b8e0f5f9c0ebb423
Matthew Love <matthewlove94@gmail.com>
2025-08-13 19:58:44 +0100

    Add moving lift item

---
+++ Source/Sandbox/actors/LoopingLift.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Sandbox/mixins/print_msg_mixin.hpp"
+
+#include "LoopingLift.generated.h"
+
+UCLASS()
+class SANDBOX_API ALoopingLift
+    : public AActor
+    , public print_msg_mixin {
+    GENERATED_BODY()
+  public:
+    ALoopingLift();
+  protected:
+    virtual void BeginPlay() override;
+  public:
+    virtual void Tick(float DeltaTime) override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float speed{200.0f};
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float distance{200.0f};
+  private:
+    FVector origin;
+    bool going_up;
+};
