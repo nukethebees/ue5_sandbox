1a46877ac1847b969455dd90b723729177d3fe22
Matthew Love <matthewlove94@gmail.com>
2025-08-13 19:58:44 +0100

    Add moving lift item

--- Source/Sandbox/actors/MovesWhenClickedActor.cpp
+++ Source/Sandbox/actors/MovesWhenClickedActor.cpp
@@ -1,11 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "MovesWhenClickedActor.h"
 
-// Sets default values
 AMovesWhenClickedActor::AMovesWhenClickedActor() {
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if
-    // you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 }
 
