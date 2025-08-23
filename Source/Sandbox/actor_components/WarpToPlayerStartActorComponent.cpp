0e9805e0c39b8a0b27d3fed152989ce054aef547
Matthew Love <matthewlove94@gmail.com>
2025-08-23 13:31:15 +0100

    Make an actor component to reset actors back to player start

---
+++ Source/Sandbox/actor_components/WarpToPlayerStartActorComponent.cpp
@@ -0,0 +1,36 @@
+#include "Sandbox/actor_components/WarpToPlayerStartActorComponent.h"
+
+#include "Components/BoxComponent.h"
+#include "EngineUtils.h"
+#include "GameFramework/PlayerStart.h"
+
+UWarpToPlayerStartActorComponent::UWarpToPlayerStartActorComponent() {
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UWarpToPlayerStartActorComponent::BeginPlay() {
+    Super::BeginPlay();
+
+    if (warp_zone) {
+        warp_zone->OnComponentBeginOverlap.AddDynamic(
+            this, &UWarpToPlayerStartActorComponent::OnOverlapBegin);
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("warp_zone is null in UWarpToPlayerStartActorComponent"));
+    }
+}
+
+void UWarpToPlayerStartActorComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComp,
+                                                      AActor* OtherActor,
+                                                      UPrimitiveComponent* OtherComp,
+                                                      int32 OtherBodyIndex,
+                                                      bool bFromSweep,
+                                                      FHitResult const& SweepResult) {
+    UE_LOG(LogTemp, Warning, TEXT("Overlap begin"));
+    for (auto it{TActorIterator<APlayerStart>(GetWorld())}; it; ++it) {
+        if (OtherActor) {
+            OtherActor->SetActorLocation(it->GetActorLocation());
+        }
+        break;
+    }
+    UE_LOG(LogTemp, Warning, TEXT("UWarpToPlayerStartActorComponent: No player start found."));
+}
