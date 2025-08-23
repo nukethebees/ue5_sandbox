800fc6675b283189beaa3f2729ae45fc48dc9359
Matthew Love <matthewlove94@gmail.com>
2025-08-23 18:11:12 +0100

    Add launchpad component

---
+++ Source/Sandbox/actor_components/LaunchpadComponent.cpp
@@ -0,0 +1,35 @@
+#include "Sandbox/actor_components/LaunchpadComponent.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/Actor.h"
+
+ULaunchpadComponent::ULaunchpadComponent() {
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void ULaunchpadComponent::BeginPlay() {
+    Super::BeginPlay();
+
+    if (auto const owner{GetOwner()}) {
+        collision_box = owner->FindComponentByClass<UBoxComponent>();
+
+        if (collision_box != nullptr) {
+            collision_box->OnComponentBeginOverlap.AddDynamic(
+                this, &ULaunchpadComponent::on_overlap_begin);
+        }
+    }
+}
+
+void ULaunchpadComponent::on_overlap_begin(UPrimitiveComponent* overlapped_comp,
+                                           AActor* other_actor,
+                                           UPrimitiveComponent* other_comp,
+                                           int32 other_body_index,
+                                           bool b_from_sweep,
+                                           FHitResult const& sweep_result) {
+
+    if (auto const character{Cast<ACharacter>(other_actor)}) {
+        FVector const launch_velocity{0.0f, 0.0f, launch_strength};
+        character->LaunchCharacter(launch_velocity, true, true);
+    }
+}
---
+++ Source/Sandbox/actor_components/LaunchpadComponent.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LaunchpadComponent.generated.h"
+
+class UBoxComponent;
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class SANDBOX_API ULaunchpadComponent : public UActorComponent {
+    GENERATED_BODY()
+  public:
+    ULaunchpadComponent();
+  protected:
+    virtual void BeginPlay() override;
+  private:
+    UFUNCTION()
+    void on_overlap_begin(UPrimitiveComponent* overlapped_comp,
+                          AActor* other_actor,
+                          UPrimitiveComponent* other_comp,
+                          int32 other_body_index,
+                          bool b_from_sweep,
+                          FHitResult const& sweep_result);
+  public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Launch")
+    UBoxComponent* collision_box{nullptr};
+
+    UPROPERTY(EditAnywhere, Category = "Launch")
+    float launch_strength{1000.0f};
+};
