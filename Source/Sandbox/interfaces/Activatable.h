cda808f38557e883cf2f78d6f1e22b7b82fb3b90
Matthew Love <matthewlove94@gmail.com>
2025-08-24 16:06:10 +0100

    Add can_activate check and try method

--- Source/Sandbox/interfaces/Activatable.h
+++ Source/Sandbox/interfaces/Activatable.h
@@ -14,4 +14,17 @@ class SANDBOX_API IActivatable {
     GENERATED_BODY()
   public:
     virtual void activate(AActor* instigator = nullptr) = 0;
+    virtual bool can_activate(AActor const* instigator) const { return true; }
+
+    template <typename T, typename U>
+    static bool try_activate(T* target, U* instigator) {
+        if (target->GetClass()->ImplementsInterface(UActivatable::StaticClass())) {
+            auto const activatable{TScriptInterface<IActivatable>(target)};
+            if (activatable->can_activate(instigator)) {
+                activatable->activate(instigator);
+                return true;
+            }
+        }
+        return false;
+    }
 };
