099b1774ff5803a3506504abaa0c6fed0e53001e
Matthew Love <matthewlove94@gmail.com>
2025-08-24 18:28:27 +0100

    Make a bridge component to allow interactables to trigger activations

--- Source/Sandbox/actor_components/ActivatorComponent.cpp
+++ Source/Sandbox/actor_components/ActivatorComponent.cpp
@@ -1,9 +1,32 @@
 #include "Sandbox/actor_components/ActivatorComponent.h"
 
+#include "GameFramework/Actor.h"
+
 UActivatorComponent::UActivatorComponent() {
     PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UActivatorComponent::BeginPlay() {
     Super::BeginPlay();
+
+    for (auto* const actor : linked_actors) {
+        if (!actor) {
+            continue;
+        }
+
+        for (auto* const component : actor->GetComponents()) {
+            if (component &&
+                component->GetClass()->ImplementsInterface(UActivatable::StaticClass())) {
+                linked_activatables.Add(TScriptInterface<IActivatable>(component));
+            }
+        }
+    }
+}
+
+void UActivatorComponent::trigger_activation(AActor* instigator) {
+    for (auto& target : linked_activatables) {
+        if (target) {
+            target->trigger_activation(instigator);
+        }
+    }
 }
--- Source/Sandbox/actor_components/ActivatorComponent.h
+++ Source/Sandbox/actor_components/ActivatorComponent.h
@@ -2,7 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Sandbox/interfaces/Activatable.h"
+
 #include "ActivatorComponent.generated.h"
+
+class AActor;
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SANDBOX_API UActivatorComponent : public UActorComponent {
@@ -11,4 +15,12 @@ class SANDBOX_API UActivatorComponent : public UActorComponent {
     UActivatorComponent();
   protected:
     virtual void BeginPlay() override;
+  public:
+    UFUNCTION(BlueprintCallable)
+    void trigger_activation(AActor* instigator);
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Activation")
+    TArray<AActor*> linked_actors;
+  private:
+    TArray<TScriptInterface<IActivatable>> linked_activatables;
 };
