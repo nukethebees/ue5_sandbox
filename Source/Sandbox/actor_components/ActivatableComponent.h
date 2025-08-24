99b0d360006c46997e07c0d00e31d4162a262413
Matthew Love <matthewlove94@gmail.com>
2025-08-24 17:59:50 +0100

    Implement activatable component

--- Source/Sandbox/actor_components/ActivatableComponent.h
+++ Source/Sandbox/actor_components/ActivatableComponent.h
@@ -2,7 +2,15 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Sandbox/interfaces/Activatable.h"
+
 #include "ActivatableComponent.generated.h"
+
+UENUM()
+enum class EActivateReady : uint8 { none_ready, some_ready, all_ready };
+
+UENUM()
+enum class EActivateTriggered : uint8 { none_triggered, some_triggered, all_triggered };
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SANDBOX_API UActivatableComponent : public UActorComponent {
@@ -11,4 +19,13 @@ class SANDBOX_API UActivatableComponent : public UActorComponent {
     UActivatableComponent();
   protected:
     virtual void BeginPlay() override;
+  public:
+    UFUNCTION(BlueprintCallable)
+    void trigger_activation(AActor* instigator);
+    UFUNCTION(BlueprintCallable)
+    EActivateReady can_activate(AActor const* instigator) const;
+    UFUNCTION(BlueprintCallable)
+    EActivateTriggered try_activate(AActor* instigator);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Activation")
+    TArray<TScriptInterface<IActivatable>> linked_activatables;
 };
