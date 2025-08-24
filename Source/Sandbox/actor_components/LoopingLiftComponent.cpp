a291e4d49418d1e7b757942d602dff5bce4b82f0
Matthew Love <matthewlove94@gmail.com>
2025-08-24 12:24:03 +0100

    Add an optional delay to the looping lift component

--- Source/Sandbox/actor_components/LoopingLiftComponent.cpp
+++ Source/Sandbox/actor_components/LoopingLiftComponent.cpp
@@ -7,13 +7,17 @@ ULoopingLiftComponent::ULoopingLiftComponent() {
 void ULoopingLiftComponent::BeginPlay() {
     Super::BeginPlay();
     origin = GetOwner()->GetActorLocation();
-    going_up = true;
 }
 
 void ULoopingLiftComponent::TickComponent(float DeltaTime,
                                           ELevelTick TickType,
                                           FActorComponentTickFunction* ThisTickFunction) {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (pause_timer <= pause_after_completion) {
+        pause_timer += DeltaTime;
+        return;
+    }
 
     auto const up{GetOwner()->GetActorLocation()};
     auto const dist_from_origin{up.Z - origin.Z};
@@ -23,12 +27,14 @@ void ULoopingLiftComponent::TickComponent(float DeltaTime,
             GetOwner()->AddActorWorldOffset(FVector::UpVector * speed * DeltaTime);
         } else {
             going_up = false;
+            pause_timer = 0.0f;
         }
     } else {
         if (dist_from_origin > 0) {
             GetOwner()->AddActorWorldOffset(FVector::DownVector * speed * DeltaTime);
         } else {
             going_up = true;
+            pause_timer = 0.0f;
         }
     }
 }
