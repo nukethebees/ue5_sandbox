cc5d9ba4ecdaca3f4801d01d005d0a3daa3509d6
Matthew Love <matthewlove94@gmail.com>
2025-08-25 13:17:58 +0100

    Add health widget

--- Source/Sandbox/widgets/MainHUDWidget.cpp
+++ Source/Sandbox/widgets/MainHUDWidget.cpp
@@ -1,26 +1,29 @@
 #include "Sandbox/widgets/MainHUDWidget.h"
 
+#define WIDGET_CHECK(widget, name_string)            \
+    do {                                             \
+        if (!widget) {                               \
+            missing_widget_error(TEXT(name_string)); \
+            return;                                  \
+        }                                            \
+    } while (0)
+
 void UMainHUDWidget::update_fuel(float new_fuel) {
-    if (!fuel_widget) {
-        missing_widget_error(TEXT("update_fuel"));
-        return;
-    }
+    WIDGET_CHECK(fuel_widget, "update_fuel");
     fuel_widget->update_fuel(new_fuel);
 }
 
 void UMainHUDWidget::update_jump(int32 new_jump) {
-    if (!jump_widget) {
-        missing_widget_error(TEXT("update_jump"));
-        return;
-    }
+    WIDGET_CHECK(jump_widget, "update_jump");
     jump_widget->update_jump(new_jump);
 }
 void UMainHUDWidget::update_coin(int32 new_coin_count) {
-    if (!coin_widget) {
-        missing_widget_error(TEXT("update_coin"));
-        return;
-    }
+    WIDGET_CHECK(coin_widget, "update_coin");
     coin_widget->update_coin(new_coin_count);
+}
+void UMainHUDWidget::update_health_percent(float health_pc) {
+    WIDGET_CHECK(health_widget, "update_health_percent");
+    health_widget->set_health_percent(health_pc);
 }
 void UMainHUDWidget::missing_widget_error(FStringView method) {
     UE_LOGFMT(LogTemp, Error, "UMainHUDWidget: {Method} no widget.", ("Method", method));
--- Source/Sandbox/widgets/MainHUDWidget.h
+++ Source/Sandbox/widgets/MainHUDWidget.h
@@ -3,9 +3,10 @@
 #include "Blueprint/UserWidget.h"
 #include "Components/TextBlock.h"
 #include "CoreMinimal.h"
-#include "Sandbox/widgets/FuelWidget.h"
-#include "Sandbox/widgets/JumpWidget.h"
 #include "Sandbox/widgets/CoinWidget.h"
+#include "Sandbox/widgets/FuelWidget.h"
+#include "Sandbox/widgets/HealthWidget.h"
+#include "Sandbox/widgets/JumpWidget.h"
 
 #include "MainHUDWidget.generated.h"
 
@@ -23,6 +24,9 @@ class SANDBOX_API UMainHUDWidget : public UUserWidget {
 
     UPROPERTY(meta = (BindWidget))
     UCoinWidget* coin_widget;
+
+    UPROPERTY(meta = (BindWidget))
+    UHealthWidget* health_widget;
   public:
     UFUNCTION(BlueprintCallable, Category = "UI")
     void update_fuel(float new_fuel);
@@ -30,6 +34,8 @@ class SANDBOX_API UMainHUDWidget : public UUserWidget {
     void update_jump(int32 new_jump);
     UFUNCTION(BlueprintCallable, Category = "UI")
     void update_coin(int32 new_coin_count);
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    void update_health_percent(float health);
   private:
     void missing_widget_error(FStringView method);
 };
