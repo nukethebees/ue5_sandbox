1cbd3c78330ce8d5e6a808605ad7ad6cd4d66285
Matthew Love <matthewlove94@gmail.com>
2025-08-28 13:29:01 +0100

    Add empty subsystem for damage calculations

---
+++ Source/Sandbox/subsystems/DamageManagerSubsystem.cpp
@@ -0,0 +1,8 @@
+#include "Sandbox/subsystems/DamageManagerSubsystem.h"
+
+void UDamageManagerSubsystem::QueueDamage(UHealthComponent* receiver, FHealthChange const& change) {
+
+}
+void UDamageManagerSubsystem::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+}
