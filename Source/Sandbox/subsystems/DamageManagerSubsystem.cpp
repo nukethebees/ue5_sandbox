4b1fcae70f643da5e60391205666f29a587135ad
Matthew Love <matthewlove94@gmail.com>
2025-08-29 10:00:28 +0100

    Implement damage manager subsystem

--- Source/Sandbox/subsystems/DamageManagerSubsystem.cpp
+++ Source/Sandbox/subsystems/DamageManagerSubsystem.cpp
@@ -1,8 +1,30 @@
 #include "Sandbox/subsystems/DamageManagerSubsystem.h"
 
 void UDamageManagerSubsystem::QueueDamage(UHealthComponent* receiver, FHealthChange const& change) {
+    if (!receiver) {
+        return;
+    }
 
+    pending_changes.Add({receiver, change});
+    tick_enabled = true;
 }
 void UDamageManagerSubsystem::Tick(float DeltaTime) {
     Super::Tick(DeltaTime);
+
+    if (pending_changes.IsEmpty()) {
+        tick_enabled = false;
+        return;
+    }
+
+    for (auto const& change : pending_changes) {
+        if (change.Receiver.IsValid()) {
+            change.Receiver->modify_health(change.Change);
+        }
+    }
+
+    pending_changes.Empty();
+    tick_enabled = false;
+}
+TStatId UDamageManagerSubsystem::GetStatId() const {
+    RETURN_QUICK_DECLARE_CYCLE_STAT(UDamageManagerSubsystem, STATGROUP_Tickables);
 }
