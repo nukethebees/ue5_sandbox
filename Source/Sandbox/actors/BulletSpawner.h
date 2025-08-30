09f1e0370c31accb9a362b0ad8ebd54710dd1984
Matthew Love <matthewlove94@gmail.com>
2025-08-30 19:24:39 +0100

    Add bullet spawner actor

---
+++ Source/Sandbox/actors/BulletSpawner.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BulletSpawner.generated.h"
+
+UCLASS()
+class SANDBOX_API ABulletSpawner : public AActor {
+    GENERATED_BODY()
+  public:
+    ABulletSpawner();
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullets")
+    float bullets_per_second{1.0f};
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullets")
+    TSubclassOf<AActor> bullet_class;
+  protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullets")
+    USceneComponent* fire_point{nullptr};
+    virtual void BeginPlay() override;
+  public:
+    virtual void Tick(float DeltaTime) override;
+  private:
+    float time_since_last_shot{0.0f};
+    void spawn_bullet();
+};
