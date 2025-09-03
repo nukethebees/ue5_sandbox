b58757399451b5286f2474c323e2ee9516636890
Matthew Love <matthewlove94@gmail.com>
2025-09-03 12:50:10 +0100

    Added a quit game button

--- Source/Sandbox/widgets/MainMenu2Widget.cpp
+++ Source/Sandbox/widgets/MainMenu2Widget.cpp
@@ -1,2 +1,14 @@
 #include "Sandbox/widgets/MainMenu2Widget.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
+void UMainMenu2Widget::NativeConstruct() {
+    Super::NativeConstruct();
+
+    if (quit_button) {
+        quit_button->on_clicked.AddDynamic(this, &UMainMenu2Widget::handle_quit);
+    }
+}
+void UMainMenu2Widget::handle_quit() {
+    UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, false);
+}
--- Source/Sandbox/widgets/MainMenu2Widget.h
+++ Source/Sandbox/widgets/MainMenu2Widget.h
@@ -2,11 +2,23 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "CoreMinimal.h"
+#include "Sandbox/widgets/TextButtonWidget.h"
+
 #include "MainMenu2Widget.generated.h"
 
 UCLASS()
 class SANDBOX_API UMainMenu2Widget : public UUserWidget {
     GENERATED_BODY()
+  protected:
+    virtual void NativeConstruct() override;
+
+    UPROPERTY(meta = (BindWidget))
+    UTextButtonWidget* play_button;
+    UPROPERTY(meta = (BindWidget))
+    UTextButtonWidget* quit_button;
+private:
+    UFUNCTION()
+    void handle_quit();
 };
