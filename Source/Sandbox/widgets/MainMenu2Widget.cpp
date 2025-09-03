e012163f8992fbaabdf318ade6dc686fcb03be00
Matthew Love <matthewlove94@gmail.com>
2025-09-03 14:38:29 +0100

    Set up the play button to switch widgets

--- Source/Sandbox/widgets/MainMenu2Widget.cpp
+++ Source/Sandbox/widgets/MainMenu2Widget.cpp
@@ -5,10 +5,22 @@
 void UMainMenu2Widget::NativeConstruct() {
     Super::NativeConstruct();
 
+    if (play_button) {
+        play_button->on_clicked.AddDynamic(this, &UMainMenu2Widget::handle_play);
+    }
+
     if (quit_button) {
         quit_button->on_clicked.AddDynamic(this, &UMainMenu2Widget::handle_quit);
     }
 }
 void UMainMenu2Widget::handle_quit() {
     UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, false);
+}
+void UMainMenu2Widget::handle_play() {
+    set_active_page(EMainMenu2MenuPage::LevelSelect);
+}
+void UMainMenu2Widget::set_active_page(EMainMenu2MenuPage page) {
+    if (widget_switcher) {
+        widget_switcher->SetActiveWidgetIndex(static_cast<int32>(EMainMenu2MenuPage::LevelSelect));
+    }
 }
