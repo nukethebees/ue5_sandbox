47956b7b6e079434ddd659806a0537a86e51457c
Matthew Love <matthewlove94@gmail.com>
2025-09-03 11:40:26 +0100

    Add SynchronizeProperties support to have labels update in the editor

--- Source/Sandbox/widgets/TextButtonWidget.cpp
+++ Source/Sandbox/widgets/TextButtonWidget.cpp
@@ -6,15 +6,21 @@
 void UTextButtonWidget::NativeConstruct() {
     Super::NativeConstruct();
 
-    if (button) {
-        button->OnClicked.AddDynamic(this, &UTextButtonWidget::handle_click);
+    if (button_widget) {
+        button_widget->OnClicked.AddDynamic(this, &UTextButtonWidget::handle_click);
     }
 
+    set_label();
+}
+void UTextButtonWidget::SynchronizeProperties() {
+    Super::SynchronizeProperties();
+    set_label();
+}
+void UTextButtonWidget::handle_click() {
+    on_clicked.Broadcast();
+}
+void UTextButtonWidget::set_label() {
     if (text_block) {
         text_block->SetText(label);
     }
-}
-
-void UTextButtonWidget::handle_click() {
-    on_clicked.Broadcast();
 }
