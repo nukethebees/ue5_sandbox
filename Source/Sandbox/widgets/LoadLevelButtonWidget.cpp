1da92a18f81bccb15d5190971a689266d9d7db1b
Matthew Love <matthewlove94@gmail.com>
2025-09-03 18:06:07 +0100

    Implement level loading functionality

--- Source/Sandbox/widgets/LoadLevelButtonWidget.cpp
+++ Source/Sandbox/widgets/LoadLevelButtonWidget.cpp
@@ -1,12 +1,40 @@
 #include "Sandbox/widgets/LoadLevelButtonWidget.h"
 
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
 #include "Kismet/GameplayStatics.h"
 
 void ULoadLevelButtonWidget::NativeConstruct() {
     Super::NativeConstruct();
 
     load_level_button->OnClicked.AddDynamic(this, &ULoadLevelButtonWidget::load_level);
+
+    if (set_level_display_name_to_path_if_unset() == DisplayNameChanged::no) {
+        update_display_name_text_box();
+    }
+}
+void ULoadLevelButtonWidget::set_level_path(FName value) {
+    level_path_ = value;
+    set_level_display_name_to_path_if_unset();
+}
+void ULoadLevelButtonWidget::set_level_display_name(FText value) {
+    level_display_name_ = value;
+    update_display_name_text_box();
 }
 void ULoadLevelButtonWidget::load_level() {
-    UGameplayStatics::OpenLevel(GetWorld(), TEXT("MainMenu"));
+    if (!level_path_.IsNone()) {
+        UGameplayStatics::OpenLevel(GetWorld(), level_path_);
+    }
+}
+DisplayNameChanged ULoadLevelButtonWidget::set_level_display_name_to_path_if_unset() {
+    if (level_display_name_.IsEmpty()) {
+        set_level_display_name(FText::FromName(level_path_));
+        return DisplayNameChanged::yes;
+    }
+    return DisplayNameChanged::no;
+}
+void ULoadLevelButtonWidget::update_display_name_text_box() {
+    if (level_text_block) {
+        level_text_block->SetText(level_display_name_);
+    }
 }
--- Source/Sandbox/widgets/LoadLevelButtonWidget.h
+++ Source/Sandbox/widgets/LoadLevelButtonWidget.h
@@ -1,19 +1,37 @@
 #pragma once
 
 #include "Blueprint/UserWidget.h"
-#include "Components/Button.h"
 #include "CoreMinimal.h"
 
 #include "LoadLevelButtonWidget.generated.h"
+
+class UButton;
+class UTextBlock;
+
+enum class DisplayNameChanged : uint8 {
+    yes, no
+};
 
 UCLASS()
 class SANDBOX_API ULoadLevelButtonWidget : public UUserWidget {
     GENERATED_BODY()
   public:
-    UPROPERTY(meta = (BindWidget))
-    UButton* load_level_button;
+    void set_level_path(FName value);
+    void set_level_display_name(FText value);
   protected:
     virtual void NativeConstruct() override;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* load_level_button{nullptr};
+    UPROPERTY(meta = (BindWidget))
+    UTextBlock* level_text_block{nullptr};
+
+    UPROPERTY(BlueprintReadOnly)
+    FText level_display_name_{};
+    UPROPERTY()
+    FName level_path_{};
   private:
     void load_level();
+    DisplayNameChanged set_level_display_name_to_path_if_unset();
+    void update_display_name_text_box();
 };
