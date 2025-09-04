5e693dd3f5057632106c275a6425b410fe8c5500
Matthew Love <matthewlove94@gmail.com>
2025-09-04 09:39:18 +0100

    Mark functions as inline

--- Source/Sandbox/utilities/math.h
+++ Source/Sandbox/utilities/math.h
@@ -3,13 +3,13 @@
 #include "Math/UnrealMathUtility.h"
 
 namespace ml {
-auto normalised_percent(float value, float max) {
+inline auto normalised_percent(float value, float max) {
     return value / max;
 }
-auto denormalise_percent_to_int(float pc) {
+inline auto denormalise_percent_to_int(float pc) {
     return FMath::RoundToInt(pc * 100.0f);
 }
-auto clamp_normalised_percent(float pc) {
+inline auto clamp_normalised_percent(float pc) {
     return FMath::Clamp(pc, 0.0f, 1.0f);
 }
 }
