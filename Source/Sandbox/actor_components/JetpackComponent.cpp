f15208db6f109f7c8bb0dc62e5eacca36c065b6e
Matthew Love <matthewlove94@gmail.com>
2025-09-10 16:35:52 +0100

    Rename members, add a delegate and a struct for it to broadcast

--- Source/Sandbox/actor_components/JetpackComponent.cpp
+++ Source/Sandbox/actor_components/JetpackComponent.cpp
@@ -5,7 +5,7 @@
 UJetpackComponent::UJetpackComponent() {
     PrimaryComponentTick.bCanEverTick = true;
     PrimaryComponentTick.bStartWithTickEnabled = false;
-    jetpack_fuel = jetpack_fuel_max;
+    fuel = fuel_max;
 }
 void UJetpackComponent::BeginPlay() {
     Super::BeginPlay();
@@ -20,7 +20,7 @@ void UJetpackComponent::TickComponent(float delta_time,
     }
 }
 void UJetpackComponent::start_jetpack() {
-    if (jetpack_fuel <= 0.0f) {
+    if (fuel <= 0.0f) {
         return;
     }
 
@@ -34,7 +34,7 @@ void UJetpackComponent::stop_jetpack() {
 
     SetComponentTickEnabled(false);
 
-    if (jetpack_fuel < jetpack_fuel_max) {
+    if (fuel < fuel_max) {
         constexpr float jetpack_timer_interval{0.2f};
         constexpr bool jetpack_timer_loops{true};
         GetWorld()->GetTimerManager().SetTimer(fuel_recharge_timer,
@@ -45,24 +45,24 @@ void UJetpackComponent::stop_jetpack() {
     }
 }
 void UJetpackComponent::apply_jetpack_force(float delta_time) {
-    if (jetpack_fuel <= 0.0f) {
+    if (fuel <= 0.0f) {
         stop_jetpack();
         return;
     }
 
     if (auto* character{Cast<ACharacter>(GetOwner())}) {
-        FVector const force{0.0f, 0.0f, jetpack_force * delta_time};
+        FVector const force{0.0f, 0.0f, lift_force * delta_time};
         character->LaunchCharacter(force, false, true);
     }
 
-    jetpack_fuel -= jetpack_fuel_consumption_rate * delta_time;
-    jetpack_fuel = FMath::Clamp(jetpack_fuel, 0.0f, jetpack_fuel_max);
+    fuel -= fuel_consumption_rate * delta_time;
+    fuel = FMath::Clamp(fuel, 0.0f, fuel_max);
 }
 void UJetpackComponent::recharge_fuel() {
-    jetpack_fuel += jetpack_fuel_recharge_rate;
-    jetpack_fuel = FMath::Clamp(jetpack_fuel, 0.0f, jetpack_fuel_max);
+    fuel += fuel_recharge_rate;
+    fuel = FMath::Clamp(fuel, 0.0f, fuel_max);
 
-    if (jetpack_fuel >= jetpack_fuel_max) {
+    if (fuel >= fuel_max) {
         GetWorld()->GetTimerManager().ClearTimer(fuel_recharge_timer);
     }
 }
--- Source/Sandbox/actor_components/JetpackComponent.h
+++ Source/Sandbox/actor_components/JetpackComponent.h
@@ -7,6 +7,28 @@
 
 #include "JetpackComponent.generated.h"
 
+USTRUCT(BlueprintType)
+struct FJetpackState {
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float fuel_remaining{0.0f};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float fuel_capacity{0.0f};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    bool is_jetpacking{false};
+
+    FJetpackState() = default;
+    FJetpackState(float fuel_remaining, float fuel_capacity, bool is_jetpacking)
+        : fuel_remaining{fuel_remaining}
+        , fuel_capacity{fuel_capacity}
+        , is_jetpacking{is_jetpacking} {}
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnFuelChanged, FJetpackState const&, NewFuel);
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SANDBOX_API UJetpackComponent : public UActorComponent {
     GENERATED_BODY()
@@ -15,16 +37,18 @@ class SANDBOX_API UJetpackComponent : public UActorComponent {
 
     void start_jetpack();
     void stop_jetpack();
-    float get_fuel() const { return jetpack_fuel; }
+    float get_fuel() const { return fuel; }
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement");
-    float jetpack_fuel_max{2.0f};
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement");
-    float jetpack_fuel_recharge_rate{1.0f};
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement");
-    float jetpack_fuel_consumption_rate{1.0f};
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement");
-    float jetpack_force{5000.0f};
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jetpack");
+    float fuel_max{2.0f};
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jetpack");
+    float fuel_recharge_rate{0.1f};
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jetpack");
+    float fuel_consumption_rate{1.0f};
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jetpack");
+    float lift_force{15000.0f};
+    UPROPERTY(BlueprintAssignable, Category = "Jetpack")
+    FOnFuelChanged on_fuel_changed;
   protected:
     virtual void BeginPlay() override;
     virtual void TickComponent(float delta_time,
@@ -36,6 +60,6 @@ class SANDBOX_API UJetpackComponent : public UActorComponent {
 
     FTimerHandle fuel_recharge_timer;
 
-    float jetpack_fuel{0.0f};
+    float fuel{0.0f};
     bool is_jetpacking{false};
 };
