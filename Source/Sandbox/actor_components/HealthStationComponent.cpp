b5bc4238e63ecf7eb91c2730ef22efb1d6cdbef1
Matthew Love <matthewlove94@gmail.com>
2025-09-10 12:07:17 +0100

    Add health station impl

--- Source/Sandbox/actor_components/HealthStationComponent.cpp
+++ Source/Sandbox/actor_components/HealthStationComponent.cpp
@@ -1,8 +1,59 @@
 #include "Sandbox/actor_components/HealthStationComponent.h"
 
+#include "Sandbox/actor_components/HealthComponent.h"
+#include "Sandbox/subsystems/DamageManagerSubsystem.h"
+
 UHealthStationComponent::UHealthStationComponent() {
     PrimaryComponentTick.bCanEverTick = false;
 }
 void UHealthStationComponent::BeginPlay() {
     Super::BeginPlay();
+    reset_current_capacity();
+}
+void UHealthStationComponent::TickComponent(float DeltaTime,
+                                            ELevelTick TickType,
+                                            FActorComponentTickFunction* ThisTickFunction) {
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    cooldown_remaining = FMath::Max(0.0f, cooldown_remaining - DeltaTime);
+
+    if (cooldown_remaining <= 0.0f) {
+        cooldown_remaining = 0.0f;
+        SetComponentTickEnabled(false);
+    }
+
+    broadcast_state();
+}
+void UHealthStationComponent::interact(AActor* interactor) {
+    if (!interactor || !can_interact(interactor)) {
+        return;
+    }
+
+    auto* const health{interactor->FindComponentByClass<UHealthComponent>()};
+    if (!health || health->at_max_health()) {
+        return;
+    }
+
+    if (auto* const manager{GetWorld()->GetSubsystem<UDamageManagerSubsystem>()}) {
+        FHealthChange change{heal_amount_per_use, EHealthChangeType::Healing};
+        manager->queue_damage(health, change);
+
+        current_capacity = FMath::Max(0.0f, current_capacity - change.value);
+        start_cooldown();
+        broadcast_state();
+    }
+}
+bool UHealthStationComponent::can_interact(AActor const* interactor) const {
+    return cooldown_remaining >= 0.0f;
+}
+void UHealthStationComponent::reset_current_capacity() {
+    current_capacity = max_capacity;
+}
+void UHealthStationComponent::broadcast_state() const {
+    on_station_state_changed.Broadcast(
+        {current_capacity, cooldown_remaining, can_interact(nullptr)});
+}
+void UHealthStationComponent::start_cooldown() {
+    cooldown_remaining = cooldown_duration;
+    SetComponentTickEnabled(true);
 }
--- Source/Sandbox/actor_components/HealthStationComponent.h
+++ Source/Sandbox/actor_components/HealthStationComponent.h
@@ -1,14 +1,60 @@
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "CoreMinimal.h"
+#include "Sandbox/interfaces/Interactable.h"
+
 #include "HealthStationComponent.generated.h"
 
+USTRUCT(BlueprintType)
+struct FStationStateData {
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadOnly)
+    float remaining_capacity;
+
+    UPROPERTY(BlueprintReadOnly)
+    float cooldown_remaining;
+
+    UPROPERTY(BlueprintReadOnly)
+    bool is_ready;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnStationStateChanged, FStationStateData, state_data);
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
-class SANDBOX_API UHealthStationComponent : public UActorComponent {
+class SANDBOX_API UHealthStationComponent
+    : public UActorComponent
+    , public IInteractable {
     GENERATED_BODY()
   public:
     UHealthStationComponent();
+
+    virtual void interact(AActor* interactor = nullptr) override;
+    virtual bool can_interact(AActor const* interactor) const override;
   protected:
     virtual void BeginPlay() override;
+    virtual void TickComponent(float DeltaTime,
+                               ELevelTick TickType,
+                               FActorComponentTickFunction* ThisTickFunction) override;
+
+    UPROPERTY(EditAnywhere, Category = "Health Station")
+    float max_capacity{100.0f};
+
+    UPROPERTY(EditAnywhere, Category = "Health Station")
+    float heal_amount_per_use{25.0f};
+
+    UPROPERTY(EditAnywhere, Category = "Health Station")
+    float cooldown_duration{2.0f}; // seconds
+
+    UPROPERTY(BlueprintAssignable, Category = "Health Station")
+    FOnStationStateChanged on_station_state_changed;
+  private:
+    void reset_current_capacity();
+    void broadcast_state() const;
+    void start_cooldown();
+
+    float current_capacity{0.0f};
+    float cooldown_remaining{0.0f};
+    FTimerHandle cooldown_timer_handle;
 };
