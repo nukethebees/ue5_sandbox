7e85b7b7d74ddd7ca23fd7f894a826c1cc087c0b
Matthew Love <matthewlove94@gmail.com>
2025-09-11 17:59:08 +0100

    Add debug print mixin

--- Source/Sandbox/actor_components/ActivatorComponent.h
+++ Source/Sandbox/actor_components/ActivatorComponent.h
@@ -1,15 +1,22 @@
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "CoreMinimal.h"
 #include "Sandbox/interfaces/Activatable.h"
+#include "Sandbox/mixins/log_msg_mixin.hpp"
 
 #include "ActivatorComponent.generated.h"
+
+namespace ml {
+inline static constexpr wchar_t ActivatorComponentLogTag[]{TEXT("ActivatorComponent")};
+}
 
 class AActor;
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
-class SANDBOX_API UActivatorComponent : public UActorComponent {
+class SANDBOX_API UActivatorComponent
+    : public UActorComponent
+    , public ml::LogMsgMixin<ml::ActivatorComponentLogTag> {
     GENERATED_BODY()
   public:
     UActivatorComponent();
