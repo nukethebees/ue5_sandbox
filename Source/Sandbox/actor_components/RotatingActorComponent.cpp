d7b7a229a7649f0b2563fef6dbfaaf0fe0a8377e
Matthew Love <matthewlove94@gmail.com>
2025-09-17 16:21:30 +0100

    Made destruction manager

--- Source/Sandbox/actor_components/RotatingActorComponent.cpp
+++ Source/Sandbox/actor_components/RotatingActorComponent.cpp
@@ -1,5 +1,6 @@
 #include "Sandbox/actor_components/RotatingActorComponent.h"
 #include "Sandbox/subsystems/RotationManagerSubsystem.h"
+#include "Sandbox/subsystems/DestructionManagerSubsystem.h"
 
 URotatingActorComponent::URotatingActorComponent() {
     PrimaryComponentTick.bCanEverTick = false;
@@ -8,23 +9,43 @@ URotatingActorComponent::URotatingActorComponent() {
 void URotatingActorComponent::BeginPlay() {
     Super::BeginPlay();
 
-    if (auto* world{GetWorld()}) {
-        if (auto* rotation_manager{world->GetSubsystem<URotationManagerSubsystem>()}) {
-            rotation_manager->register_rotating_actor(this, GetOwner(), rotation_type);
-        }
+    auto* world{GetWorld()};
+    if (!world) {
+        UE_LOGFMT(LogTemp, Warning, "Couldn't get world in in BeginPlay.");
+        return;
+    }
+
+    if (auto* rotation_manager{world->GetSubsystem<URotationManagerSubsystem>()}) {
+        rotation_manager->register_rotating_actor(this, GetOwner(), rotation_type);
+    } else {
+        UE_LOGFMT(LogTemp, Warning, "Couldn't get URotationManagerSubsystem.");
     }
 
     if (destroy_component_after_static_registration) {
-        DestroyComponent();
+        if (auto* destruction_manager{world->GetSubsystem<UDestructionManagerSubsystem>()}) {
+            destruction_manager->queue_component_destruction(this);
+        } else {
+            UE_LOGFMT(LogTemp, Warning, "Couldn't get UDestructionManagerSubsystem in BeginPlay.");
+        }
     }
 }
 
 void URotatingActorComponent::EndPlay(EEndPlayReason::Type EndPlayReason) {
-    if (auto* world{GetWorld()}) {
-        if (auto* rotation_manager{world->GetSubsystem<URotationManagerSubsystem>()}) {
-            rotation_manager->unregister_rotating_actor(this);
-        }
+    if (!destroy_component_after_static_registration) {
+        unregister_from_subsystem();
     }
 
     Super::EndPlay(EndPlayReason);
+}
+
+void URotatingActorComponent::unregister_from_subsystem() {
+    if (auto* world{GetWorld()}) {
+        if (auto* rotation_manager{world->GetSubsystem<URotationManagerSubsystem>()}) {
+            rotation_manager->unregister_rotating_actor(this);
+        } else {
+            UE_LOGFMT(LogTemp, Warning, "Couldn't get UDestructionManagerSubsystem in EndPlay.");
+        }
+    } else {
+        UE_LOGFMT(LogTemp, Warning, "Couldn't get world in in BeginPlay.");
+    }
 }
