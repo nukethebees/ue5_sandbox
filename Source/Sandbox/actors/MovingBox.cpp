2f91fb6eab953c4c3343849f7550342795bd3e5b
Matthew Love <matthewlove94@gmail.com>
2025-09-17 16:47:41 +0100

    Remove some comments

--- Source/Sandbox/actors/MovingBox.cpp
+++ Source/Sandbox/actors/MovingBox.cpp
@@ -1,20 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "MovingBox.h"
 
-// Sets default values
 AMovingBox::AMovingBox() {
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if
-    // you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
 void AMovingBox::BeginPlay() {
     Super::BeginPlay();
 }
 
-// Called every frame
 void AMovingBox::Tick(float DeltaTime) {
     Super::Tick(DeltaTime);
 
