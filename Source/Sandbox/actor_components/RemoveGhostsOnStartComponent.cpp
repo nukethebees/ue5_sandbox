020630cdfb467586dfe6fe5957e6fa2ae5d7b04e
Matthew Love <matthewlove94@gmail.com>
2025-09-17 16:29:51 +0100

    Use the destruction manager

--- Source/Sandbox/actor_components/RemoveGhostsOnStartComponent.cpp
+++ Source/Sandbox/actor_components/RemoveGhostsOnStartComponent.cpp
@@ -4,6 +4,7 @@
 
 #include "EngineUtils.h"
 #include "GameFramework/Actor.h"
+#include "Sandbox/subsystems/DestructionManagerSubsystem.h"
 
 URemoveGhostsOnStartComponent::URemoveGhostsOnStartComponent() {
     PrimaryComponentTick.bCanEverTick = true;
@@ -14,6 +15,11 @@ void URemoveGhostsOnStartComponent::TickComponent(float DeltaTime,
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
     if (auto* world{GetWorld()}) {
+        auto* destruction_manager{world->GetSubsystem<UDestructionManagerSubsystem>()};
+        if (!destruction_manager) {
+            return;
+        }
+
         for (auto* actor : TActorRange<AActor>(world)) {
             if (!actor) {
                 continue;
@@ -22,20 +28,12 @@ void URemoveGhostsOnStartComponent::TickComponent(float DeltaTime,
             auto components{actor->GetComponents()};
             for (auto* component : components) {
                 if (component->ComponentHasTag(cleanup_tag)) {
-#if UE_BUILD_DEBUG || UE_BUILD_DEVELOPMENT
-                    UE_LOG(LogTemp,
-                           Display,
-                           TEXT("URemoveGhostsOnStartComponent: Destroyed component: \"%s\" on "
-                                "actor: \"%s\""),
-                           *component->GetName(),
-                           *actor->GetName());
-#endif
-                    component->DestroyComponent();
+                    destruction_manager->queue_component_destruction(component);
                 }
             }
         }
-    }
 
-    // Clean itself up
-    DestroyComponent();
+        // Clean itself up
+        destruction_manager->queue_component_destruction(this);
+    }
 }
