7d064de80204d33b8dfbc66494f5af0dbdf8870e
Matthew Love <matthewlove94@gmail.com>
2025-09-18 14:24:29 +0100

    Make a pickup actor

--- Source/Sandbox/actors/SpeedBoostItemActor.cpp
+++ Source/Sandbox/actors/SpeedBoostItemActor.cpp
@@ -3,34 +3,11 @@
 #include "Sandbox/actor_components/SpeedBoostItemComponent.h"
 
 ASpeedBoostItemActor::ASpeedBoostItemActor() {
-    PrimaryActorTick.bCanEverTick = false;
-
+    // Create mesh component for visual representation
     mesh_component = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
-    RootComponent = mesh_component;
-    mesh_component->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-    mesh_component->SetCollisionResponseToAllChannels(ECR_Ignore);
-    mesh_component->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+    mesh_component->SetupAttachment(RootComponent);
+    mesh_component->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
     speed_boost_component =
         CreateDefaultSubobject<USpeedBoostItemComponent>(TEXT("SpeedBoostComponent"));
-}
-
-UPrimitiveComponent* ASpeedBoostItemActor::get_pickup_collision_component() {
-    return mesh_component;
-}
-
-bool ASpeedBoostItemActor::should_destroy_after_pickup() const {
-    return destroy_after_pickup;
-}
-
-float ASpeedBoostItemActor::get_pickup_cooldown() const {
-    return pickup_cooldown;
-}
-
-void ASpeedBoostItemActor::on_pre_pickup_effect(AActor* collector) {
-    // Actor can add visual/audio effects here
-}
-
-void ASpeedBoostItemActor::on_post_pickup_effect(AActor* collector) {
-    // Actor can add cleanup effects here
 }
