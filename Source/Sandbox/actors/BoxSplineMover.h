02912982c96ba8febfa2f2a600ba98cc333a4373
Matthew Love <matthewlove94@gmail.com>
2025-09-18 12:19:53 +0100

    Add actor that moves along a spline

---
+++ Source/Sandbox/actors/BoxSplineMover.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Components/SplineComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "BoxSplineMover.generated.h"
+
+UCLASS()
+class SANDBOX_API ABoxSplineMover : public AActor {
+    GENERATED_BODY()
+  public:
+    ABoxSplineMover();
+  protected:
+    virtual void BeginPlay() override;
+  public:
+    virtual void Tick(float DeltaTime) override;
+  protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USplineComponent* spline_component{};
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* mesh_component{};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float movement_speed{100.0f};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    bool loop_movement{true};
+  private:
+    float current_distance{0.0f};
+    float spline_length{0.0f};
+    bool moving_forward{true};
+
+    void update_position_along_spline(float distance);
+};
