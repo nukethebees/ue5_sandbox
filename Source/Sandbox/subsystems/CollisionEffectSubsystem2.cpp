b92d3ac653cd92ec46b5a4c8507cf39786f1b381
Matthew Love <matthewlove94@gmail.com>
2025-09-19 11:46:16 +0100

    Register the overlap event

--- Source/Sandbox/subsystems/CollisionEffectSubsystem2.cpp
+++ Source/Sandbox/subsystems/CollisionEffectSubsystem2.cpp
@@ -1,1 +1,8 @@
 #include "Sandbox/subsystems/CollisionEffectSubsystem2.h"
+
+void UCollisionEffectSubsystem2::handle_collision_event(UPrimitiveComponent* OverlappedComponent,
+                                                        AActor* OtherActor,
+                                                        UPrimitiveComponent* OtherComp,
+                                                        int32 OtherBodyIndex,
+                                                        bool bFromSweep,
+                                                        FHitResult const& SweepResult) {}
