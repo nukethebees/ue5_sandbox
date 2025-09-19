a43dbeb691ca0d54eb58b44a0198efab34a3db09
Matthew Love <matthewlove94@gmail.com>
2025-09-19 10:48:26 +0100

    Register actors and a struct of payload indexes

--- Source/Sandbox/subsystems/CollisionEffectSubsystem2.h
+++ Source/Sandbox/subsystems/CollisionEffectSubsystem2.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#include <optional>
 #include <tuple>
 #include <utility>
 
-#include "CoreMinimal.h"
 #include "Components/PrimitiveComponent.h"
-#include "Subsystems/WorldSubsystem.h"
-#include "Sandbox/mixins/log_msg_mixin.hpp"
+#include "CoreMinimal.h"
 #include "Sandbox/data/SpeedBoost.h"
+#include "Sandbox/mixins/log_msg_mixin.hpp"
+#include "Subsystems/WorldSubsystem.h"
 
 #include "CollisionEffectSubsystem2.generated.h"
 
@@ -47,12 +48,39 @@ struct FJumpIncreasePayload {
     int32 jump_increase{1};
 };
 
+USTRUCT(BlueprintType)
+struct FActorPayloadIndexes {
+    GENERATED_BODY()
+  public:
+    FActorPayloadIndexes() = default;
+};
+
 class UCollisionEffectSubsystem2Mixins {
   public:
     template <typename Self, typename Payload>
-    void add_payload(this Self&& self, Payload&& payload) {
+    void add_payload(this Self&& self, AActor* actor, Payload&& payload) {
+        auto const actor_index{self.register_actor(actor)};
+        if (!actor_index) {
+            return;
+        }
+
         auto& payload_array{ml::ArrayGet<Payload>(std::forward<Self>(self).payloads)};
         payload_array.Add(std::forward<Payload>(payload));
+    }
+
+    template <typename Self>
+    std::optional<int32> register_actor(this Self&& self, AActor* actor) {
+        if (!actor) {
+            return std::nullopt;
+        }
+
+        if (auto const* i{self.actor_ids.Find(actor)}) {
+            return *i;
+        }
+
+        auto const i{self.actor_ids.Add(actor)};
+        std::forward<Self>(self).actor_payload_indexes.AddDefaulted();
+        return i;
     }
 };
 
@@ -62,6 +90,9 @@ class SANDBOX_API UCollisionEffectSubsystem2
     , public UCollisionEffectSubsystem2Mixins
     , public ml::LogMsgMixin<ml::UCollisionEffectSubsystem2LogTag> {
     GENERATED_BODY()
-  public:
+    friend class UCollisionEffectSubsystem2Mixins;
+  private:
     ml::ArrayTuple<FSpeedBoostPayload, FJumpIncreasePayload> payloads;
+    TMap<AActor*, int32> actor_ids;
+    TArray<FActorPayloadIndexes> actor_payload_indexes;
 };
