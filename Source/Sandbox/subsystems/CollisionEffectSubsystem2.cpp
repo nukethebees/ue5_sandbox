77545026be7eb4775dd46cd42ed19979eb3a9808
Matthew Love <matthewlove94@gmail.com>
2025-09-19 11:58:46 +0100

    Add separate arrays for the collision boxes and actors. Implement part of handle_collision_event

--- Source/Sandbox/subsystems/CollisionEffectSubsystem2.cpp
+++ Source/Sandbox/subsystems/CollisionEffectSubsystem2.cpp
@@ -1,8 +1,47 @@
 #include "Sandbox/subsystems/CollisionEffectSubsystem2.h"
 
+#include "Sandbox/subsystems/DestructionManagerSubsystem.h"
+
 void UCollisionEffectSubsystem2::handle_collision_event(UPrimitiveComponent* OverlappedComponent,
                                                         AActor* OtherActor,
                                                         UPrimitiveComponent* OtherComp,
                                                         int32 OtherBodyIndex,
                                                         bool bFromSweep,
-                                                        FHitResult const& SweepResult) {}
+                                                        FHitResult const& SweepResult) {
+    if (!OtherActor || !OverlappedComponent) {
+        log_warning(TEXT("No OtherActor or OverlappedComponent."));
+        return;
+    }
+
+    auto const* index_ptr{collision_ids.Find(OverlappedComponent)};
+    if (!index_ptr) {
+        log_warning(TEXT("No collision entry."));
+        return;
+    }
+
+    int32 const index{*index_ptr};
+    auto* owner{actors[index].Get()};
+    if (!owner) {
+        log_warning(TEXT("No owner."));
+        return;
+    }
+
+    // Should have already been validated
+    auto& collision_owner{*Cast<ICollisionOwner>(owner)};
+
+    // Execute the effects
+    collision_owner.on_pre_collision_effect(OtherActor);
+    // for (auto const& entry : effect_components[index]) {
+    //     if (entry.component.IsValid() && entry.effect_interface) {
+    //         entry.effect_interface->execute_effect(OtherActor);
+    //     }
+    // }
+    collision_owner.on_collision_effect(OtherActor);
+    collision_owner.on_post_collision_effect(OtherActor);
+
+    if (collision_owner.should_destroy_after_collision()) {
+        if (auto* destruction_manager{GetWorld()->GetSubsystem<UDestructionManagerSubsystem>()}) {
+            destruction_manager->queue_actor_destruction(owner);
+        }
+    }
+}
