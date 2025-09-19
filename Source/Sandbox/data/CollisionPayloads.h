428cebc0c24d01d03299f7477f205a2bc093fbab
Matthew Love <matthewlove94@gmail.com>
2025-09-19 13:41:33 +0100

    Split the subsystem into different files

---
+++ Source/Sandbox/data/CollisionPayloads.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Sandbox/data/SpeedBoost.h"
+
+#include "CollisionPayloads.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSpeedBoostPayload {
+    GENERATED_BODY()
+
+    FSpeedBoostPayload() = default;
+    FSpeedBoostPayload(FSpeedBoost boost)
+        : speed_boost(boost) {}
+
+    void execute(AActor* actor) {}
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FSpeedBoost speed_boost{};
+};
+
+USTRUCT(BlueprintType)
+struct FJumpIncreasePayload {
+    GENERATED_BODY()
+
+    FJumpIncreasePayload() = default;
+    FJumpIncreasePayload(int32 inc)
+        : jump_count_increase(inc) {}
+
+    void execute(AActor* actor) {
+        UE_LOGFMT(LogTemp, Verbose, "FJumpIncreasePayload::execute");
+        if (auto* character{Cast<AMyCharacter>(actor)}) {
+            character->increase_max_jump_count(jump_count_increase);
+        } else {
+            UE_LOGFMT(
+                LogTemp, Warning, "FJumpIncreasePayload: Could not cast the actor to character.");
+        }
+    }
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 jump_count_increase{1};
+};
