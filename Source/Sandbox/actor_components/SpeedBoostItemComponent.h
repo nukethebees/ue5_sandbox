11d866abcab699da067f8bd120534dd62aedf405
Matthew Love <matthewlove94@gmail.com>
2025-09-19 15:34:57 +0100

    Remove the collision effect component

--- Source/Sandbox/actor_components/SpeedBoostItemComponent.h
+++ Source/Sandbox/actor_components/SpeedBoostItemComponent.h
@@ -3,7 +3,6 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "Sandbox/data/SpeedBoost.h"
-#include "Sandbox/interfaces/CollisionEffectComponent.h"
 
 #include "SpeedBoostItemComponent.generated.h"
 
