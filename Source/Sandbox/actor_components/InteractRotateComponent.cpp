5acd7ec4757793664b9d81c5808786c56f64bb5c
Matthew Love <matthewlove94@gmail.com>
2025-09-22 11:14:44 +0100

    Removed unneeded trigger overload

--- Source/Sandbox/actor_components/InteractRotateComponent.cpp
+++ Source/Sandbox/actor_components/InteractRotateComponent.cpp
@@ -12,49 +12,15 @@ void UInteractRotateComponent::BeginPlay() {
 
     log_verbose(TEXT("BeginPlay()"));
 
-    // Register with TriggerSubsystem
     if (auto* owner{GetOwner()}) {
         if (auto* const world{GetWorld()}) {
             if (auto* const trigger_subsystem{world->GetSubsystem<UTriggerSubsystem>()}) {
-                if (auto const id{
-                        trigger_subsystem->register_triggerable(*owner, rotation_payload)}) {
-                    triggerable_id = *id;
-                }
-
+                trigger_subsystem->register_triggerable(*owner, rotation_payload);
             } else {
                 log_warning(TEXT("Couldn't get UTriggerSubsystem."));
             }
         } else {
             log_warning(TEXT("Couldn't get world."));
         }
-    }
-}
-
-void UInteractRotateComponent::interact(AActor* interactor) {
-    trigger_rotation(interactor);
-}
-
-void UInteractRotateComponent::trigger_rotation(AActor* instigator) {
-    log_verbose(TEXT("Triggering rotation."));
-
-    if (auto* const world{GetWorld()}) {
-        if (auto* const trigger_subsystem{world->GetSubsystem<UTriggerSubsystem>()}) {
-            FTriggeringSource source{.type = ETriggerForm::PlayerInteraction,
-                                     .capabilities = {},
-                                     .instigator = instigator,
-                                     .source_location = instigator ? instigator->GetActorLocation()
-                                                                   : GetOwner()->GetActorLocation(),
-                                     .source_triggerable = std::nullopt};
-
-            if (instigator) {
-                source.capabilities.add_capability(ETriggerCapability::Humanoid);
-            }
-
-            trigger_subsystem->trigger(triggerable_id, source);
-        } else {
-            log_warning(TEXT("Couldn't get UTriggerSubsystem."));
-        }
-    } else {
-        log_warning(TEXT("Couldn't get world."));
     }
 }
--- Source/Sandbox/actor_components/InteractRotateComponent.h
+++ Source/Sandbox/actor_components/InteractRotateComponent.h
@@ -12,22 +12,13 @@
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SANDBOX_API UInteractRotateComponent
     : public UActorComponent
-    , public IInteractable
     , public ml::LogMsgMixin<"UInteractRotateComponent"> {
     GENERATED_BODY()
   public:
     UInteractRotateComponent();
   protected:
     virtual void BeginPlay() override;
-  public:
-    UFUNCTION()
-    void interact(AActor* interactor) override;
-
-    // Trigger this component's rotation behavior
-    void trigger_rotation(AActor* instigator = nullptr);
   private:
     UPROPERTY(EditAnywhere, Category = "Rotation")
     FRotatePayload rotation_payload{};
-
-    TriggerableId triggerable_id{TriggerableId::invalid()};
 };
