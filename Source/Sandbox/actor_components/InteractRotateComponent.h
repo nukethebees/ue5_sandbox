39fa0ad8da864d71c85141c498a6e5681c48b0a7
Matthew Love <matthewlove94@gmail.com>
2025-09-22 12:06:10 +0100

    Remove interactable interface

--- Source/Sandbox/actor_components/InteractRotateComponent.h
+++ Source/Sandbox/actor_components/InteractRotateComponent.h
@@ -2,7 +2,6 @@
 
 #include "Components/ActorComponent.h"
 #include "CoreMinimal.h"
-#include "Sandbox/interfaces/Interactable.h"
 #include "Sandbox/data/trigger/FRotatePayload.h"
 #include "Sandbox/data/trigger/TriggerableId.h"
 #include "Sandbox/mixins/log_msg_mixin.hpp"
