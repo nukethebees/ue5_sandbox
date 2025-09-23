d5d298232692c3c24851e90c4c15da31b748f3ce
Matthew Love <matthewlove94@gmail.com>
2025-09-23 11:54:13 +0100

    Use a concept to reduce SettingRange size

--- Source/Sandbox/data/video_options/VideoSettingRange.h
+++ Source/Sandbox/data/video_options/VideoSettingRange.h
@@ -1,33 +1,21 @@
 #pragma once
 
+#include <concepts> 
+
 #include "CoreMinimal.h"
 
-template <typename T>
-struct SettingRange; // Forward declaration
+template <std::totally_ordered T>
+struct SettingRange {
+    T min{};
+    T max{};
+
+    constexpr SettingRange() = default;
+    constexpr SettingRange(T const& min_val, T const& max_val)
+        : min{min_val}
+        , max{max_val} {}
+};
 
 template <>
 struct SettingRange<bool> {
     // No range needed for boolean settings
-};
-
-template <>
-struct SettingRange<float> {
-    float min{0.0f};
-    float max{1.0f};
-
-    SettingRange() = default;
-    SettingRange(float min_val, float max_val)
-        : min{min_val}
-        , max{max_val} {}
-};
-
-template <>
-struct SettingRange<int32> {
-    int32 min{0};
-    int32 max{100};
-
-    SettingRange() = default;
-    SettingRange(int32 min_val, int32 max_val)
-        : min{min_val}
-        , max{max_val} {}
 };
