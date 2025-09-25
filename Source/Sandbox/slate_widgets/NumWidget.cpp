6e9548e87867f2b10cb6793b2055270ceccd315d
Matthew Love <matthewlove94@gmail.com>
2025-09-25 15:03:30 +0100

    Add ability to print number

--- Source/Sandbox/slate_widgets/NumWidget.cpp
+++ Source/Sandbox/slate_widgets/NumWidget.cpp
@@ -1,18 +1,7 @@
 #include "Sandbox/slate_widgets/NumWidget.h"
 
-#include "SlateOptMacros.h"
-
-BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
-void SNumWidget::Construct(FArguments const& InArgs) {
-    label = InArgs._label;
-
-    ChildSlot[SNew(SHorizontalBox) +
-              SHorizontalBox::Slot().AutoWidth()[SNew(STextBlock).Text(label)]];
-}
-END_SLATE_FUNCTION_BUILD_OPTIMIZATION
-
 TSharedRef<SWidget> UNumWidget::RebuildWidget() {
-    slate_widget = SNew(SNumWidget).label(FText::FromString(TEXT("Default")));
+    slate_widget = SNew(SNumWidget<float>).label(FText::FromString(TEXT("Default"))).value(0);
 
     return slate_widget.ToSharedRef();
 }
