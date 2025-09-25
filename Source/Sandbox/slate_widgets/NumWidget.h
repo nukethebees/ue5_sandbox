e0d2a7385de42aa73032ded79165e73daa95db4c
Matthew Love <matthewlove94@gmail.com>
2025-09-25 15:03:30 +0100

    Add ability to print number

--- Source/Sandbox/slate_widgets/NumWidget.h
+++ Source/Sandbox/slate_widgets/NumWidget.h
@@ -2,21 +2,49 @@
 
 #pragma once
 
+#include <optional>
+#include <utility>
+
 #include "CoreMinimal.h"
 #include "Widgets/SCompoundWidget.h"
 #include "Components/Widget.h"
+#include "SlateOptMacros.h"
 
 #include "NumWidget.generated.h"
 
+template <typename T>
 class SANDBOX_API SNumWidget : public SCompoundWidget {
   public:
     SLATE_BEGIN_ARGS(SNumWidget) {}
     SLATE_ARGUMENT(FText, label)
+    SLATE_ARGUMENT(T, value)
+    SLATE_ARGUMENT(std::optional<T>, max_value)
     SLATE_END_ARGS()
 
-    void Construct(FArguments const& InArgs);
+    BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+    void Construct(FArguments const& InArgs) {
+        label_ = InArgs._label;
+        value_ = InArgs._value;
+        max_value_ = InArgs._max_value;
 
-    FText label;
+        ChildSlot[SNew(STextBlock).Text(this, &SNumWidget::get_display_text)];
+    }
+    END_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+    FText get_display_text() const {
+        if (max_value_) {
+            return FText::Format(NSLOCTEXT("StatWidget", "ValueWithMax", "{0}: {1} / {2}"),
+                                 label_,
+                                 FText::AsNumber(value_),
+                                 FText::AsNumber(*max_value_));
+        }
+        return FText::Format(
+            NSLOCTEXT("StatWidget", "ValueOnly", "{0}: {1}"), label_, FText::AsNumber(value_));
+    }
+  private:
+    FText label_;
+    T value_;
+    std::optional<T> max_value_;
 };
 
 UCLASS()
@@ -27,5 +55,5 @@ class UNumWidget : public UWidget {
     virtual TSharedRef<SWidget> RebuildWidget() override;
     virtual void ReleaseSlateResources(bool bReleaseChildren) override;
   private:
-    TSharedPtr<SNumWidget> slate_widget;
+    TSharedPtr<SNumWidget<float>> slate_widget;
 };
