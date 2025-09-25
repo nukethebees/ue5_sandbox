991db2e838f2362f0a04531e771bf033b4bf71d1
Matthew Love <matthewlove94@gmail.com>
2025-09-25 15:27:36 +0100

    Add accessors and an int instantiation

--- Source/Sandbox/slate_widgets/NumWidget.cpp
+++ Source/Sandbox/slate_widgets/NumWidget.cpp
@@ -1,11 +1,21 @@
 #include "Sandbox/slate_widgets/NumWidget.h"
 
-TSharedRef<SWidget> UNumWidget::RebuildWidget() {
+TSharedRef<SWidget> UFloatNumWidget::RebuildWidget() {
     slate_widget = SNew(SNumWidget<float>).label(FText::FromString(TEXT("Default"))).value(0);
 
     return slate_widget.ToSharedRef();
 }
-void UNumWidget::ReleaseSlateResources(bool bReleaseChildren) {
+void UFloatNumWidget::ReleaseSlateResources(bool bReleaseChildren) {
+    Super::ReleaseSlateResources(bReleaseChildren);
+    slate_widget.Reset();
+}
+
+TSharedRef<SWidget> UIntNumWidget::RebuildWidget() {
+    slate_widget = SNew(SNumWidget<int32>).label(FText::FromString(TEXT("Default"))).value(0);
+
+    return slate_widget.ToSharedRef();
+}
+void UIntNumWidget::ReleaseSlateResources(bool bReleaseChildren) {
     Super::ReleaseSlateResources(bReleaseChildren);
     slate_widget.Reset();
 }
