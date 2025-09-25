c4c2c5849c6f8591fda566549f19b220f81d8c50
Matthew Love <matthewlove94@gmail.com>
2025-09-25 18:21:14 +0100

    Add another mixin function

--- Source/Sandbox/slate_widgets/NumWidget.cpp
+++ Source/Sandbox/slate_widgets/NumWidget.cpp
@@ -63,20 +63,9 @@ FText SNumWidget<T>::get_display_text() const {
     return cached_display_text_;
 }
 
-TSharedRef<SWidget> UFloatNumWidget::RebuildWidget() {
-    slate_widget = SNew(SNumWidget<float>).label(FText::FromString(TEXT("Default"))).value(0);
-
-    return slate_widget.ToSharedRef();
-}
 void UFloatNumWidget::ReleaseSlateResources(bool bReleaseChildren) {
     Super::ReleaseSlateResources(bReleaseChildren);
     slate_widget.Reset();
-}
-
-TSharedRef<SWidget> UIntNumWidget::RebuildWidget() {
-    slate_widget = SNew(SNumWidget<int32>).label(FText::FromString(TEXT("Default"))).value(0);
-
-    return slate_widget.ToSharedRef();
 }
 void UIntNumWidget::ReleaseSlateResources(bool bReleaseChildren) {
     Super::ReleaseSlateResources(bReleaseChildren);
