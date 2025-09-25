efeeed6ff4dc5652e1caf0e3c1dee5573d038150
Matthew Love <matthewlove94@gmail.com>
2025-09-25 17:40:34 +0100

    Added killable cube actor as an example killable thing

---
+++ Source/Sandbox/actors/KillableCube.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "components/StaticMeshComponent.h"
+#include "CoreMinimal.h"
+#include "engine/World.h"
+#include "GameFramework/Actor.h"
+#include "Sandbox/actor_components/HealthComponent.h"
+
+#include "KillableCube.generated.h"
+
+UCLASS()
+class SANDBOX_API AKillableCube : public AActor {
+    GENERATED_BODY()
+  public:
+    AKillableCube();
+  protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* mesh_component_{nullptr};
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UHealthComponent* health_component_{nullptr};
+  private:
+    UFUNCTION()
+    void on_death();
+};
