dcc878c10dca8c5a239802878eefaf42dbdbc819
Matthew Love <matthewlove94@gmail.com>
2025-10-01 10:01:32 +0100

    Use get_random_nav_point

--- Source/Sandbox/behaviour_tree/tasks/BTTask_MoveToRandom.cpp
+++ Source/Sandbox/behaviour_tree/tasks/BTTask_MoveToRandom.cpp
@@ -3,6 +3,9 @@
 #include "AIController.h"
 #include "Navigation/PathFollowingComponent.h"
 #include "NavigationSystem.h"
+
+#include "Sandbox/utilities/navigation.h"
+
 #include "Sandbox/macros/null_checks.hpp"
 
 UBTTask_MoveToRandom::UBTTask_MoveToRandom() {
@@ -16,27 +19,16 @@ UBTTask_MoveToRandom::UBTTask_MoveToRandom() {
 EBTNodeResult::Type UBTTask_MoveToRandom::ExecuteTask(UBehaviorTreeComponent& OwnerComp,
                                                       uint8* NodeMemory) {
     TRY_INIT_BTTASK_PTR(ai_controller, OwnerComp.GetAIOwner());
-    TRY_INIT_BTTASK_PTR(pawn, ai_controller->GetPawn());
-    TRY_INIT_BTTASK_PTR(world, ai_controller->GetWorld());
-    TRY_INIT_BTTASK_PTR(nav_sys, UNavigationSystemV1::GetCurrent(world));
-
-    auto const actor_location{pawn->GetActorLocation()};
-
-    FNavLocation random_point{};
-    bool const found{
-        nav_sys->GetRandomReachablePointInRadius(actor_location, radius, random_point)};
-
-    if (!found) {
+    auto const random_point{ml::get_random_nav_point(ai_controller->GetPawn(), radius)};
+    if (!random_point) {
         UE_LOG(LogTemp, Warning, TEXT("No point found."));
         return EBTNodeResult::Failed;
     }
 
-    // Move to the random point
-    FAIMoveRequest move_request{random_point.Location};
+    FAIMoveRequest move_request{random_point->Location};
     move_request.SetAcceptanceRadius(acceptable_radius);
 
-    FPathFollowingRequestResult result{ai_controller->MoveTo(move_request)};
-
+    auto const result{ai_controller->MoveTo(move_request)};
     if (result.Code == EPathFollowingRequestResult::RequestSuccessful) {
         return EBTNodeResult::Succeeded;
     }
