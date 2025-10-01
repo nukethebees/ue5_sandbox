4ca19a2fc8a81a3c74d4defff88532004f2e33bd
Matthew Love <matthewlove94@gmail.com>
2025-10-01 17:56:57 +0100

    Hold world as a reference

--- Source/Sandbox/subsystems/ObjectPoolSubsystem.h
+++ Source/Sandbox/subsystems/ObjectPoolSubsystem.h
@@ -11,23 +11,26 @@ UCLASS()
 class SANDBOX_API UObjectPoolSubsystem : public UWorldSubsystem {
     GENERATED_BODY()
   public:
-    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    using pool_type = UObjectPoolSubsystemCore<FBulletPoolConfig>;
+
+    UObjectPoolSubsystem()
+        : core_(MakeUnique<pool_type>(*GetWorld())) {}
 
     template <typename Config>
     typename Config::ActorType*
         get_item(TSubclassOf<typename Config::ActorType> subclass = nullptr) {
-        return core_.get_item<Config>(subclass);
+        return core_->get_item<Config>(subclass);
     }
 
     template <typename Config>
     void return_item(typename Config::ActorType* item) {
-        core_.return_item<Config>(item);
+        core_->return_item<Config>(item);
     }
 
     template <typename Config>
-    void preallocate(UWorld& world, TSubclassOf<typename Config::ActorType> actor_class, int32 n) {
-        core_.preallocate<Config>(world, actor_class, n);
+    void preallocate(TSubclassOf<typename Config::ActorType> actor_class, int32 n) {
+        core_->preallocate<Config>(actor_class, n);
     }
   private:
-    UObjectPoolSubsystemCore<FBulletPoolConfig> core_{};
+    TUniquePtr<pool_type> core_{nullptr};
 };
