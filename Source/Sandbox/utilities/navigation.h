59060fff950d751d1531a2192d2bf2d798b821a5
Matthew Love <matthewlove94@gmail.com>
2025-10-01 10:01:16 +0100

    Add function for getting random navigation point

---
+++ Source/Sandbox/utilities/navigation.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <optional>
+
+#include "CoreMinimal.h"
+#include "AI/Navigation/NavigationTypes.h"
+
+class AActor;
+
+namespace ml {
+auto get_random_nav_point(AActor* actor, float radius) -> std::optional<FNavLocation>;
+}
