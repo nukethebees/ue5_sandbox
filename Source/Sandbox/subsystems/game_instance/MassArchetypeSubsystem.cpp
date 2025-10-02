2e25dc09c666e724394dcc0de9365d60938c8af4
Matthew Love <matthewlove94@gmail.com>
2025-10-02 12:20:15 +0100

    Add bullet archetype to manager

--- Source/Sandbox/subsystems/game_instance/MassArchetypeSubsystem.cpp
+++ Source/Sandbox/subsystems/game_instance/MassArchetypeSubsystem.cpp
@@ -1,3 +1,51 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Sandbox/subsystems/game_instance/MassArchetypeSubsystem.h"
+
+#include "MassEntityManager.h"
+#include "MassEntitySubsystem.h"
+#include "MassEntityTypes.h"
+
+#include "Sandbox/mass_entity/fragments/MassBulletFragments.h"
+
+void UMassArchetypeSubsystem::Initialize(FSubsystemCollectionBase& collection) {
+    Super::Initialize(collection);
+    constexpr auto logger{NestedLogger<"Initialize">()};
+
+    // Archetypes are created once and cached
+    auto* world{GetWorld()};
+    if (!world) {
+        logger.log_error(TEXT("world is nullptr"));
+        return;
+    }
+
+    auto* entity_subsystem{world->GetSubsystem<UMassEntitySubsystem>()};
+    if (!entity_subsystem) {
+        logger.log_error(TEXT("entity_subsystem is nullptr"));
+        return;
+    }
+
+    build_archetypes(entity_subsystem->GetMutableEntityManager());
+}
+
+void UMassArchetypeSubsystem::Deinitialize() {
+    Super::Deinitialize();
+
+    bullet_archetype = {};
+}
+
+auto UMassArchetypeSubsystem::get_bullet_archetype() const -> FMassArchetypeHandle {
+    return bullet_archetype;
+}
+
+void UMassArchetypeSubsystem::build_archetypes(FMassEntityManager& entity_manager) {
+    {
+        auto descriptor{FMassArchetypeCompositionDescriptor{}};
+        descriptor.Fragments.Add(*FMassBulletTransformFragment::StaticStruct());
+        descriptor.Fragments.Add(*FMassBulletVelocityFragment::StaticStruct());
+        descriptor.Fragments.Add(*FMassBulletInstanceIndexFragment::StaticStruct());
+
+        auto creation_params{FMassArchetypeCreationParams{}};
+        creation_params.DebugName = FName(TEXT("bullet_archetype"));
+
+        bullet_archetype = entity_manager.CreateArchetype(descriptor, creation_params);
+    }
+}
