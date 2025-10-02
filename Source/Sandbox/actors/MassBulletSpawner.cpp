77874c39eb95b1ecdeab5a6015a5e7f5143ff875
Matthew Love <matthewlove94@gmail.com>
2025-10-02 12:59:14 +0100

    Spawn entities without visuals

--- Source/Sandbox/actors/MassBulletSpawner.cpp
+++ Source/Sandbox/actors/MassBulletSpawner.cpp
@@ -1,10 +1,15 @@
 #include "Sandbox/actors/MassBulletSpawner.h"
 
 #include "Components/ArrowComponent.h"
+#include "MassEntitySubsystem.h"
 
 #include "Sandbox/actors/BulletActor.h"
 #include "Sandbox/data/pool/PoolConfig.h"
+#include "Sandbox/mass_entity/fragments/MassBulletFragments.h"
+#include "Sandbox/subsystems/game_instance/MassArchetypeSubsystem.h"
 #include "Sandbox/subsystems/world/ObjectPoolSubsystem.h"
+
+#include "Sandbox/macros/null_checks.hpp"
 
 AMassBulletSpawner::AMassBulletSpawner() {
     PrimaryActorTick.bCanEverTick = true;
@@ -32,6 +37,7 @@ void AMassBulletSpawner::Tick(float DeltaTime) {
 
 void AMassBulletSpawner::spawn_bullet() {
     TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("Sandbox::AMassBulletSpawner::spawn_bullet"))
+    constexpr auto logger{NestedLogger<"spawn_bullet">()};
 
     if (!bullet_class) {
         log_warning(TEXT("bullet_class is nullptr."));
@@ -42,6 +48,36 @@ void AMassBulletSpawner::spawn_bullet() {
         return;
     }
 
+    TRY_INIT_PTR(world, GetWorld());
+    TRY_INIT_PTR(game_instance, GetGameInstance());
+    TRY_INIT_PTR(mass_subsystem, world->GetSubsystem<UMassEntitySubsystem>());
+    TRY_INIT_PTR(archetype_subsystem, game_instance->GetSubsystem<UMassArchetypeSubsystem>());
+
+    auto bullet_archetype{archetype_subsystem->get_bullet_archetype()};
+    if (!bullet_archetype.IsValid()) {
+        logger.log_verbose(TEXT("bullet_archetype is invalid."));
+        return;
+    }
+
+    logger.log_verbose(TEXT("Creating entity"));
+    auto& entity_manager{mass_subsystem->GetMutableEntityManager()};
+    auto entity = entity_manager.CreateEntity(bullet_archetype);
+
     auto const spawn_location{fire_point->GetComponentLocation()};
     auto const spawn_rotation{fire_point->GetComponentRotation()};
+
+    auto& transform_frag{
+        entity_manager.GetFragmentDataChecked<FMassBulletTransformFragment>(entity)};
+    transform_frag.transform = FTransform(spawn_location);
+
+    auto& velocity_frag =
+        entity_manager.GetFragmentDataChecked<FMassBulletVelocityFragment>(entity);
+    auto const velocity{spawn_rotation.Vector() * bullet_speed};
+    velocity_frag.velocity = velocity;
+
+    //  auto & index_frag =
+    //  entity_manager.GetFragmentDataChecked<FMassBulletInstanceIndexFragment>(entity); if (auto
+    //  idx = visualization_component->add_instance(FTransform(position))) {
+    //      index_frag.instance_index = *idx;
+    //  }
 }
--- Source/Sandbox/subsystems/game_instance/MassArchetypeSubsystem.h
+++ Source/Sandbox/subsystems/game_instance/MassArchetypeSubsystem.h
@@ -15,10 +15,11 @@ class SANDBOX_API UMassArchetypeSubsystem
     : public UGameInstanceSubsystem
     , public ml::LogMsgMixin<"UMassArchetypeSubsystem"> {
     GENERATED_BODY()
+  public:
+    auto get_bullet_archetype() const -> FMassArchetypeHandle;
+  protected:
     virtual void Initialize(FSubsystemCollectionBase& Collection) override;
     virtual void Deinitialize() override;
-
-    auto get_bullet_archetype() const -> FMassArchetypeHandle;
   private:
     void on_post_world_initialisation(UWorld* world,
                                       UWorld::InitializationValues const initialisation_values);
