58cd5d585327dadc9683d0dbee7bd856604d2e66
Matthew Love <matthewlove94@gmail.com>
2025-10-02 12:20:15 +0100

    Add bullet archetype to manager

--- Source/Sandbox/subsystems/game_instance/MassArchetypeSubsystem.h
+++ Source/Sandbox/subsystems/game_instance/MassArchetypeSubsystem.h
@@ -1,11 +1,24 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "MassArchetypeTypes.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+
+#include "Sandbox/mixins/log_msg_mixin.hpp"
 
 #include "MassArchetypeSubsystem.generated.h"
 
 UCLASS()
-class SANDBOX_API UMassArchetypeSubsystem : public UGameInstanceSubsystem {
+class SANDBOX_API UMassArchetypeSubsystem
+    : public UGameInstanceSubsystem
+    , public ml::LogMsgMixin<"UMassArchetypeSubsystem"> {
     GENERATED_BODY()
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+
+    auto get_bullet_archetype() const -> FMassArchetypeHandle;
+  private:
+    void build_archetypes(FMassEntityManager& entity_manager);
+
+    FMassArchetypeHandle bullet_archetype;
 };
