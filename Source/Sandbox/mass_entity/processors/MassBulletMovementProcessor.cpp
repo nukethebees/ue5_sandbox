db8d02a05f3fa90c680151ad35067b6fc89dd520
Matthew Love <matthewlove94@gmail.com>
2025-10-03 12:16:58 +0100

    Make alternate impls of the movement execution

--- Source/Sandbox/mass_entity/processors/MassBulletMovementProcessor.cpp
+++ Source/Sandbox/mass_entity/processors/MassBulletMovementProcessor.cpp
@@ -6,7 +6,8 @@
 #include "Sandbox/mass_entity/fragments/MassBulletFragments.h"
 
 void FMassBulletMovementExecutor::Execute(FMassExecutionContext& context) {
-#if 1
+#define EXECUTION_BRANCH 0
+#if (EXECUTION_BRANCH == 0)
     TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("Sandbox::FMassBulletMovementExecutor::Execute"))
 
     constexpr auto executor{[](FMassExecutionContext& context, auto& Data, uint32 EntityIndex) {
@@ -19,7 +20,20 @@ void FMassBulletMovementExecutor::Execute(FMassExecutionContext& context) {
     }};
 
     ForEachEntity(context, accessors, std::move(executor));
-#else
+#elif (EXECUTION_BRANCH == 1)
+    TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("Sandbox::FMassBulletMovementExecutor::Execute"))
+
+    constexpr auto executor{[](FMassExecutionContext& context, auto& Data, uint32 EntityIndex) {
+        auto const delta_time{context.GetDeltaTimeSeconds()};
+        auto const transforms{context.GetMutableFragmentView<FMassBulletTransformFragment>()};
+        auto const velocities{context.GetFragmentView<FMassBulletVelocityFragment>()};
+
+        auto const displacement{velocities[EntityIndex].velocity * delta_time};
+        transforms[EntityIndex].transform.AddToTranslation(displacement);
+    }};
+
+    ParallelForEachEntity(context, accessors, std::move(executor));
+#elif (EXECUTION_BRANCH == 2)
     TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("Sandbox::FMassBulletMovementExecutor::Execute"))
 
     constexpr auto executor{[](FMassExecutionContext& context, auto& Data) {
@@ -33,11 +47,20 @@ void FMassBulletMovementExecutor::Execute(FMassExecutionContext& context) {
             auto const displacement{velocities[i].velocity * delta_time};
             transforms[i].transform.AddToTranslation(displacement);
         }
+    }};
 
-        // for (int32 i{0}; i < n; ++i) {
-        //     Data.Get<FMassBulletTransformFragment>()[i].transform.AddToTranslation(
-        //         Data.Get<FMassBulletVelocityFragment>()[i].velocity * delta_time);
-        // }
+    ParallelForEachEntityChunk(context, accessors, std::move(executor));
+#else
+    TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("Sandbox::FMassBulletMovementExecutor::Execute"))
+
+    constexpr auto executor{[](FMassExecutionContext& context, auto& Data) {
+        auto const n{context.GetNumEntities()};
+        auto const delta_time{context.GetDeltaTimeSeconds()};
+
+        for (int32 i{0}; i < n; ++i) {
+            Data.Get<FMassBulletTransformFragment>()[i].transform.AddToTranslation(
+                Data.Get<FMassBulletVelocityFragment>()[i].velocity * delta_time);
+        }
     }};
 
     ParallelForEachEntityChunk(context, accessors, std::move(executor));
