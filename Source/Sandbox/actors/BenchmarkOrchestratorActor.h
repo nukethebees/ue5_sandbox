490d28db2ec9b31a8dcb8e6548dddd7804917ac8
Matthew Love <matthewlove94@gmail.com>
2025-10-03 11:56:41 +0100

    Add printing and config options to the benchmark

--- Source/Sandbox/actors/BenchmarkOrchestratorActor.h
+++ Source/Sandbox/actors/BenchmarkOrchestratorActor.h
@@ -5,12 +5,23 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 
+#include "Sandbox/mixins/log_msg_mixin.hpp"
+
 #include "BenchmarkOrchestratorActor.generated.h"
 
 class ACameraActor;
 
+UENUM(BlueprintType)
+enum class EBenchmarkEndState : uint8 {
+    Play UMETA(DisplayName = "Play"),
+    Quit UMETA(DisplayName = "Quit"),
+    Pause UMETA(DisplayName = "Pause")
+};
+
 UCLASS()
-class SANDBOX_API ABenchmarkOrchestratorActor : public AActor {
+class SANDBOX_API ABenchmarkOrchestratorActor
+    : public AActor
+    , public ml::LogMsgMixin<"ABenchmarkOrchestratorActor"> {
     GENERATED_BODY()
   public:
     ABenchmarkOrchestratorActor();
@@ -18,14 +29,29 @@ class SANDBOX_API ABenchmarkOrchestratorActor : public AActor {
     virtual void BeginPlay() override;
     virtual void EndPlay(EEndPlayReason::Type const EndPlayReason) override;
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Benchmark")
+    FName benchmark_name{NAME_None};
+
     UPROPERTY(EditAnywhere, Category = "Benchmark")
     float benchmark_duration_seconds{10.0f};
 
+    UPROPERTY(EditAnywhere, Category = "Benchmark")
+    float benchmark_print_update_seconds{5.0f};
+
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Benchmark")
     ACameraActor* benchmark_camera;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Benchmark")
+    bool run_benchmark{true};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Benchmark")
+    EBenchmarkEndState benchmark_end{EBenchmarkEndState::Quit};
   private:
     void start_trace();
     void stop_trace();
+    void log_time();
 
     FTimerHandle trace_timer_handle;
+    FTimerHandle log_timer_handle;
+    float time_elapsed{0.0f};
 };
