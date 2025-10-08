916a55bfe515d24bba1be4a6b969e509613f2acd
Matthew Love <matthewlove94@gmail.com>
2025-10-08 13:50:38 +0100

    Fix UI code not working in shipping

--- Source/Sandbox/utilities/ui.cpp
+++ Source/Sandbox/utilities/ui.cpp
@@ -5,10 +5,16 @@
 namespace ml {
 int32 scale_font_to_umg(int32 font_size) {
     constexpr float slate_dpi{96.0f};
+    constexpr float default_umg_dpi{72.0f};
 
     if (auto* ui_settings{GetDefault<UUserInterfaceSettings>()}) {
-        auto const umg_api{static_cast<float>(ui_settings->GetFontDisplayDPI())};
-        auto const dpi_scale_factor{umg_api / slate_dpi};
+        auto umg_dpi{72.0f};
+        if (GEngine && GEngine->GameViewport->Viewport) {
+            auto const size{GEngine->GameViewport->Viewport->GetSizeXY()};
+            umg_dpi = ui_settings->GetDPIScaleBasedOnSize(size);
+        }
+
+        auto const dpi_scale_factor{umg_dpi / slate_dpi};
         return FMath::RoundToInt(static_cast<float>(font_size) * dpi_scale_factor);
     }
 
