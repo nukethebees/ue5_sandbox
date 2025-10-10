9a09e27503f432de3d3fe734bdd846ff294ec457
Matthew Love <matthewlove94@gmail.com>
2025-10-10 13:56:05 +0100

    Created a core logging function

--- Source/Sandbox/mixins/log_msg_mixin.hpp
+++ Source/Sandbox/mixins/log_msg_mixin.hpp
@@ -7,6 +7,12 @@
 
 #include "Sandbox/utilities/string_literal_wrapper.h"
 
+#define LOG_MSG_TO(VERBOSITY, LOG_CATEGORY) \
+    log_to<ELogVerbosity::VERBOSITY, LOG_CATEGORY>(std::move(fmt), std::forward<Args>(args)...);
+
+#define LOG_MSG(VERBOSITY) \
+    log_to<ELogVerbosity::VERBOSITY, log_category>(std::move(fmt), std::forward<Args>(args)...);
+
 namespace ml {
 template <StaticTCharString tag, auto& log_category_ = LogTemp>
 struct LogMsgMixin {
@@ -26,108 +32,112 @@ struct LogMsgMixin {
         UE::Core::TCheckedFormatString<typename FString::FmtCharType,
                                        std::remove_cv_t<std::remove_reference_t<Args>>...>;
 
+    template <ELogVerbosity::Type verbosity, auto& log_cat, typename... Args>
+        requires (verbosity < ELogVerbosity::NumVerbosity)
+    void log_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
+#define LOG_BRANCH(VERBOSITY)                                                 \
+    else if constexpr (verbosity == ELogVerbosity::VERBOSITY) {               \
+        UE_LOG(log_cat,                                                       \
+               VERBOSITY,                                                     \
+               TEXT("%s: %s"),                                                \
+               get_tag(),                                                     \
+               *FString::Printf(std::move(fmt), std::forward<Args>(args)...)) \
+    }
+
+        if constexpr (verbosity == ELogVerbosity::Fatal) {
+            LOG_MSG(Fatal);
+        }
+        LOG_BRANCH(Error)
+        LOG_BRANCH(Warning)
+        LOG_BRANCH(Log)
+        LOG_BRANCH(Display)
+        LOG_BRANCH(Verbose)
+        LOG_BRANCH(VeryVerbose)
+#undef LOG_BRANCH
+    }
+
+    template <ELogVerbosity::Type verbosity, auto& log_cat, typename... Args>
+        requires (verbosity < ELogVerbosity::NumVerbosity)
+    void log(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
+        log_to<verbosity, log_cat>(std::move(fmt), std::forward<Args>(args)...);
+    }
+
     template <auto& log_cat, typename... Args>
     void log_fatal_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        UE_LOG(log_cat,
-               Fatal,
-               TEXT("%s: %s"),
-               get_tag(),
-               *FString::Printf(std::move(fmt), std::forward<Args>(args)...));
+        LOG_MSG_TO(Fatal, log_cat);
     }
 
     template <typename... Args>
     void log_fatal(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        log_fatal_to<log_category>(std::move(fmt), std::forward<Args>(args)...);
+        LOG_MSG(Fatal);
     }
 
     template <auto& log_cat, typename... Args>
     void log_error_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        UE_LOG(log_cat,
-               Error,
-               TEXT("%s: %s"),
-               get_tag(),
-               *FString::Printf(std::move(fmt), std::forward<Args>(args)...));
+        LOG_MSG_TO(Error, log_cat);
     }
 
     template <typename... Args>
     void log_error(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        log_error_to<log_category>(std::move(fmt), std::forward<Args>(args)...);
+        LOG_MSG(Error);
     }
 
     template <auto& log_cat, typename... Args>
     void log_warning_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        UE_LOG(log_cat,
-               Warning,
-               TEXT("%s: %s"),
-               get_tag(),
-               *FString::Printf(std::move(fmt), std::forward<Args>(args)...));
+        LOG_MSG_TO(Warning, log_cat);
     }
 
     template <typename... Args>
     void log_warning(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        log_warning_to<log_category>(std::move(fmt), std::forward<Args>(args)...);
+        LOG_MSG(Warning);
     }
 
     template <auto& log_cat, typename... Args>
     void log_display_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        UE_LOG(log_cat,
-               Display,
-               TEXT("%s: %s"),
-               get_tag(),
-               *FString::Printf(std::move(fmt), std::forward<Args>(args)...));
+        LOG_MSG_TO(Display, log_cat);
     }
 
     template <typename... Args>
     void log_display(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        log_display_to<log_category>(std::move(fmt), std::forward<Args>(args)...);
+        LOG_MSG(Display);
     }
 
     template <auto& log_cat, typename... Args>
     void log_log_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
 #if UE_BUILD_DEVELOPMENT
-        UE_LOG(log_cat,
-               Log,
-               TEXT("%s: %s"),
-               get_tag(),
-               *FString::Printf(std::move(fmt), std::forward<Args>(args)...));
+        LOG_MSG_TO(Log, log_cat);
 #endif
     }
 
     template <typename... Args>
     void log_log(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        log_log_to<log_category>(std::move(fmt), std::forward<Args>(args)...);
+        LOG_MSG(Log);
     }
 
     template <auto& log_cat, typename... Args>
     void log_verbose_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
 #if UE_BUILD_DEVELOPMENT
-        UE_LOG(log_cat,
-               Verbose,
-               TEXT("%s: %s"),
-               get_tag(),
-               *FString::Printf(std::move(fmt), std::forward<Args>(args)...));
+        LOG_MSG_TO(Verbose, log_cat);
 #endif
     }
 
     template <typename... Args>
     void log_verbose(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        log_verbose_to<log_category>(std::move(fmt), std::forward<Args>(args)...);
+        LOG_MSG(Verbose);
     }
 
     template <auto& log_cat, typename... Args>
     void log_very_verbose_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
 #if UE_BUILD_DEVELOPMENT
-        UE_LOG(log_cat,
-               VeryVerbose,
-               TEXT("%s: %s"),
-               get_tag(),
-               *FString::Printf(std::move(fmt), std::forward<Args>(args)...));
+        LOG_MSG_TO(VeryVerbose, log_cat);
 #endif
     }
 
     template <typename... Args>
     void log_very_verbose(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-        log_very_verbose_to<log_category>(std::move(fmt), std::forward<Args>(args)...);
+        LOG_MSG(VeryVerbose);
     }
 };
 }
+
+#undef LOG_MSG
