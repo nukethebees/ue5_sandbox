6e16080d3e411b23fe09a6dccd45650cc9ec72f7
Matthew Love <matthewlove94@gmail.com>
2025-10-10 15:11:00 +0100

    Add project log categories.

---
+++ Source/Sandbox/SandboxLogCategories.cpp
@@ -0,0 +1,6 @@
+#include "SandboxLogCategories.h"
+
+DEFINE_LOG_CATEGORY(LogSandboxCore);
+DEFINE_LOG_CATEGORY(LogSandboxUI);
+DEFINE_LOG_CATEGORY(LogSandboxMassEntity);
+DEFINE_LOG_CATEGORY(LogSandboxGuns);
