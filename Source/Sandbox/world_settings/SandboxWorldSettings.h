ff47bac43b801ffc8199a1aebc83a6e70adc3f4b
Matthew Love <matthewlove94@gmail.com>
2025-10-10 15:56:47 +0100

    Make a world settings class

---
+++ Source/Sandbox/world_settings/SandboxWorldSettings.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/WorldSettings.h"
+
+#include "SandboxWorldSettings.generated.h"
+
+UCLASS(Blueprintable)
+class SANDBOX_API ASandboxWorldSettings : public AWorldSettings {
+    GENERATED_BODY()
+};
