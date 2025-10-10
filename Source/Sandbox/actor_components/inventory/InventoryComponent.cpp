1b7832bce30be84eeefe6dfe654fb6800c7d583b
Matthew Love <matthewlove94@gmail.com>
2025-10-10 16:32:33 +0100

    Add stub item_fits function

--- Source/Sandbox/actor_components/inventory/InventoryComponent.cpp
+++ Source/Sandbox/actor_components/inventory/InventoryComponent.cpp
@@ -3,7 +3,14 @@
 UInventoryComponent::UInventoryComponent() {}
 
 bool UInventoryComponent::add_item(TScriptInterface<IInventoryItem> item) {
+    if (!item_fits(*item)) {
+        return false;
+    }
+
     slots.Add({item});
 
+    return true;
+}
+bool UInventoryComponent::item_fits(IInventoryItem const& item) const {
     return true;
 }
