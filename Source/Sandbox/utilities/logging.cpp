10c01a629fa53c93e04c9099678837f0187ac35b
Matthew Love <matthewlove94@gmail.com>
2025-10-10 13:28:36 +0100

    Change to new UE string casting method

--- Source/Sandbox/utilities/logging.cpp
+++ Source/Sandbox/utilities/logging.cpp
@@ -4,8 +4,8 @@ namespace ml {
 void log_value_is_x(FString const& var_name,
                     FString const& null_form,
                     std::source_location const loc) {
-    FString const fn_name{ANSI_TO_TCHAR(loc.function_name())};
-    FString const file_name{ANSI_TO_TCHAR(loc.file_name())};
+    FString const fn_name{StringCast<TCHAR>(loc.function_name())};
+    FString const file_name{StringCast<TCHAR>(loc.file_name())};
     UE_LOGFMT(LogTemp,
               Warning,
               "{0} is {1}. [{2}, {3}, {4}]",
