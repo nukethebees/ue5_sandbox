fb3de30e25d764208724cb3850349bc418a7cd5b
Matthew Love <matthewlove94@gmail.com>
2025-10-10 10:50:34 +0100

    Make a logging var-is-null function to simplify macros

--- Source/Sandbox/macros/null_checks.hpp
+++ Source/Sandbox/macros/null_checks.hpp
@@ -1,62 +1,41 @@
-#include <source_location>
+#include "Sandbox/utilities/logging.h"
 
-#define RETURN_IF_NULLPTR(PTR_NAME)                                    \
-    do {                                                               \
-        if (!PTR_NAME) {                                               \
-            auto const loc{std::source_location::current()};           \
-            FString const fn_name{ANSI_TO_TCHAR(loc.function_name())}; \
-            FString const file_name{ANSI_TO_TCHAR(loc.file_name())};   \
-            UE_LOGFMT(LogTemp,                                         \
-                      Warning,                                         \
-                      #PTR_NAME " is nullptr. [{0}, {1}, {2}]",        \
-                      *fn_name,                                        \
-                      loc.line(),                                      \
-                      *file_name);                                     \
-            return;                                                    \
-        }                                                              \
+#define RETURN_VALUE_IF_BANG_VAR_BASE_MACRO(VAR_NAME, NULL_FORM, RETURN_VALUE) \
+    do {                                                                       \
+        if (!VAR_NAME) {                                                       \
+            ml::log_value_is_x(TEXT(#VAR_NAME), NULL_FORM);                    \
+            return RETURN_VALUE;                                               \
+        }                                                                      \
     } while (0)
+
+#define RETURN_VALUE_IF_NULLPTR(PTR_NAME, VALUE) \
+    RETURN_VALUE_IF_BANG_VAR_BASE_MACRO(PTR_NAME, TEXT("nullptr"), VALUE)
+
+#define RETURN_IF_BANG_VAR_BASE_MACRO(VAR_NAME, NULL_FORM) \
+    RETURN_VALUE_IF_BANG_VAR_BASE_MACRO(VAR_NAME, NULL_FORM, )
+
+#define RETURN_IF_NULLPTR(PTR_NAME) RETURN_IF_BANG_VAR_BASE_MACRO(PTR_NAME, TEXT("nullptr"))
 
 // Initialse a pointer and return early with a log message if it's null
 #define TRY_INIT_PTR(PTR_NAME, PTR_EXPR) \
     auto PTR_NAME{PTR_EXPR};             \
     RETURN_IF_NULLPTR(PTR_NAME)
 
-// TRY_INIT_PTR but for Behaviour Tree tasks
-#define TRY_INIT_BTTASK_PTR(PTR_NAME, PTR_EXPR)                \
-    auto PTR_NAME{PTR_EXPR};                                   \
-    if (!PTR_NAME) {                                           \
-        UE_LOGFMT(LogTemp, Warning, #PTR_NAME " is nullptr."); \
-        return EBTNodeResult::Failed;                          \
-    }
-
-// With return value
-#define RETURN_VALUE_IF_NULLPTR(PTR_NAME, VALUE)                       \
-    do {                                                               \
-        if (!PTR_NAME) {                                               \
-            auto const loc{std::source_location::current()};           \
-            FString const fn_name{ANSI_TO_TCHAR(loc.function_name())}; \
-            FString const file_name{ANSI_TO_TCHAR(loc.file_name())};   \
-            UE_LOGFMT(LogTemp,                                         \
-                      Warning,                                         \
-                      #PTR_NAME " is nullptr. [{0}, {1}, {2}]",        \
-                      *fn_name,                                        \
-                      loc.line(),                                      \
-                      *file_name);                                     \
-            return VALUE;                                              \
-        }                                                              \
-    } while (0)
-
 #define INIT_PTR_OR_RETURN_VALUE(PTR_NAME, PTR_EXPR, RETURN_VALUE) \
     auto PTR_NAME{PTR_EXPR};                                       \
     RETURN_VALUE_IF_NULLPTR(PTR_NAME, RETURN_VALUE)
 
+// TRY_INIT_PTR but for Behaviour Tree tasks
+#define TRY_INIT_BTTASK_PTR(PTR_NAME, PTR_EXPR) \
+    INIT_PTR_OR_RETURN_VALUE(PTR_NAME, PTR_EXPR, EBTNodeResult::Failed)
+
 // For objects that require calls to IsValid()
-#define RETURN_IF_INVALID(VAR_NAME)                                  \
-    do {                                                             \
-        if (!VAR_NAME.IsValid()) {                                   \
-            UE_LOGFMT(LogTemp, Warning, #VAR_NAME " is not valid."); \
-            return;                                                  \
-        }                                                            \
+#define RETURN_IF_INVALID(VAR_NAME)                               \
+    do {                                                          \
+        if (!VAR_NAME.IsValid()) {                                \
+            ml::log_value_is_x(TEXT(#VAR_NAME), TEXT("invalid")); \
+            return;                                               \
+        }                                                         \
     } while (0)
 
 #define TRY_INIT_VALID(VAR_NAME, VAR_EXPR) \
@@ -64,13 +43,7 @@
     RETURN_IF_INVALID(VAR_NAME)
 
 // std::optional
-#define RETURN_IF_NULLOPT(VAR_NAME)                                \
-    do {                                                           \
-        if (!VAR_NAME) {                                           \
-            UE_LOGFMT(LogTemp, Warning, #VAR_NAME " is nullopt."); \
-            return;                                                \
-        }                                                          \
-    } while (0)
+#define RETURN_IF_NULLOPT(VAR_NAME) RETURN_IF_BANG_VAR_BASE_MACRO(VAR_NAME, TEXT("nullopt"))
 
 #define TRY_INIT_OPTIONAL(VAR_NAME, VAR_EXPR) \
     auto VAR_NAME{VAR_EXPR};                  \
