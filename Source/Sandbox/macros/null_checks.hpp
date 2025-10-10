a6847262ed4ac172a0b71c3ad51a1417e89f7d28
Matthew Love <matthewlove94@gmail.com>
2025-10-10 11:13:22 +0100

    Update null check undef file

--- Source/Sandbox/macros/null_checks.hpp
+++ Source/Sandbox/macros/null_checks.hpp
@@ -1,47 +1,51 @@
 #include "Sandbox/utilities/logging.h"
 
-#define RETURN_VALUE_IF_EXPR_BASE_MACRO(VAR_NAME, BOOL_EXPR, NULL_FORM, RETURN_VALUE) \
-    do {                                                                              \
-        if (BOOL_EXPR) {                                                              \
-            ml::log_value_is_x(TEXT(#VAR_NAME), NULL_FORM);                           \
-            return RETURN_VALUE;                                                      \
-        }                                                                             \
+// Macros for testing if variables are in a valid state
+// If not, the function returns early and logs a warning
+// Mainly used for null checking pointers
+
+// INIT macros initialise variables and then perform a null checks
+
+#define RETURN_VALUE_AND_WARN_IF_EXPR(VAR_NAME, BOOL_EXPR, NULL_FORM, RETURN_VALUE) \
+    do {                                                                            \
+        if (BOOL_EXPR) {                                                            \
+            ml::log_value_is_x(TEXT(#VAR_NAME), TEXT(NULL_FORM));                   \
+            return RETURN_VALUE;                                                    \
+        }                                                                           \
     } while (0)
 
-#define RETURN_VALUE_IF_BANG_VAR_BASE_MACRO(VAR_NAME, NULL_FORM, RETURN_VALUE) \
-    RETURN_VALUE_IF_EXPR_BASE_MACRO(VAR_NAME, !VAR_NAME, NULL_FORM, RETURN_VALUE)
+// Pointers
+// -------------------------------------------------------------------------------------------------
+#define RETURN_IF_NULLPTR(VAR_NAME) RETURN_VALUE_AND_WARN_IF_EXPR(VAR_NAME, !VAR_NAME, "nullptr", )
 
-#define RETURN_VALUE_IF_NULLPTR(PTR_NAME, VALUE) \
-    RETURN_VALUE_IF_BANG_VAR_BASE_MACRO(PTR_NAME, TEXT("nullptr"), VALUE)
+#define RETURN_VALUE_IF_NULLPTR(VAR_NAME, RETURN_VALUE) \
+    RETURN_VALUE_AND_WARN_IF_EXPR(VAR_NAME, !VAR_NAME, "nullptr", RETURN_VALUE)
 
-#define RETURN_IF_BANG_VAR_BASE_MACRO(VAR_NAME, NULL_FORM) \
-    RETURN_VALUE_IF_BANG_VAR_BASE_MACRO(VAR_NAME, NULL_FORM, )
+#define TRY_INIT_PTR(VAR_NAME, PTR_EXPR) \
+    auto VAR_NAME{PTR_EXPR};             \
+    RETURN_IF_NULLPTR(VAR_NAME)
 
-#define RETURN_IF_NULLPTR(PTR_NAME) RETURN_IF_BANG_VAR_BASE_MACRO(PTR_NAME, TEXT("nullptr"))
+#define INIT_PTR_OR_RETURN_VALUE(VAR_NAME, PTR_EXPR, RETURN_VALUE) \
+    auto VAR_NAME{PTR_EXPR};                                       \
+    RETURN_VALUE_IF_NULLPTR(VAR_NAME, RETURN_VALUE)
 
-// Initialse a pointer and return early with a log message if it's null
-#define TRY_INIT_PTR(PTR_NAME, PTR_EXPR) \
-    auto PTR_NAME{PTR_EXPR};             \
-    RETURN_IF_NULLPTR(PTR_NAME)
+// Behaviour Tree tasks
+// -------------------------------------------------------------------------------------------------
+#define TRY_INIT_BTTASK_PTR(VAR_NAME, PTR_EXPR) \
+    INIT_PTR_OR_RETURN_VALUE(VAR_NAME, PTR_EXPR, EBTNodeResult::Failed)
 
-#define INIT_PTR_OR_RETURN_VALUE(PTR_NAME, PTR_EXPR, RETURN_VALUE) \
-    auto PTR_NAME{PTR_EXPR};                                       \
-    RETURN_VALUE_IF_NULLPTR(PTR_NAME, RETURN_VALUE)
-
-// TRY_INIT_PTR but for Behaviour Tree tasks
-#define TRY_INIT_BTTASK_PTR(PTR_NAME, PTR_EXPR) \
-    INIT_PTR_OR_RETURN_VALUE(PTR_NAME, PTR_EXPR, EBTNodeResult::Failed)
-
-// For objects that require calls to IsValid()
+// Objects with IsValid()
+// -------------------------------------------------------------------------------------------------
 #define RETURN_IF_INVALID(VAR_NAME) \
-    RETURN_VALUE_IF_EXPR_BASE_MACRO(VAR_NAME, !VAR_NAME.IsValid(), TEXT("invalid"), )
+    RETURN_VALUE_AND_WARN_IF_EXPR(VAR_NAME, !VAR_NAME.IsValid(), "invalid", )
 
 #define TRY_INIT_VALID(VAR_NAME, VAR_EXPR) \
     auto VAR_NAME{VAR_EXPR};               \
     RETURN_IF_INVALID(VAR_NAME)
 
 // std::optional
-#define RETURN_IF_NULLOPT(VAR_NAME) RETURN_IF_BANG_VAR_BASE_MACRO(VAR_NAME, TEXT("nullopt"))
+// -------------------------------------------------------------------------------------------------
+#define RETURN_IF_NULLOPT(VAR_NAME) RETURN_VALUE_AND_WARN_IF_EXPR(VAR_NAME, !VAR_NAME, "nullopt", )
 
 #define TRY_INIT_OPTIONAL(VAR_NAME, VAR_EXPR) \
     auto VAR_NAME{VAR_EXPR};                  \
