73adb0677324e8e140d5b56313efefc3046f4f9b
Matthew Love <matthewlove94@gmail.com>
2025-10-13 15:48:19 +0100

    Add test machine gun

---
+++ Source/Sandbox/actors/weapons/TestMachineGun.cpp
@@ -0,0 +1,121 @@
+#include "Sandbox/actors/weapons/TestMachineGun.h"
+
+#include "Components/ArrowComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+#include "Sandbox/subsystems/world/MassBulletSubsystem.h"
+
+#include "Sandbox/macros/null_checks.hpp"
+
+ATestMachineGun::ATestMachineGun()
+    : ammo{max_ammo} {
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+    RootComponent->SetMobility(EComponentMobility::Movable);
+
+    gun_mesh_component = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GunMeshComponent"));
+    gun_mesh_component->SetupAttachment(RootComponent);
+
+    fire_point_arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("FirePoint"));
+    fire_point_arrow->SetupAttachment(RootComponent);
+}
+
+bool ATestMachineGun::can_fire() const {
+    return ammo > 0;
+}
+
+bool ATestMachineGun::can_reload() const {
+    return ammo < max_ammo;
+}
+
+int32 ATestMachineGun::get_ammo_needed() const {
+    return max_ammo - ammo;
+}
+
+FAmmoData ATestMachineGun::get_current_ammo() const {
+    return FAmmoData::make_discrete(EAmmoType::Bullets, ammo);
+}
+
+FAmmoData ATestMachineGun::get_max_ammo() const {
+    return FAmmoData::make_discrete(EAmmoType::Bullets, max_ammo);
+}
+
+FAmmoReloadResult ATestMachineGun::reload(FAmmoData const& offered) {
+    FAmmoReloadResult result;
+    result.AmmoOfferedRemaining = offered;
+
+    if (offered.type != get_ammo_type()) {
+        return result;
+    }
+
+    if (!can_reload()) {
+        return result;
+    }
+
+    auto const needed{get_ammo_needed()};
+    auto const taken{FMath::Min(needed, offered.discrete_amount)};
+
+    ammo += taken;
+
+    result.AmmoTaken = FAmmoData::make_discrete(EAmmoType::Bullets, taken);
+    result.AmmoOfferedRemaining.discrete_amount -= taken;
+
+    return result;
+}
+
+void ATestMachineGun::start_firing() {
+    is_firing = true;
+    time_since_last_shot = 0.0f;
+}
+
+void ATestMachineGun::sustain_firing(float delta_time) {
+    if (!is_firing) {
+        return;
+    }
+
+    if (!can_fire()) {
+        return;
+    }
+
+    time_since_last_shot += delta_time;
+    auto const seconds_per_bullet{1.0f / fire_rate};
+
+    while (time_since_last_shot >= seconds_per_bullet && can_fire()) {
+        time_since_last_shot -= seconds_per_bullet;
+        fire_single_bullet(time_since_last_shot);
+    }
+}
+
+void ATestMachineGun::stop_firing() {
+    is_firing = false;
+    time_since_last_shot = 0.0f;
+}
+
+void ATestMachineGun::fire_single_bullet(float travel_time) {
+    RETURN_IF_NULLPTR(fire_point_arrow);
+    TRY_INIT_PTR(world, GetWorld());
+    TRY_INIT_PTR(mass_bullet_subsystem, world->GetSubsystem<UMassBulletSubsystem>());
+
+    constexpr float bullet_speed{5000.0f};
+
+    FTransform const spawn_transform{get_spawn_transform(travel_time)};
+    mass_bullet_subsystem->add_bullet(spawn_transform, bullet_speed);
+
+    ammo -= 1;
+}
+
+FTransform ATestMachineGun::get_spawn_transform(float travel_time) const {
+    auto const spawn_rotation{fire_point_arrow->GetComponentRotation()};
+    auto const travel_offset{spawn_rotation.Vector() * 5000.0f * travel_time};
+    auto const spawn_location{fire_point_arrow->GetComponentLocation() + travel_offset};
+    auto const spawn_scale{FVector::OneVector};
+
+    FTransform const spawn_transform{spawn_rotation, spawn_location, spawn_scale};
+
+    return spawn_transform;
+}
+
+UStaticMesh* ATestMachineGun::get_display_mesh() const {
+    RETURN_VALUE_IF_NULLPTR(gun_mesh_component, nullptr);
+    return gun_mesh_component->GetStaticMesh();
+}
---
+++ Source/Sandbox/actors/weapons/TestMachineGun.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Sandbox/actors/weapons/WeaponBase.h"
+#include "Sandbox/SandboxLogCategories.h"
+
+#include "TestMachineGun.generated.h"
+
+class UStaticMeshComponent;
+class UArrowComponent;
+
+UCLASS()
+class SANDBOX_API ATestMachineGun : public AWeaponBase {
+    GENERATED_BODY()
+  public:
+    ATestMachineGun();
+
+    virtual void start_firing() override;
+    virtual void sustain_firing(float delta_time) override;
+    virtual void stop_firing() override;
+    virtual bool can_fire() const override;
+
+    virtual FAmmoReloadResult reload(FAmmoData const& offered) override;
+    virtual bool can_reload() const override;
+
+    virtual EAmmoType get_ammo_type() const override { return EAmmoType::Bullets; }
+    virtual FAmmoData get_current_ammo() const override;
+    virtual FAmmoData get_max_ammo() const override;
+    int32 get_ammo_needed() const;
+
+    virtual UStaticMesh* get_display_mesh() const override;
+    virtual FString const& get_name() const {
+        static FString const default_name{TEXT("ATestMachineGun")};
+        return default_name;
+    };
+  protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gun")
+    int32 max_ammo{50};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gun")
+    int32 ammo{0};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gun")
+    float fire_rate{10.0f};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gun")
+    UStaticMeshComponent* gun_mesh_component{nullptr};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gun")
+    UArrowComponent* fire_point_arrow{nullptr};
+  private:
+    void fire_single_bullet(float travel_time);
+    FTransform get_spawn_transform(float travel_time) const;
+
+    bool is_firing{false};
+    float time_since_last_shot{0.0f};
+};
