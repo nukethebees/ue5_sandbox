8c4a83b8f075ae7b526f6739271df49dfeaf3d04
Matthew Love <matthewlove94@gmail.com>
2025-10-14 11:05:17 +0100

    Make character killable

--- Source/Sandbox/characters/SimpleCharacter.cpp
+++ Source/Sandbox/characters/SimpleCharacter.cpp
@@ -1,9 +1,14 @@
 #include "Sandbox/characters/SimpleCharacter.h"
 
+#include "Sandbox/actor_components/HealthComponent.h"
 #include "Sandbox/ai_controllers/SimpleAIController.h"
 
-ASimpleCharacter::ASimpleCharacter() {
+ASimpleCharacter::ASimpleCharacter()
+    : health(CreateDefaultSubobject<UHealthComponent>(TEXT("Health"))) {
     PrimaryActorTick.bCanEverTick = false;
 
     AIControllerClass = ASimpleAIController::StaticClass();
+}
+void ASimpleCharacter::handle_death() {
+    SetLifeSpan(0.1f);
 }
