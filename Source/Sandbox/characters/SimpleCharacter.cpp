6c948c784ee246a932eb6133731ff52f37e31277
Matthew Love <matthewlove94@gmail.com>
2025-10-14 11:32:43 +0100

    Make AI controller configurable

--- Source/Sandbox/characters/SimpleCharacter.cpp
+++ Source/Sandbox/characters/SimpleCharacter.cpp
@@ -7,8 +7,22 @@ ASimpleCharacter::ASimpleCharacter()
     : health(CreateDefaultSubobject<UHealthComponent>(TEXT("Health"))) {
     PrimaryActorTick.bCanEverTick = false;
 
-    AIControllerClass = ASimpleAIController::StaticClass();
+    AutoPossessAI = EAutoPossessAI::Spawned;
+    if (controller_class) {
+        AIControllerClass = controller_class;
+    } else {
+        AIControllerClass = ASimpleAIController::StaticClass();
+    }
 }
+
+void ASimpleCharacter::BeginPlay() {
+    Super::BeginPlay();
+
+    if (!Controller && AIControllerClass) {
+        SpawnDefaultController();
+    }
+}
+
 void ASimpleCharacter::handle_death() {
     SetLifeSpan(0.1f);
 }
