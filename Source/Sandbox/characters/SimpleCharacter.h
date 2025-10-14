c847805a892437f12bc581de389770c82724b475
Matthew Love <matthewlove94@gmail.com>
2025-10-14 11:05:17 +0100

    Make character killable

--- Source/Sandbox/characters/SimpleCharacter.h
+++ Source/Sandbox/characters/SimpleCharacter.h
@@ -3,15 +3,27 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
+
+#include "Sandbox/interfaces/DeathHandler.h"
 #include "Sandbox/mixins/log_msg_mixin.hpp"
+#include "Sandbox/SandboxLogCategories.h"
 
 #include "SimpleCharacter.generated.h"
+
+class UHealthComponent;
 
 UCLASS()
 class SANDBOX_API ASimpleCharacter
     : public ACharacter
-    , public ml::LogMsgMixin<"ASimpleCharacter"> {
+    , public ml::LogMsgMixin<"ASimpleCharacter", LogSandboxCharacter>
+    , public IDeathHandler {
     GENERATED_BODY()
   public:
     ASimpleCharacter();
+  protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    UHealthComponent* health{nullptr};
+  private:
+    // IDeathHandler
+    virtual void handle_death() override;
 };
