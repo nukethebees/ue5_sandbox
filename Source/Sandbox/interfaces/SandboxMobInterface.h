443836ad32d92b9c942427732cabc99e94f956de
Matthew Love <matthewlove94@gmail.com>
2025-10-14 16:57:28 +0100

    Add attack radius

--- Source/Sandbox/interfaces/SandboxMobInterface.h
+++ Source/Sandbox/interfaces/SandboxMobInterface.h
@@ -11,11 +11,10 @@ class USandboxMobInterface : public UInterface {
     GENERATED_BODY()
 };
 
-class SANDBOX_API ISandboxMobInterface{GENERATED_BODY() public: virtual UBehaviorTree *
-                                       get_behaviour_tree_asset() const {return nullptr;
-}
-virtual float get_acceptable_radius() const {
-    return 100.0f;
-}
-}
-;
+class SANDBOX_API ISandboxMobInterface {
+    GENERATED_BODY()
+  public:
+    virtual UBehaviorTree* get_behaviour_tree_asset() const { return nullptr; }
+    virtual float get_acceptable_radius() const { return 100.0f; }
+    virtual float get_attack_radius() const { return 100.0f; }
+};
