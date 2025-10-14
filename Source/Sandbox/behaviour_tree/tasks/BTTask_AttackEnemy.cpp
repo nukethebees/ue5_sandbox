ce9e7dfc5726142fdf3d8c29f6b135191a3f95fc
Matthew Love <matthewlove94@gmail.com>
2025-10-14 17:17:15 +0100

    Add empty attack node

---
+++ Source/Sandbox/behaviour_tree/tasks/BTTask_AttackEnemy.cpp
@@ -0,0 +1,27 @@
+#include "Sandbox/behaviour_tree/tasks/BTTask_AttackEnemy.h"
+
+#include "AIController.h"
+#include "Navigation/PathFollowingComponent.h"
+#include "NavigationSystem.h"
+
+#include "Sandbox/utilities/navigation.h"
+
+#include "Sandbox/macros/null_checks.hpp"
+
+UBTTask_AttackEnemy::UBTTask_AttackEnemy() {
+    NodeName = TEXT("Attack enemy");
+
+    // This task can be aborted if higher priority behavior needs to run
+    bNotifyTick = false;
+    bNotifyTaskFinished = true;
+}
+
+EBTNodeResult::Type UBTTask_AttackEnemy::ExecuteTask(UBehaviorTreeComponent& owner_comp,
+                                                     uint8* node_memory) {
+    constexpr auto logger{NestedLogger<"ExecuteTask">()};
+
+    INIT_PTR_OR_RETURN_VALUE(ai_controller, owner_comp.GetAIOwner(), EBTNodeResult::Failed);
+    INIT_PTR_OR_RETURN_VALUE(pawn, ai_controller->GetPawn(), EBTNodeResult::Failed);
+
+    return EBTNodeResult::Failed;
+}
