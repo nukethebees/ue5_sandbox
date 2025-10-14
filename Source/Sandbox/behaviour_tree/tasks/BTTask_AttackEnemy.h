d11e35df1981e7c15475aec09d304c9638daed73
Matthew Love <matthewlove94@gmail.com>
2025-10-14 17:17:15 +0100

    Add empty attack node

---
+++ Source/Sandbox/behaviour_tree/tasks/BTTask_AttackEnemy.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/ValueOrBBKey.h"
+
+#include "Sandbox/mixins/log_msg_mixin.hpp"
+#include "Sandbox/SandboxLogCategories.h"
+
+#include "BTTask_AttackEnemy.generated.h"
+
+UCLASS()
+class SANDBOX_API UBTTask_AttackEnemy
+    : public UBTTaskNode
+    , public ml::LogMsgMixin<"UBTTask_AttackEnemy", LogSandboxAI> {
+    GENERATED_BODY()
+  public:
+    UBTTask_AttackEnemy();
+
+    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp,
+                                            uint8* NodeMemory) override;
+};
