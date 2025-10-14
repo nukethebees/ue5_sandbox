f72c716b976537689cf0bbf032d8d7f19225ddfd
Matthew Love <matthewlove94@gmail.com>
2025-10-14 11:03:28 +0100

    Add character category

--- Source/Sandbox/SandboxLogCategories.cpp
+++ Source/Sandbox/SandboxLogCategories.cpp
@@ -2,13 +2,16 @@
 
 DEFINE_LOG_CATEGORY(LogSandboxFrameCount);
 
+// Systems
 DEFINE_LOG_CATEGORY(LogSandboxCore);
 DEFINE_LOG_CATEGORY(LogSandboxUI);
 DEFINE_LOG_CATEGORY(LogSandboxMassEntity);
 DEFINE_LOG_CATEGORY(LogSandboxSubsystem);
 DEFINE_LOG_CATEGORY(LogSandboxActor);
+DEFINE_LOG_CATEGORY(LogSandboxCharacter);
 DEFINE_LOG_CATEGORY(LogSandboxActorComponent);
 
+// Gameplay
 DEFINE_LOG_CATEGORY(LogSandboxWeapon);
 DEFINE_LOG_CATEGORY(LogSandboxInput);
 DEFINE_LOG_CATEGORY(LogSandboxInventory);
--- Source/Sandbox/SandboxLogCategories.h
+++ Source/Sandbox/SandboxLogCategories.h
@@ -4,13 +4,16 @@
 
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxFrameCount, Log, All);
 
+// Systems
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxCore, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxUI, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxMassEntity, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxActor, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogSandboxCharacter, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxActorComponent, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxSubsystem, Log, All);
 
+// Gameplay
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxWeapon, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxInput, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSandboxInventory, Log, All);
