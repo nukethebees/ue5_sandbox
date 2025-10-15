4691117fa4533f4d708001e4f6a22ce2fe284657
Matthew Love <matthewlove94@gmail.com>
2025-10-15 15:04:03 +0100

    Add explosion actor

---
+++ Source/Sandbox/actors/Explosion.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NiagaraDataChannel.h"
+
+#include "Sandbox/data/health/HealthChange.h"
+#include "Sandbox/generated/strong_typedefs/NdcWriterIndex.h"
+#include "Sandbox/mixins/LogMsgMixin.hpp"
+#include "Sandbox/SandboxLogCategories.h"
+
+#include "Explosion.generated.h"
+
+UCLASS()
+class SANDBOX_API AExplosion
+    : public AActor
+    , public ml::LogMsgMixin<"AExplosion", LogSandboxActor> {
+    GENERATED_BODY()
+  public:
+    AExplosion();
+  protected:
+    virtual void BeginPlay() override;
+    virtual void Tick(float delta_time) override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosion")
+    FHealthChange damage_config{25.0f, EHealthChangeType::Damage};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosion")
+    float explosion_radius{300.0f};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosion")
+    float explosion_force{5000.0f};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosion")
+    TObjectPtr<UNiagaraDataChannelAsset> explosion_channel{};
+  private:
+    void explode();
+    FVector calculate_impulse(FVector target_location, float target_distance);
+
+    FNdcWriterIndex writer_index;
+};
