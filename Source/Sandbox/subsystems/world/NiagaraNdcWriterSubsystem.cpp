84455e4609ff17e0aa952ef636692e788941aeda
Matthew Love <matthewlove94@gmail.com>
2025-10-15 10:04:50 +0100

    Add some members and stub function impls

--- Source/Sandbox/subsystems/world/NiagaraNdcWriterSubsystem.cpp
+++ Source/Sandbox/subsystems/world/NiagaraNdcWriterSubsystem.cpp
@@ -1,1 +1,52 @@
 #include "Sandbox/subsystems/world/NiagaraNdcWriterSubsystem.h"
+
+#include "Misc/CoreDelegates.h"
+#include "NiagaraDataChannel.h"
+#include "NiagaraDataChannelPublic.h"
+
+#include "Sandbox/macros/null_checks.hpp"
+
+FNdcWriterIndex UNiagaraNdcWriterSubsystem::register_asset(UNiagaraDataChannelAsset& asset) {
+    auto const asset_name{asset.GetFName()};
+    if (auto i{asset_lookup_.Find(asset_name)}) {
+        return *i;
+    }
+
+    FNdcWriterIndex i{assets_.Emplace(&asset)};
+    asset_lookup_.Add(asset_name, i);
+
+    return i;
+}
+
+void UNiagaraNdcWriterSubsystem::Initialize(FSubsystemCollectionBase& collection) {
+    Super::Initialize(collection);
+}
+void UNiagaraNdcWriterSubsystem::Deinitialize() {
+    FCoreDelegates::OnEndFrame.RemoveAll(this);
+    Super::Deinitialize();
+}
+void UNiagaraNdcWriterSubsystem::OnWorldBeginPlay(UWorld& world) {
+    Super::OnWorldBeginPlay(world);
+    FCoreDelegates::OnEndFrame.AddUObject(this, &UNiagaraNdcWriterSubsystem::flush_ndc_writes);
+}
+
+void UNiagaraNdcWriterSubsystem::flush_ndc_writes() {}
+auto UNiagaraNdcWriterSubsystem::create_data_channel_writer(NdcAsset& asset, int32 n)
+    -> NdcWriter* {
+    INIT_PTR_OR_RETURN_VALUE(world, GetWorld(), nullptr);
+
+    constexpr bool visible_to_game{false};
+    constexpr bool visible_to_cpu{true};
+    constexpr bool visible_to_gpu{true};
+    FString debug_source{};
+
+    auto* writer{UNiagaraDataChannelLibrary::WriteToNiagaraDataChannel(world,
+                                                                       &asset,
+                                                                       search_parameters,
+                                                                       n,
+                                                                       visible_to_game,
+                                                                       visible_to_cpu,
+                                                                       visible_to_gpu,
+                                                                       debug_source)};
+    return writer;
+}
--- Source/Sandbox/subsystems/world/NiagaraNdcWriterSubsystem.h
+++ Source/Sandbox/subsystems/world/NiagaraNdcWriterSubsystem.h
@@ -1,11 +1,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "NiagaraDataChannelPublic.h"
 
+#include "Sandbox/generated/strong_typedefs/NdcWriterIndex.h"
 #include "Sandbox/mixins/log_msg_mixin.hpp"
 #include "Sandbox/SandboxLogCategories.h"
 
 #include "NiagaraNdcWriterSubsystem.generated.h"
+
+class UNiagaraDataChannelAsset;
+class UNiagaraDataChannelWriter;
 
 // Subsystem for writing to niagara data channels
 UCLASS()
@@ -13,4 +18,24 @@ class SANDBOX_API UNiagaraNdcWriterSubsystem
     : public UWorldSubsystem
     , public ml::LogMsgMixin<"UNiagaraNdcWriterSubsystem", LogSandboxSubsystem> {
     GENERATED_BODY()
+  public:
+    using NdcAsset = UNiagaraDataChannelAsset;
+    using NdcWriter = UNiagaraDataChannelWriter;
+
+    UNiagaraNdcWriterSubsystem() = default;
+
+    FNdcWriterIndex register_asset(UNiagaraDataChannelAsset& asset);
+  protected:
+    virtual void Initialize(FSubsystemCollectionBase& collection) override;
+    virtual void Deinitialize() override;
+    virtual void OnWorldBeginPlay(UWorld& world) override;
+  private:
+    void flush_ndc_writes();
+    NdcWriter* create_data_channel_writer(NdcAsset& asset, int32 n);
+
+
+    TArray<TWeakObjectPtr<UNiagaraDataChannelAsset>> assets_;
+    TMap<FName, FNdcWriterIndex> asset_lookup_;
+    UPROPERTY()
+    FNiagaraDataChannelSearchParameters search_parameters{};
 };
