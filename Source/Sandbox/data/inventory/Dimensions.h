dda543265b1e7b5eb99cf6a3d51f1cb0a7ed4323
Matthew Love <matthewlove94@gmail.com>
2025-10-17 12:37:14 +0100

    Add an area member function

--- Source/Sandbox/data/inventory/Dimensions.h
+++ Source/Sandbox/data/inventory/Dimensions.h
@@ -25,6 +25,8 @@ struct FDimensions {
 
     FIntPoint get_value() const { return value; }
 
+    auto area() const { return value.X * value.Y; }
+
     // Hash support for TMap/TSet
     friend uint32 GetTypeHash(FDimensions const& obj) { return GetTypeHash(obj.value); }
 };
