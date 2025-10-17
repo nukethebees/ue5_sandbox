c186cb50bb61498d5335219a8349db59aebcea3b
Matthew Love <matthewlove94@gmail.com>
2025-10-17 15:26:58 +0100

    Make dimensions member function and rename the variable to item_dimensions

--- Source/Sandbox/data/inventory/InventorySlot.h
+++ Source/Sandbox/data/inventory/InventorySlot.h
@@ -15,27 +15,33 @@ struct FInventorySlot {
     FInventorySlot() = default;
     FInventorySlot(TScriptInterface<IInventoryItem> item,
                    FStackSize stack_size,
-                   FDimensions dimensions,
+                   FDimensions item_dimensions,
                    FCoord origin)
         : item(item)
         , stack_size(stack_size)
-        , dimensions(dimensions)
+        , item_dimensions(item_dimensions)
         , origin(origin) {}
     FInventorySlot(TScriptInterface<IInventoryItem> item)
         : item(item)
         , stack_size(1)
-        , dimensions(item->get_size()) {}
+        , item_dimensions(item->get_size()) {}
 
-    auto area() const { return dimensions.area(); }
-    auto width() const { return is_rotated ? dimensions.y() : dimensions.x(); }
-    auto height() const { return is_rotated ? dimensions.x() : dimensions.y(); }
+    auto area() const { return item_dimensions.area(); }
+    auto width() const { return is_rotated ? item_dimensions.y() : item_dimensions.x(); }
+    auto height() const { return is_rotated ? item_dimensions.x() : item_dimensions.y(); }
+    auto dimensions() const {
+        if (is_rotated) {
+            return FDimensions{item_dimensions.y(), item_dimensions.x()};
+        }
+        return item_dimensions;
+    }
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
     TScriptInterface<IInventoryItem> item{nullptr};
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
     FStackSize stack_size{1};
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
-    FDimensions dimensions{1, 1};
+    FDimensions item_dimensions{1, 1};
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
     FCoord origin{0, 0};
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
