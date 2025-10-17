e36aebcb0e3fb0d08c4132759a95c9392f67fe22
Matthew Love <matthewlove94@gmail.com>
2025-10-17 13:06:57 +0100

    Add members for origin and rotation

--- Source/Sandbox/data/inventory/InventorySlot.h
+++ Source/Sandbox/data/inventory/InventorySlot.h
@@ -15,19 +15,29 @@ struct FInventorySlot {
     FInventorySlot() = default;
     FInventorySlot(TScriptInterface<IInventoryItem> item,
                    FStackSize stack_size,
-                   FDimensions dimensions)
+                   FDimensions dimensions,
+                   FCoord origin)
         : item(item)
         , stack_size(stack_size)
-        , dimensions(dimensions) {}
+        , dimensions(dimensions)
+        , origin(origin) {}
     FInventorySlot(TScriptInterface<IInventoryItem> item)
         : item(item)
         , stack_size(1)
         , dimensions(item->get_size()) {}
 
-    UPROPERTY()
+    auto area() const { return dimensions.area(); }
+    auto width() const { return is_rotated ? dimensions.y() : dimensions.x(); }
+    auto height() const { return is_rotated ? dimensions.x() : dimensions.y(); }
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
     TScriptInterface<IInventoryItem> item{nullptr};
-    UPROPERTY()
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
     FStackSize stack_size{1};
-    UPROPERTY()
-    FDimensions dimensions{};
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+    FDimensions dimensions{1, 1};
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+    FCoord origin{0, 0};
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+    bool is_rotated{false};
 };
