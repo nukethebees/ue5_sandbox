a0ed4cb740169e5a0d00da5470ecb404433ee12f
Matthew Love <matthewlove94@gmail.com>
2025-10-20 12:03:44 +0100

    Allow tags to be overridden

--- Source/Sandbox/mixins/LogMsgMixin.hpp
+++ Source/Sandbox/mixins/LogMsgMixin.hpp
@@ -10,16 +10,32 @@
 #define GLOBAL_CATEGORY log_category
 #define LOCAL_CATEGORY log_cat
 
-#define LOG_MSG_TO(VERBOSITY) \
-    log_to<ELogVerbosity::VERBOSITY, LOCAL_CATEGORY>(std::move(fmt), std::forward<Args>(args)...);
+#define LOG_MSG_TO(VERBOSITY)                                             \
+    self.log_to<ELogVerbosity::VERBOSITY, LOCAL_CATEGORY>(std::move(fmt), \
+                                                          std::forward<Args>(args)...);
 
-#define LOG_MSG(VERBOSITY) \
-    log_to<ELogVerbosity::VERBOSITY, GLOBAL_CATEGORY>(std::move(fmt), std::forward<Args>(args)...);
+#define LOG_MSG(VERBOSITY)                                                 \
+    self.log_to<ELogVerbosity::VERBOSITY, GLOBAL_CATEGORY>(std::move(fmt), \
+                                                           std::forward<Args>(args)...);
+
+template <typename T>
+concept HasTagMember = requires {
+    { T::tag.data() } -> std::same_as<TCHAR const*>;
+};
 
 namespace ml {
 template <StaticTCharString tag, auto& log_category_ = LogTemp>
 struct LogMsgMixin {
-    static constexpr auto* get_tag() { return tag.data(); }
+    template <typename Self>
+    constexpr TCHAR const* get_tag(this Self&& self) {
+        using T = std::remove_cvref_t<Self>;
+
+        if constexpr (HasTagMember<T>) {
+            return T::tag.data();
+        } else {
+            return tag.data();
+        }
+    }
     static constexpr auto& GLOBAL_CATEGORY{log_category_};
 
     template <StaticTCharString inner_tag>
@@ -35,13 +51,15 @@ struct LogMsgMixin {
         UE::Core::TCheckedFormatString<typename FString::FmtCharType,
                                        std::remove_cv_t<std::remove_reference_t<Args>>...>;
 
-    template <ELogVerbosity::Type verbosity, auto& LOCAL_CATEGORY, typename... Args>
+    template <ELogVerbosity::Type verbosity, auto& LOCAL_CATEGORY, typename... Args, typename Self>
         requires (verbosity < ELogVerbosity::NumVerbosity)
-    void log_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
-#define LOG_BRANCH(VERBOSITY)                                              \
-    if constexpr (verbosity == ELogVerbosity::VERBOSITY) {                 \
-        UE_LOG(LOCAL_CATEGORY, VERBOSITY, TEXT("%s: %s"), get_tag(), *msg) \
+    void log_to(this Self const& self, DecayedFormatString<Args...>&& fmt, Args&&... args) {
+
+#define LOG_BRANCH(VERBOSITY)                                                   \
+    if constexpr (verbosity == ELogVerbosity::VERBOSITY) {                      \
+        UE_LOG(LOCAL_CATEGORY, VERBOSITY, TEXT("%s: %s"), self.get_tag(), *msg) \
     }
+
 #define LOG_ELSE_BRANCH(VERBOSITY) else LOG_BRANCH(VERBOSITY)
 
         if constexpr (verbosity > LOCAL_CATEGORY.GetCompileTimeVerbosity()) {
@@ -52,6 +70,7 @@ struct LogMsgMixin {
             return;
         }
 
+        auto tagx{self.get_tag()};
         auto msg{FString::Printf(std::move(fmt), std::forward<Args>(args)...)};
 
         LOG_BRANCH(Fatal)
@@ -67,7 +86,7 @@ struct LogMsgMixin {
 
     template <ELogVerbosity::Type verbosity, typename... Args>
         requires (verbosity < ELogVerbosity::NumVerbosity)
-    void log(DecayedFormatString<Args...>&& fmt, Args&&... args) const {
+    void log(this auto const& self, DecayedFormatString<Args...>&& fmt, Args&&... args) {
         log_to<verbosity, GLOBAL_CATEGORY>(std::move(fmt), std::forward<Args>(args)...);
     }
 
@@ -81,28 +100,32 @@ struct LogMsgMixin {
     // Macro to generate logging functions
 #define LOG_X_FN(LOWERCASE, VERBOSITY)                                               \
     template <typename... Args>                                                      \
-    void log_##LOWERCASE(DecayedFormatString<Args...>&& fmt, Args&&... args) const { \
+    void log_##LOWERCASE(                                                            \
+        this auto const& self, DecayedFormatString<Args...>&& fmt, Args&&... args) { \
         LOG_MSG(VERBOSITY);                                                          \
     }
 
     // Macro to generate _to variant logging functions
-#define LOG_X_TO_FN(LOWERCASE, VERBOSITY)                                                 \
-    template <auto& LOCAL_CATEGORY, typename... Args>                                     \
-    void log_##LOWERCASE##_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const { \
-        LOG_MSG_TO(VERBOSITY);                                                            \
+#define LOG_X_TO_FN(LOWERCASE, VERBOSITY)                                            \
+    template <auto& LOCAL_CATEGORY, typename... Args>                                \
+    void log_##LOWERCASE##_to(                                                       \
+        this auto const& self, DecayedFormatString<Args...>&& fmt, Args&&... args) { \
+        LOG_MSG_TO(VERBOSITY);                                                       \
     }
 
     // Development-only variants
 #define LOG_X_FN_DEV(LOWERCASE, VERBOSITY)                                           \
     template <typename... Args>                                                      \
-    void log_##LOWERCASE(DecayedFormatString<Args...>&& fmt, Args&&... args) const { \
+    void log_##LOWERCASE(                                                            \
+        this auto const& self, DecayedFormatString<Args...>&& fmt, Args&&... args) { \
         LOG_DEV_CODE(LOG_MSG(VERBOSITY);)                                            \
     }
 
-#define LOG_X_TO_FN_DEV(LOWERCASE, VERBOSITY)                                             \
-    template <auto& LOCAL_CATEGORY, typename... Args>                                     \
-    void log_##LOWERCASE##_to(DecayedFormatString<Args...>&& fmt, Args&&... args) const { \
-        LOG_DEV_CODE(LOG_MSG_TO(VERBOSITY);)                                              \
+#define LOG_X_TO_FN_DEV(LOWERCASE, VERBOSITY)                                        \
+    template <auto& LOCAL_CATEGORY, typename... Args>                                \
+    void log_##LOWERCASE##_to(                                                       \
+        this auto const& self, DecayedFormatString<Args...>&& fmt, Args&&... args) { \
+        LOG_DEV_CODE(LOG_MSG_TO(VERBOSITY);)                                         \
     }
 
     // Generate all logging functions
