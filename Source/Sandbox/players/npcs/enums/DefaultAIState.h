cb314e864ed7d6cbad56e58b61d4ba235151425f
Matthew Love <matthewlove94@gmail.com>
2025-10-21 10:33:19 +0100

    Add enum for default state

---
+++ Source/Sandbox/players/npcs/enums/DefaultAIState.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "DefaultAIState.generated.h"
+
+UENUM(BlueprintType)
+enum class EDefaultAIState : uint8 {
+    Idle UMETA(DisplayName = "Idle"),
+    RandomlyMove UMETA(DisplayName = "RandomlyMove"),
+};
