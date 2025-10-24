df17b0ff74c328195ab236534324cd1c2b39bcd8
Matthew Love <matthewlove94@gmail.com>
2025-10-24 15:22:38 +0100

    Add increment/decrement functions

--- Source/Sandbox/utilities/grids.h
+++ Source/Sandbox/utilities/grids.h
@@ -12,4 +12,11 @@ inline auto to_1d_index(FDimensions grid, FCoord coord) -> int32 {
 inline auto to_coord(FDimensions grid, int32 index) -> FCoord {
     return FCoord{index % grid.width(), index / grid.width()};
 }
+inline auto decrement_index(int32 width, int32 index) -> int32 {
+    auto const next{index - 1};
+    return next < 0 ? (width - 1) : next;
+}
+inline auto increment_index(int32 width, int32 index) -> int32 {
+    return (index + 1) % width;
+}
 }
