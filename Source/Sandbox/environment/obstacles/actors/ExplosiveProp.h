2a0d97f9a2b59c4bc956ffe058e30460a05546f0
Matthew Love <matthewlove94@gmail.com>
2025-10-24 17:13:58 +0100

    Add explosive barrel

---
+++ Source/Sandbox/environment/obstacles/actors/ExplosiveProp.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/Actor.h"
+
+#include "Sandbox/combat/effects/data/ExplosionConfig.h"
+#include "Sandbox/health/interfaces/DeathHandler.h"
+#include "Sandbox/logging/mixins/LogMsgMixin.hpp"
+#include "Sandbox/logging/SandboxLogCategories.h"
+
+#include "ExplosiveProp.generated.h"
+
+class UHealthComponent;
+
+UCLASS()
+class SANDBOX_API AExplosiveProp
+    : public AActor
+    , public IDeathHandler
+    , public ml::LogMsgMixin<"AExplosiveProp", LogSandboxActor> {
+    GENERATED_BODY()
+  public:
+    AExplosiveProp();
+
+    // IDeathHandler implementation
+    virtual void handle_death() override;
+  protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosive")
+    UStaticMeshComponent* mesh_component{};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosive")
+    UHealthComponent* health_component{};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosive")
+    FExplosionConfig explosion_config{};
+};
