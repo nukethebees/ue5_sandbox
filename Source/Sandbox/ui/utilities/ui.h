1442de8e453cf267d144139863bc57ded4616c5b
Matthew Love <matthewlove94@gmail.com>
2025-10-27 11:55:58 +0000

    Add get_screen_bounds

--- Source/Sandbox/ui/utilities/ui.h
+++ Source/Sandbox/ui/utilities/ui.h
@@ -2,6 +2,12 @@
 
 #include "CoreMinimal.h"
 
+#include "Sandbox/ui/data/ScreenBounds.h"
+
+class AActor;
+class APlayerController;
+
 namespace ml {
 int32 scale_font_to_umg(int32 slate_dpi);
+auto get_screen_bounds(APlayerController const& pc, AActor const& actor) -> FScreenBounds;
 }
