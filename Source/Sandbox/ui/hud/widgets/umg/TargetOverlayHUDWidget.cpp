58461f58cde9b33a7e8b53b26188217041c84db2
Matthew Love <matthewlove94@gmail.com>
2025-10-27 11:56:38 +0000

    Implement update function for outline

--- Source/Sandbox/ui/hud/widgets/umg/TargetOverlayHUDWidget.cpp
+++ Source/Sandbox/ui/hud/widgets/umg/TargetOverlayHUDWidget.cpp
@@ -1,1 +1,37 @@
 #include "Sandbox/ui/hud/widgets/umg/TargetOverlayHUDWidget.h"
+
+#include "Blueprint/SlateBlueprintLibrary.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Components/Image.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "Sandbox/ui/data/ScreenBounds.h"
+
+#include "Sandbox/utilities/macros/null_checks.hpp"
+
+void UTargetOverlayHUDWidget::update_target_screen_bounds(FScreenBounds const& bounds) {
+    constexpr auto logger{NestedLogger<"update_target_screen_bounds">()};
+
+    SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+
+    TRY_INIT_PTR(canvas_slot, Cast<UCanvasPanelSlot>(Slot));
+
+    auto* pc{UGameplayStatics::GetPlayerController(GetWorld(), 0)};
+    FVector2D viewport_position_2d;
+    FVector2D viewport_size_2d;
+
+    auto const padded_size{bounds.size + outline_padding};
+
+    USlateBlueprintLibrary::ScreenToViewport(pc, bounds.get_centre(), viewport_position_2d);
+    USlateBlueprintLibrary::ScreenToViewport(pc, bounds.size + outline_padding, viewport_size_2d);
+
+    canvas_slot->SetPosition(viewport_position_2d);
+    canvas_slot->SetSize(viewport_size_2d);
+}
+
+void UTargetOverlayHUDWidget::NativeOnInitialized() {
+    Super::NativeOnInitialized();
+
+    corners_ = {outline_top_left, outline_top_right, outline_bottom_left, outline_bottom_right};
+}
--- Source/Sandbox/ui/hud/widgets/umg/TargetOverlayHUDWidget.h
+++ Source/Sandbox/ui/hud/widgets/umg/TargetOverlayHUDWidget.h
@@ -3,14 +3,25 @@
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 
+#include "Sandbox/logging/mixins/LogMsgMixin.hpp"
+#include "Sandbox/logging/SandboxLogCategories.h"
+
 #include "TargetOverlayHUDWidget.generated.h"
 
 class UImage;
 
+struct FScreenBounds;
+
 UCLASS()
-class SANDBOX_API UTargetOverlayHUDWidget : public UUserWidget {
+class SANDBOX_API UTargetOverlayHUDWidget
+    : public UUserWidget
+    , public ml::LogMsgMixin<"UTargetOverlayHUDWidget", LogSandboxUI> {
     GENERATED_BODY()
+  public:
+    void update_target_screen_bounds(FScreenBounds const& bounds);
   protected:
+    virtual void NativeOnInitialized() override;
+
     UPROPERTY(meta = (BindWidget))
     UImage* outline_top_left{nullptr};
 
@@ -22,4 +33,9 @@ class SANDBOX_API UTargetOverlayHUDWidget : public UUserWidget {
 
     UPROPERTY(meta = (BindWidget))
     UImage* outline_bottom_right{nullptr};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HUD")
+    float outline_padding{100.0f};
+
+    TStaticArray<UImage*, 4> corners_;
 };
