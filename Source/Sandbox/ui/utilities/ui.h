06729306bc2b6fdb67d70c24ab9d4ac95503917f
Matthew Love <matthewlove94@gmail.com>
2025-10-28 12:02:26 +0000

    Remove log messages

--- Source/Sandbox/ui/utilities/ui.h
+++ Source/Sandbox/ui/utilities/ui.h
@@ -19,8 +19,6 @@ using UpdateBoundsFn = void (*)(APlayerController&, FVector, FVector2D&);
 
 template <detail::UpdateBoundsFn update_bounds>
 auto get_screen_bounds(APlayerController& pc, FActorCorners const& corners) -> FScreenBounds {
-    UE_LOG(LogSandboxUI, Verbose, TEXT("Corner data: %s"), *corners.to_string());
-
     auto corner_view{TArrayView<FVector const>(corners.data)};
 
     FVector2D top_left{1e9, 1e9};
@@ -35,12 +33,6 @@ auto get_screen_bounds(APlayerController& pc, FActorCorners const& corners) -> F
 
         bottom_right.X = std::max(bottom_right.X, screen_location.X);
         bottom_right.Y = std::max(bottom_right.Y, screen_location.Y);
-
-        UE_LOG(LogSandboxUI,
-               Verbose,
-               TEXT("TL: %s, BR: %s"),
-               *top_left.ToString(),
-               *bottom_right.ToString());
     }
 
     return {top_left, bottom_right - top_left};
