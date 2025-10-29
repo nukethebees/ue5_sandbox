266d0ef9a3353cc0e869b24f20623b520663098b
Matthew Love <matthewlove94@gmail.com>
2025-10-29 16:30:01 +0000

    Add functions to get num of each category

--- Source/Sandbox/combat/weapons/enums/AmmoType.h
+++ Source/Sandbox/combat/weapons/enums/AmmoType.h
@@ -18,6 +18,7 @@ enum class EAmmoType : uint8 {
     // Continuous ammo types (measurable)
     Energy UMETA(DisplayName = "Energy"),
     Plasma UMETA(DisplayName = "Plasma"),
+    CONTINUOUS_END UMETA(Hidden),
 };
 
 constexpr bool is_discrete(EAmmoType type) {
@@ -28,4 +29,14 @@ constexpr bool is_discrete(EAmmoType type) {
 
 constexpr bool is_continuous(EAmmoType type) {
     return !is_discrete(type);
+}
+
+constexpr int32 num_discrete_types(EAmmoType type) {
+    return static_cast<int32>(EAmmoType::DISCRETE_END);
+}
+constexpr int32 num_continuous_types(EAmmoType type) {
+    constexpr auto d_end{std::to_underlying(EAmmoType::DISCRETE_END)};
+    constexpr auto c_end{std::to_underlying(EAmmoType::CONTINUOUS_END)};
+    constexpr auto diff{c_end - d_end};
+    return diff - 1;
 }
