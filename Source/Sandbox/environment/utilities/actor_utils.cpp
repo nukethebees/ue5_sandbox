2fccbbc02025a0315990d2a4178f623a2bf2ec7e
Matthew Love <matthewlove94@gmail.com>
2025-10-29 11:49:45 +0000

    Limit corners to colliding components

--- Source/Sandbox/environment/utilities/actor_utils.cpp
+++ Source/Sandbox/environment/utilities/actor_utils.cpp
@@ -6,7 +6,7 @@ namespace ml {
 auto get_actor_corners(AActor const& actor) -> FActorCorners {
     FVector origin;
     FVector box_extent;
-    constexpr bool only_colliding_components{false};
+    constexpr bool only_colliding_components{true};
     constexpr bool include_from_child_actors{true};
     actor.GetActorBounds(only_colliding_components, origin, box_extent, include_from_child_actors);
 
