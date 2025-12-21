7ca48e6eb7706d8631ec43feb57e529133d5e256
Matthew Love <matthewlove94@gmail.com>
2025-12-21 20:48:37 +0000

    Set up switch for turret state

--- Source/Sandbox/environment/obstacles/actors/FloorTurret.cpp
+++ Source/Sandbox/environment/obstacles/actors/FloorTurret.cpp
@@ -1,5 +1,7 @@
 #include "Sandbox/environment/obstacles/actors/FloorTurret.h"
 
+#include "Sandbox/logging/SandboxLogCategories.h"
+
 AFloorTurret::AFloorTurret() {
     PrimaryActorTick.bCanEverTick = true;
     PrimaryActorTick.bStartWithTickEnabled = true;
@@ -7,6 +9,23 @@ AFloorTurret::AFloorTurret() {
 
 void AFloorTurret::Tick(float dt) {
     Super::Tick(dt);
+
+    switch (state) {
+        case EFloorTurretState::Disabled: {
+            SetActorTickEnabled(false);
+            break;
+        }
+        case EFloorTurretState::Watching: {
+            break;
+        }
+        case EFloorTurretState::Attacking: {
+            break;
+        }
+        default: {
+            UE_LOG(LogSandboxActor, Warning, TEXT("Unhandled enum state."));
+            break;
+        }
+    }
 }
 void AFloorTurret::set_state(EFloorTurretState new_state) {
     state = new_state;
