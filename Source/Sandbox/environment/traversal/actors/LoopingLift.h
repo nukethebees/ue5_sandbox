d80b175a9fc935f2c25871e51d3a2931b9343a16
Matthew Love <matthewlove94@gmail.com>
2025-12-27 22:25:21 +0000

    Remove unused header

--- Source/Sandbox/environment/traversal/actors/LoopingLift.h
+++ Source/Sandbox/environment/traversal/actors/LoopingLift.h
@@ -2,14 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Sandbox/logging/mixins/print_msg_mixin.hpp"
 
 #include "LoopingLift.generated.h"
 
 UCLASS()
-class SANDBOX_API ALoopingLift
-    : public AActor
-    , public print_msg_mixin {
+class SANDBOX_API ALoopingLift : public AActor {
     GENERATED_BODY()
   public:
     ALoopingLift();
