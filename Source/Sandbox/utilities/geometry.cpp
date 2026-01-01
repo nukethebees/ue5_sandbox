3afb38c4234904918f109173b76cdad753bd02cd
Matthew Love <matthewlove94@gmail.com>
2026-01-01 19:26:10 +0000

    Add functions for angles to actor

--- Source/Sandbox/utilities/geometry.cpp
+++ Source/Sandbox/utilities/geometry.cpp
@@ -1,6 +1,20 @@
 #include "Sandbox/utilities/geometry.h"
 
+#include "GameFramework/Actor.h"
+
 namespace ml {
+auto get_norm_vector_to_actor(AActor& actor, FVector point) -> FVector {
+    auto const fwd{actor.GetActorForwardVector()};
+    auto const pos{actor.GetActorLocation()};
+    return (point - pos).GetSafeNormal();
+}
+auto get_angle_to_actor(AActor& actor, FVector point) -> double {
+    auto const fwd{actor.GetActorForwardVector()};
+    auto const direction_to_point{get_norm_vector_to_actor(actor, point)};
+
+    return get_angle_between_norm_lines(fwd, direction_to_point);
+}
+
 auto subdivide_arc_into_segments(float starting_angle_deg, float arc_deg, int32 segments)
     -> TArray<float> {
     int32 const n_lines{segments + 1};
--- Source/Sandbox/utilities/geometry.h
+++ Source/Sandbox/utilities/geometry.h
@@ -3,12 +3,16 @@
 #include "CoreMinimal.h"
 #include "Math/UnrealMathUtility.h"
 
+class AActor;
+
 namespace ml {
 // Get angle (rads) between two lines from the same point
 // Written for vision calculations
 inline auto get_angle_between_norm_lines(FVector a, FVector b) -> double {
     return FMath::Acos(FVector::DotProduct(a, b));
 }
+auto get_norm_vector_to_actor(AActor& actor, FVector point) -> FVector;
+auto get_angle_to_actor(AActor& actor, FVector point) -> double;
 
 // 2 segments = 3 lines
 auto subdivide_arc_into_segments(float starting_angle_deg, float arc_deg, int32 segments)
