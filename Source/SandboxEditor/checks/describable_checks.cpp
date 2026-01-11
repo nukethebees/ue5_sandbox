3d680db96a7b387dfa0f31f0320fe9fb211dd173
Matthew Love <matthewlove94@gmail.com>
2026-01-11 14:11:50 +0000

    Implement function to find BPs implementing IDescribable

--- Source/SandboxEditor/checks/describable_checks.cpp
+++ Source/SandboxEditor/checks/describable_checks.cpp
@@ -1,9 +1,84 @@
 #include "SandboxEditor/checks/describable_checks.h"
 
+#include "AssetRegistry/ARFilter.h"
+#include "AssetRegistry/AssetData.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "AssetRegistry/IAssetRegistry.h"
+#include "Engine/Blueprint.h"
+#include "GameFramework/Actor.h"
+
+#include "Sandbox/environment/utilities/actor_utils.h"
+#include "Sandbox/interaction/interfaces/Describable.h"
 #include "SandboxEditor/logging/SandboxEditorLogCategories.h"
+
+#define LOG(VERBOSITY, MSG, ...) \
+    UE_LOG(LogSandboxEditorChecks, VERBOSITY, TEXT(MSG) __VA_OPT__(, ) __VA_ARGS__);
 
 namespace ml {
 void check_describable_actors_are_visible_to_hitscan() {
-    UE_LOG(LogSandboxEditorChecks, Log, TEXT("Checking IDescribable actors."));
+    LOG(Log, "Checking IDescribable actors.");
+
+    auto& asset_registry{
+        FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry").Get()};
+
+    FARFilter asset_reg_filter;
+    // Get all blueprints
+    asset_reg_filter.ClassPaths.Add(UBlueprint::StaticClass()->GetClassPathName());
+    asset_reg_filter.bRecursiveClasses = true;
+    asset_reg_filter.bRecursivePaths = true;
+
+    FString const game_dir{"/Game"};
+    TArray<FString> asset_dirs;
+    TArray<FString> to_exclude{TEXT("/UltraDynamicSky"), TEXT("/Fab")};
+    asset_registry.GetSubPaths(*game_dir, asset_dirs, false);
+    asset_dirs = asset_dirs.FilterByPredicate([&](FString const& fs) -> bool {
+        for (auto const& x : to_exclude) {
+            if ((fs == game_dir + x) || fs.StartsWith(game_dir + "/__")) {
+                return false;
+            }
+        }
+
+        return true;
+    });
+
+    if (asset_dirs.IsEmpty()) {
+        LOG(Warning, "No directories to scan.");
+        return;
+    }
+
+    LOG(Verbose, "Scanning %d directories", asset_dirs.Num());
+    for (auto const& asset_dir : asset_dirs) {
+        LOG(Verbose, "    %s", *asset_dir);
+        asset_reg_filter.PackagePaths.Add(*asset_dir);
+    }
+
+    TArray<FAssetData> blueprints;
+    asset_registry.GetAssets(asset_reg_filter, blueprints);
+
+    LOG(Verbose, "Found %d candidate BPs.", blueprints.Num());
+
+    int32 count{0};
+    for (auto const& asset : blueprints) {
+        LOG(VeryVerbose, "Checking blueprint: %s", *asset.GetFullName());
+
+        auto* bp{Cast<UBlueprint>(asset.GetAsset())};
+        if (!bp || !bp->GeneratedClass) {
+            continue;
+        }
+
+        auto* actor{Cast<AActor>(bp->GeneratedClass->GetDefaultObject())};
+        if (!actor) {
+            continue;
+        }
+        if (!Cast<IDescribable>(actor)) {
+            continue;
+        }
+
+        LOG(Verbose, "Found %s", *ml::get_best_display_name(*actor));
+
+        ++count;
+    }
+
+    LOG(Log, "Found %d BPs that implement IDescribable", count);
 }
 }
