a1199c3c61ed31ac87871bde973cd3dc67d7638e
Matthew Love <matthewlove94@gmail.com>
2026-03-17 11:18:38 +0000

    Make moving target

--- Source/Sandbox/players/ShipTrainingTarget.cpp
+++ Source/Sandbox/players/ShipTrainingTarget.cpp
@@ -1,8 +1,14 @@
 #include "Sandbox/players/ShipTrainingTarget.h"
 
+#include "Sandbox/constants/collision_channels.h"
+#include "Sandbox/logging/SandboxLogCategories.h"
+
 #include "Components/BoxComponent.h"
 #include "Components/SceneComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Engine/StaticMesh.h"
+
+#include "Sandbox/utilities/macros/null_checks.hpp"
 
 AShipTrainingTarget::AShipTrainingTarget()
     : mesh(CreateDefaultSubobject<UStaticMeshComponent>(TEXT("mesh")))
@@ -10,14 +16,48 @@ AShipTrainingTarget::AShipTrainingTarget()
     RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("root"));
 
     mesh->SetupAttachment(RootComponent);
-    collision_box->SetupAttachment(RootComponent);
+    mesh->SetMobility(EComponentMobility::Movable);
+    mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
-    PrimaryActorTick.bCanEverTick = false;
-    PrimaryActorTick.bStartWithTickEnabled = false;
+    collision_box->SetupAttachment(RootComponent);
+    collision_box->SetMobility(EComponentMobility::Movable);
+    collision_box->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    collision_box->SetCollisionResponseToChannel(ml::collision::projectile,
+                                                 ECollisionResponse::ECR_Block);
+
+    PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bStartWithTickEnabled = true;
+}
+
+void AShipTrainingTarget::Tick(float dt) {
+    Super::Tick(dt);
+
+    switch (movement_mode) {
+        using enum EShipTrainingTargetMovementMode;
+        case move_forward: {
+            SetActorLocation(GetActorLocation() + GetActorForwardVector() * velocity);
+            break;
+        }
+        default: {
+            UE_LOG(LogSandboxActor, Warning, TEXT("AShipTrainingTarget: Unhandled enum"));
+            [[fallthrough]];
+        }
+        case stationary: {
+            break;
+        }
+    }
 }
 
 auto AShipTrainingTarget::apply_damage(int32 damage, AActor const& instigator)
     -> FShipDamageResult {
     SetLifeSpan(0.01f);
     return FShipDamageResult{EDamageResult::Killed};
+}
+
+void AShipTrainingTarget::OnConstruction(FTransform const& transform) {
+    Super::OnConstruction(transform);
+
+    TRY_INIT_PTR(tgt_mesh, mesh->GetStaticMesh());
+    auto const bbox{tgt_mesh->GetBoundingBox()};
+    collision_box->SetBoxExtent(bbox.GetExtent());
 }
--- Source/Sandbox/players/ShipTrainingTarget.h
+++ Source/Sandbox/players/ShipTrainingTarget.h
@@ -10,6 +10,9 @@
 class UStaticMeshComponent;
 class UBoxComponent;
 
+UENUM()
+enum class EShipTrainingTargetMovementMode : uint8 { stationary, move_forward };
+
 UCLASS()
 class AShipTrainingTarget
     : public APawn
@@ -18,11 +21,19 @@ class AShipTrainingTarget
   public:
     AShipTrainingTarget();
 
+    void Tick(float dt) override;
+
     // IDamageableShip
     auto apply_damage(int32 damage, AActor const& instigator) -> FShipDamageResult override;
+  protected:
+    void OnConstruction(FTransform const& transform) override;
 
     UPROPERTY(EditAnywhere, Category = "Target")
     UStaticMeshComponent* mesh{nullptr};
     UPROPERTY(EditAnywhere, Category = "Target")
     UBoxComponent* collision_box{nullptr};
+    UPROPERTY(EditAnywhere, Category = "Target")
+    EShipTrainingTargetMovementMode movement_mode{EShipTrainingTargetMovementMode::stationary};
+    UPROPERTY(EditAnywhere, Category = "Target")
+    FVector3d velocity{FVector3d::ZeroVector};
 };
