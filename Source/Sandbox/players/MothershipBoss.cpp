408f80650bf93995c787383c75ed0508a73a47fd
Matthew Love <matthewlove94@gmail.com>
2026-03-22 14:57:32 +0000

    Add initial mothership

---
+++ Source/Sandbox/players/MothershipBoss.cpp
@@ -0,0 +1,113 @@
+#include "Sandbox/players/MothershipBoss.h"
+
+#include "Sandbox/logging/SandboxLogCategories.h"
+#include "Sandbox/players/DamageableShip.h"
+
+#include "Components/PointLightComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/StaticMesh.h"
+#include "Materials/MaterialInstanceDynamic.h"
+
+#include <type_traits>
+
+#include "Sandbox/utilities/macros/null_checks.hpp"
+
+AMothershipBoss::AMothershipBoss()
+    : mesh_component{CreateDefaultSubobject<UStaticMeshComponent>(TEXT("main_ship"))} {
+    PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bStartWithTickEnabled = true;
+
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("root"));
+
+    mesh_component->SetupAttachment(RootComponent);
+
+    add_n_components<n_hatches>(hatch_meshes, TEXT("hatch"));
+    add_n_components<n_search_lights>(search_light_meshes, TEXT("search_light"));
+    add_n_components<n_point_lights>(point_lights, TEXT("point_light"));
+}
+
+template <int32 N, typename T>
+void AMothershipBoss::add_n_components(TArray<T*>& components, FString const& name_base) {
+    for (int32 i{0}; i < N; i++) {
+        auto const name{FString::Printf(TEXT("%s_%d"), *name_base, i)};
+        auto* new_component{CreateDefaultSubobject<T>(*name)};
+        if (!new_component) {
+            WARN_IS_FALSE(LogSandboxActor, new_component);
+        }
+        new_component->SetupAttachment(mesh_component);
+        components.Add(new_component);
+    }
+}
+
+void AMothershipBoss::Tick(float dt) {
+    Super::Tick(dt);
+}
+void AMothershipBoss::BeginPlay() {
+    Super::BeginPlay();
+}
+void AMothershipBoss::OnConstruction(FTransform const& transform) {
+    Super::OnConstruction(transform);
+
+    UE_LOG(LogSandboxActor, Log, TEXT("AMothershipBoss::OnConstruction"));
+
+    RETURN_IF_NULLPTR(mesh_component);
+    TRY_INIT_PTR(mesh, mesh_component->GetStaticMesh());
+
+    attach_components<n_hatches>(*mesh_component, *mesh, hatch_meshes, TEXT("Hatch"));
+    attach_components<n_search_lights>(
+        *mesh_component, *mesh, search_light_meshes, TEXT("SearchLight"));
+    attach_components<n_point_lights>(*mesh_component, *mesh, point_lights, TEXT("ShipLight"));
+
+    set_meshes(hatch_mesh, hatch_meshes);
+    set_meshes(search_light_mesh, search_light_meshes);
+
+    if (search_light_mesh_material_base) {
+        light_material_instances.Empty();
+        for (auto* c : search_light_meshes) {
+            auto* new_material{
+                UMaterialInstanceDynamic::Create(search_light_mesh_material_base, this)};
+            CONTINUE_IF_NULLPTR(new_material);
+            light_material_instances.Add(new_material);
+            c->SetMaterial(0, new_material);
+
+            auto const light_origin{c->GetComponentLocation()};
+            new_material->SetVectorParameterValue(TEXT("light_origin"), light_origin);
+        }
+    }
+}
+template <int32 N, typename T>
+void AMothershipBoss::attach_components(UStaticMeshComponent& parent,
+                                        UStaticMesh& parent_mesh,
+                                        TArray<T*>& components,
+                                        FString const& socket_base) {
+    auto const n_components{components.Num()};
+    if (n_components != N) {
+        UE_LOG(LogSandboxActor, Warning, TEXT("Incorrect number of components"));
+        return;
+    }
+
+    auto const rules{FAttachmentTransformRules::SnapToTargetNotIncludingScale};
+
+    for (int32 i{0}; i < N; i++) {
+        auto const socket_name{FString::Printf(TEXT("%s_%d"), *socket_base, i)};
+        auto* socket{parent_mesh.FindSocket(*socket_name)};
+        CONTINUE_IF_NULLPTR(socket);
+        auto* component{components[i]};
+        CONTINUE_IF_NULLPTR(component);
+        component->AttachToComponent(&parent, rules, *socket_name);
+    }
+}
+void AMothershipBoss::set_meshes(UStaticMesh* mesh, TArray<UStaticMeshComponent*>& components) {
+    RETURN_IF_NULLPTR(mesh);
+    for (auto* c : components) {
+        c->SetStaticMesh(mesh);
+    }
+}
+void AMothershipBoss::config_point_lights() {
+    for (auto* pl : point_lights) {
+        CONTINUE_IF_NULLPTR(pl);
+        pl->SetLightColor(point_light_colour);
+        pl->SetLightBrightness(point_light_brightness);
+    }
+}
---
+++ Source/Sandbox/players/MothershipBoss.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "MothershipBoss.generated.h"
+
+class UStaticMeshComponent;
+class UStaticMesh;
+class UPointLightComponent;
+class UMaterialInstance;
+class UMaterialInstanceDynamic;
+
+UCLASS()
+class AMothershipBoss : public AActor {
+    GENERATED_BODY()
+  public:
+    AMothershipBoss();
+
+    inline static constexpr int32 n_hatches{4};
+    inline static constexpr int32 n_search_lights{8};
+    inline static constexpr int32 n_point_lights{8};
+
+    void Tick(float dt) override;
+  protected:
+    void BeginPlay() override;
+    void OnConstruction(FTransform const& transform) override;
+
+    UPROPERTY(EditAnywhere, Category = "Boss")
+    UStaticMeshComponent* mesh_component{nullptr};
+
+    // Hatches
+    UPROPERTY(EditAnywhere, Category = "Boss|Hatches")
+    UStaticMesh* hatch_mesh{nullptr};
+    UPROPERTY(EditAnywhere, Category = "Boss|Hatches")
+    TArray<UStaticMeshComponent*> hatch_meshes{};
+
+    // Search lights
+    UPROPERTY(EditAnywhere, Category = "Boss|Search light")
+    UStaticMesh* search_light_mesh{nullptr};
+    UPROPERTY(EditAnywhere, Category = "Boss|Search light")
+    TArray<UStaticMeshComponent*> search_light_meshes{};
+    UPROPERTY(EditAnywhere, Category = "Boss|Search light")
+    UMaterialInstance* search_light_mesh_material_base{nullptr};
+    UPROPERTY(VisibleAnywhere, Category = "Boss|Search light")
+    TArray<UMaterialInstanceDynamic*> light_material_instances;
+
+    // Point lights
+    UPROPERTY(EditAnywhere, Category = "Boss|Point lights")
+    TArray<UPointLightComponent*> point_lights{};
+    UPROPERTY(EditAnywhere, Category = "Boss|Point lights")
+    FLinearColor point_light_colour{FLinearColor::Red};
+    UPROPERTY(EditAnywhere, Category = "Boss|Point lights")
+    float point_light_brightness{100e6};
+  private:
+    template <int32 N, typename T>
+    void add_n_components(TArray<T*>& components, FString const& name_base);
+    template <int32 N, typename T>
+    void attach_components(UStaticMeshComponent& parent,
+                           UStaticMesh& parent_mesh,
+                           TArray<T*>& components,
+                           FString const& socket_base);
+    void set_meshes(UStaticMesh* mesh, TArray<UStaticMeshComponent*>& components);
+    void config_point_lights();
+};
