7103478bb0a0a603bd0c4f297ba863972a5fafec
Matthew Love <matthewlove94@gmail.com>
2026-05-15 21:10:45 +0100

    Start box size struct

---
+++ Source/Sandbox/utilities/BoxSize.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <CoreMinimal.h>
+
+#include "BoxSize.generated.h"
+
+USTRUCT()
+struct FBoxSize {
+    GENERATED_BODY()
+
+    FBoxSize() = default;
+
+    UPROPERTY(EditAnywhere)
+    FVector box_size;
+};
