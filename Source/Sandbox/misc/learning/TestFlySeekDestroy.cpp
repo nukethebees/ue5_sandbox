1b5b6c3bd91d8bbfd10fe44757d7f22699dd8034
Matthew Love <matthewlove94@gmail.com>
2026-05-15 20:47:54 +0100

    Start working on attacking actor

---
+++ Source/Sandbox/misc/learning/TestFlySeekDestroy.cpp
@@ -0,0 +1,193 @@
+#include "TestFlySeekDestroy.h"
+
+#include "Sandbox/logging/SandboxLogCategories.h"
+#include "Sandbox/misc/learning/TestVolume.h"
+#include "Sandbox/utilities/actor_utils.h"
+#include "Sandbox/utilities/macros/null_checks.hpp"
+#include "Sandbox/utilities/vision_maths.h"
+
+#include <Components/BoxComponent.h>
+#include <Engine/HitResult.h>
+#include <Engine/World.h>
+#include <Kismet/KismetMathLibrary.h>
+
+ATestFlySeekDestroy::ATestFlySeekDestroy() {}
+
+void ATestFlySeekDestroy::Tick(float dt) {
+    Super::Tick(dt);
+
+    switch (state) {
+        case ETestFlySeekDestroyState::searching: {
+            handle_search(dt);
+            break;
+        }
+        case ETestFlySeekDestroyState::chasing: {
+            handle_chase(dt);
+            break;
+        }
+        case ETestFlySeekDestroyState::attacking: {
+            handle_attack(dt);
+            break;
+        }
+    }
+
+    if (show_debug_shapes) {
+        draw_debug_shapes();
+    }
+}
+
+void ATestFlySeekDestroy::BeginPlay() {
+    Super::BeginPlay();
+
+    if (!search_volume) {
+        UE_LOG(LogSandboxLearning, Warning, TEXT("search_volume is nullptr"));
+        SetActorTickEnabled(false);
+    }
+
+    draw_config.world = GetWorld();
+    set_state(ETestFlySeekDestroyState::searching);
+
+    draw_config.cone_angle_half_width_deg = vision.half_angle_deg;
+    draw_config.cone_angle_half_height_deg = vision.half_angle_deg;
+}
+
+// Movement
+auto ATestFlySeekDestroy::within_radius(FVector const& point, float const r) const -> bool {
+    return FVector::DistSquared(point, GetActorLocation()) <= (r * r);
+}
+auto ATestFlySeekDestroy::at_destination() const -> bool {
+    return within_radius(search_destination, acceptance_radius);
+}
+
+// State
+void ATestFlySeekDestroy::set_state(ETestFlySeekDestroyState new_state) {
+    state = new_state;
+
+    switch (state) {
+        case ETestFlySeekDestroyState::searching: {
+            reset_search_destination();
+            material_state.config = search_material_config;
+            break;
+        }
+        case ETestFlySeekDestroyState::chasing: {
+            material_state.config = chase_material_config;
+            break;
+        }
+        case ETestFlySeekDestroyState::attacking: {
+            break;
+        }
+    }
+
+    material_state.update_instance();
+}
+
+// Search
+void ATestFlySeekDestroy::handle_search(float dt) {
+    if (scan_for_target()) {
+        set_state(ETestFlySeekDestroyState::chasing);
+        return handle_chase(dt);
+    }
+
+    if (at_destination()) {
+        if (log_config.can_log(EActorLoggingVerbosity::Basic)) {
+            UE_LOG(LogSandboxLearning,
+                   Display,
+                   TEXT("(%s) At destination. Finding new destination."),
+                   *ml::get_best_display_name(*this));
+        }
+
+        set_new_search_destination();
+    }
+
+    move_to_location(dt, search_destination);
+}
+void ATestFlySeekDestroy::set_new_search_destination() {
+    auto* box{search_volume->get_box()};
+    auto const box_pos{box->GetComponentLocation()};
+    auto const box_extent{box->GetScaledBoxExtent()};
+
+    while (within_radius(search_destination, min_distance_to_new_point)) {
+        search_destination = UKismetMathLibrary::RandomPointInBoundingBox(box_pos, box_extent);
+    }
+
+    if (log_config.can_log(EActorLoggingVerbosity::Basic)) {
+        UE_LOG(LogSandboxLearning,
+               Display,
+               TEXT("(%s) New destination: %s"),
+               *ml::get_best_display_name(*this),
+               *search_destination.ToCompactString());
+    }
+}
+void ATestFlySeekDestroy::reset_search_destination() {
+    search_destination = GetActorLocation();
+}
+auto ATestFlySeekDestroy::scan_for_target() -> bool {
+    FVector origin;
+    FVector extent;
+
+    GetActorBounds(false, origin, extent);
+
+    auto hits{ml::find_actors_within_cone(
+        *this, vision.radius, extent.Z * 2.0, vision.half_angle_rad(), ThisClass::StaticClass())};
+
+    chase_target = ml::get_centre_actor_in_fov(*this, hits);
+    if (!chase_target.IsValid()) {
+        return false;
+    }
+
+    return true;
+}
+
+// Chase
+void ATestFlySeekDestroy::handle_chase(float dt) {
+    if (!chase_target.IsValid()) {
+        set_state(ETestFlySeekDestroyState::searching);
+        return;
+    }
+
+    move_to_location(dt, chase_target->GetActorLocation());
+}
+void ATestFlySeekDestroy::move_to_location(float dt, FVector const& location) {
+    auto const current_rotation{GetActorRotation()};
+    auto const direction_to_target{(location - GetActorLocation()).GetSafeNormal()};
+    auto const rotation_to_target{direction_to_target.Rotation()};
+
+    auto const new_rotation{
+        FMath::RInterpConstantTo(current_rotation, rotation_to_target, dt, turn_speed_deg_per_s)};
+
+    SetActorRotation(new_rotation);
+
+    auto const pos{GetActorLocation()};
+    auto const max_delta_dist{speed * dt};
+
+    SetActorLocation(pos + max_delta_dist * new_rotation.Vector());
+}
+
+// Attack
+void ATestFlySeekDestroy::handle_attack(float dt) {}
+
+void ATestFlySeekDestroy::draw_debug_shapes() {
+    TRY_INIT_PTR(world, GetWorld());
+    auto const pos{GetActorLocation()};
+
+    draw_config.draw_sphere(pos, acceptance_radius);
+
+    switch (state) {
+        case ETestFlySeekDestroyState::searching: {
+            draw_config.draw_line(pos, search_destination);
+            draw_config.draw_cone(pos, GetActorForwardVector(), vision.radius);
+
+            draw_config.draw_sphere(search_destination, destination_sphere_radius);
+
+            break;
+        }
+        case ETestFlySeekDestroyState::chasing: {
+            draw_config.draw_line(pos, chase_target->GetActorLocation());
+            break;
+        }
+        case ETestFlySeekDestroyState::attacking: {
+            draw_config.draw_line(pos, chase_target->GetActorLocation());
+            break;
+        }
+    }
+}
---
+++ Source/Sandbox/misc/learning/TestFlySeekDestroy.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "Sandbox/combat/BurstFire.h"
+#include "Sandbox/logging/ActorLoggingConfig.h"
+#include "Sandbox/misc/learning/TestMaterialConfig.h"
+#include "Sandbox/players/VisionConfig.h"
+#include "Sandbox/utilities/DrawDebugConfig.h"
+#include "TestFlyBase.h"
+
+#include "CoreMinimal.h"
+
+#include "TestFlySeekDestroy.generated.h"
+
+UENUM()
+enum class ETestFlySeekDestroyState : uint8 {
+    searching,
+    chasing,
+    attacking,
+};
+
+class ATestVolume;
+
+UCLASS()
+class ATestFlySeekDestroy : public ATestFlyBase {
+  public:
+    GENERATED_BODY()
+
+    ATestFlySeekDestroy();
+
+    void Tick(float dt) override;
+  protected:
+    void BeginPlay() override;
+
+    // Movement
+    void move_to_location(float dt, FVector const& location);
+    auto within_radius(FVector const& point, float const r) const -> bool;
+    auto at_destination() const -> bool;
+
+    // State
+    void set_state(ETestFlySeekDestroyState new_state);
+
+    // Search
+    void handle_search(float dt);
+    void set_new_search_destination();
+    void reset_search_destination();
+    auto scan_for_target() -> bool;
+
+    // Chase
+    void handle_chase(float dt);
+
+    // Attack
+    void handle_attack(float dt);
+
+    // Debugging
+    void draw_debug_shapes();
+
+    // Visuals
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    FTestMaterialConfig search_material_config;
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    FTestMaterialConfig chase_material_config;
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    FTestMaterialConfig attack_material_config;
+
+    // Movement
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    float speed{1000.f};
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    float turn_speed_deg_per_s{60.f};
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    float acceptance_radius{500.f};
+
+    // Vision
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    FVisionConfig vision{};
+
+    // State
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    ETestFlySeekDestroyState state{ETestFlySeekDestroyState::searching};
+
+    // Search
+    UPROPERTY(VisibleAnywhere, Category = "Fly")
+    FVector search_destination;
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    TObjectPtr<ATestVolume> search_volume{nullptr};
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    float min_distance_to_new_point{5000.f};
+
+    // Chase
+    UPROPERTY(VisibleAnywhere, Category = "Fly")
+    TWeakObjectPtr<AActor> chase_target{nullptr};
+
+    // Attacking
+    UPROPERTY(VisibleAnywhere, Category = "Fly")
+    FBurstFire burst{};
+
+    // Debugging
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    bool show_debug_shapes{false};
+    UPROPERTY(EditAnywhere, Category = "Fly")
+    FDrawDebugConfig draw_config;
+    UPROPERTY(EditAnywhere, Category = "Ship")
+    FActorLoggingConfig log_config{1.f};
+    UPROPERTY(EditAnywhere, Category = "Ship")
+    float destination_sphere_radius{100.f};
+};
