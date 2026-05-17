dcc48019935ad4731c575630b35f068025d2238c
Matthew Love <matthewlove94@gmail.com>
2026-05-17 17:53:24 +0100

    Add constructor

--- Source/Sandbox/players/VisionConfig.h
+++ Source/Sandbox/players/VisionConfig.h
@@ -9,6 +9,9 @@ struct FVisionConfig {
     GENERATED_BODY()
 
     FVisionConfig() = default;
+    FVisionConfig(float radius, float half_angle_deg)
+        : radius(radius)
+        , half_angle_deg(half_angle_deg) {}
 
     auto half_angle_degrees() const -> float;
     auto half_angle_rad() const -> float;
