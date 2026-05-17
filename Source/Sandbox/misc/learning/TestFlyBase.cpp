35ad88d3fc0de145b798a0c8bb4272514045c7d0
Matthew Love <matthewlove94@gmail.com>
2026-05-17 17:53:14 +0100

    Add health

--- Source/Sandbox/misc/learning/TestFlyBase.cpp
+++ Source/Sandbox/misc/learning/TestFlyBase.cpp
@@ -1,5 +1,9 @@
 #include "TestFlyBase.h"
 
+#include "Sandbox/constants/collision_channels.h"
+#include "Sandbox/health/ShipHealthComponent.h"
+#include "Sandbox/players/ShipDamageResult.h"
+
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
 
@@ -8,10 +12,14 @@
 
 ATestFlyBase::ATestFlyBase()
     : main_mesh{CreateDefaultSubobject<UStaticMeshComponent>(TEXT("main_mesh"))}
-    , main_collision{CreateDefaultSubobject<UBoxComponent>(TEXT("main_collision"))} {
+    , main_collision{CreateDefaultSubobject<UBoxComponent>(TEXT("main_collision"))}
+    , health{CreateDefaultSubobject<UShipHealthComponent>(TEXT("health"))} {
     RootComponent = main_collision;
 
     main_mesh->SetupAttachment(RootComponent);
+
+    main_collision->SetCollisionResponseToChannel(ml::collision::projectile,
+                                                  ECollisionResponse::ECR_Block);
 
     PrimaryActorTick.bStartWithTickEnabled = true;
     PrimaryActorTick.bCanEverTick = true;
@@ -43,4 +51,18 @@ auto ATestFlyBase::check_if_hit(FVector start, FVector end) -> FHitResult {
 }
 void ATestFlyBase::configure_material() {
     material_state.initialise_mesh_material(this, *main_mesh, 0);
+}
+
+// IDamageableShip
+auto ATestFlyBase::apply_damage(ShipDamageContext context) -> FShipDamageResult {
+    health->apply_damage(context.damage);
+    if (health->is_dead()) {
+        Destroy();
+        return {EDamageResult::ActorKilled};
+    }
+
+    return {EDamageResult::Damaged};
+}
+auto ATestFlyBase::get_on_killed_delegate() -> FOnKilled& {
+    return on_killed;
 }
--- Source/Sandbox/misc/learning/TestFlyBase.h
+++ Source/Sandbox/misc/learning/TestFlyBase.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "Sandbox/misc/learning/TestMaterialConfig.h"
+#include "Sandbox/players/DamageableShip.h"
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
 
 #include "TestFlyBase.generated.h"
 
@@ -12,8 +13,12 @@ class UStaticMeshComponent;
 
 struct FHitResult;
 
+class UShipHealthComponent;
+
 UCLASS()
-class ATestFlyBase : public AActor {
+class ATestFlyBase
+    : public APawn
+    , public IDamageableShip {
   public:
     GENERATED_BODY()
 
@@ -22,15 +27,23 @@ class ATestFlyBase : public AActor {
     void OnConstruction(FTransform const& transform) override;
     void BeginPlay() override;
 
+    auto check_if_hit(FVector start, FVector end) -> FHitResult;
+
     void configure_material();
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Fly")
-    UStaticMeshComponent* main_mesh{nullptr};
-    UPROPERTY(EditAnywhere, Category = "target")
+    // IDamageableShip
+    auto apply_damage(ShipDamageContext context) -> FShipDamageResult override;
+    auto get_on_killed_delegate() -> FOnKilled& override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Fly|Visuals")
+    TObjectPtr<UStaticMeshComponent> main_mesh{nullptr};
+    UPROPERTY(EditAnywhere, Category = "Fly|Visuals")
     FTestMaterialState material_state;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Fly")
-    UBoxComponent* main_collision{nullptr};
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Fly|Collision")
+    TObjectPtr<UBoxComponent> main_collision{nullptr};
 
-    auto check_if_hit(FVector start, FVector end) -> FHitResult;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Fly|Health")
+    TObjectPtr<UShipHealthComponent> health{nullptr};
+    FOnKilled on_killed;
 };
