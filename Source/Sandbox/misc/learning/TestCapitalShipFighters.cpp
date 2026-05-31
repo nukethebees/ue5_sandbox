14341a1e7c2a947bd5bfa3474da5713428915ab4
Matthew Love <matthewlove94@gmail.com>
2026-05-31 13:43:58 +0100

    Add basic setup

--- Source/Sandbox/misc/learning/TestCapitalShipFighters.cpp
+++ Source/Sandbox/misc/learning/TestCapitalShipFighters.cpp
@@ -1,1 +1,38 @@
 #include "TestCapitalShipFighters.h"
+
+#include <Components/InstancedStaticMeshComponent.h>
+#include <Components/SceneComponent.h>
+
+ATestCapitalShipFighters::ATestCapitalShipFighters()
+    : instances{CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("instances"))} {
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("root"));
+
+    instances->SetupAttachment(RootComponent);
+
+    PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bStartWithTickEnabled = true;
+}
+
+// Actor life cycle
+void ATestCapitalShipFighters::OnConstruction(FTransform const& transform) {
+    Super::OnConstruction(transform);
+}
+void ATestCapitalShipFighters::PostInitializeComponents() {
+    Super::PostInitializeComponents();
+
+    clear_runtime_state();
+}
+void ATestCapitalShipFighters::BeginPlay() {
+    Super::BeginPlay();
+}
+void ATestCapitalShipFighters::Tick(float dt) {
+    Super::Tick(dt);
+}
+
+// Spawning
+void ATestCapitalShipFighters::spawn_instance(FTransform const& transform) {}
+
+// Misc
+void ATestCapitalShipFighters::clear_runtime_state() {
+    instances->ClearInstances();
+}
--- Source/Sandbox/misc/learning/TestCapitalShipFighters.h
+++ Source/Sandbox/misc/learning/TestCapitalShipFighters.h
@@ -1,18 +1,32 @@
 #pragma once
 
-#pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 
 #include "TestCapitalShipFighters.generated.h"
 
+class UInstancedStaticMeshComponent;
+
 UCLASS()
 class ATestCapitalShipFighters : public AActor {
     GENERATED_BODY()
   public:
-    ATestCapitalShipFighters() = default;
+    ATestCapitalShipFighters();
+
+    void PostInitializeComponents() override;
+    void Tick(float dt) override;
+
+    void spawn_instance(FTransform const& transform);
   protected:
+    void OnConstruction(FTransform const& transform) override;
+    void BeginPlay() override;
+
+    // Misc
+    void clear_runtime_state();
+
+    UPROPERTY()
+    TObjectPtr<UInstancedStaticMeshComponent> instances;
+
     UPROPERTY()
     TArray<FTransform> world_transforms;
 };
