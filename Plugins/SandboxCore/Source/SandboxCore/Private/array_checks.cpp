c111415f9a38cd49df06c86abe35533075babc08
Matthew Love <matthewlove94@gmail.com>
2026-06-07 13:38:49 +0100

    Add fatal_if_nums_not_equal

---
+++ Plugins/SandboxCore/Source/SandboxCore/Private/array_checks.cpp
@@ -0,0 +1,35 @@
+#include <SandboxCore/array_checks.h>
+
+#include <initializer_list>
+
+namespace ml {
+void fatal_if_nums_not_equal(std::initializer_list<NamedNum> const values) {
+    if (values.size() <= 1) {
+        return;
+    }
+
+    auto const expected{values.begin()->num};
+    auto const* expected_name{values.begin()->name};
+
+    FString message{};
+
+    for (auto const& value : values) {
+        if (value.num == expected) {
+            continue;
+        }
+
+        message += FString::Printf(TEXT("%s.Num() == %d, "), value.name, value.num);
+    }
+
+    if (message.IsEmpty()) {
+        return;
+    }
+
+    UE_LOG(LogTemp,
+           Fatal,
+           TEXT("Array size invariant failed. Expected %s.Num() == %d. Mismatches: %s"),
+           expected_name,
+           expected,
+           *message);
+}
+}
---
+++ Plugins/SandboxCore/Source/SandboxCore/Public/SandboxCore/array_checks.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <SandboxCore/container_traits.h>
+
+#include <HAL/Platform.h>
+
+#include <initializer_list>
+#include <type_traits>
+
+namespace ml {
+
+struct NamedNum {
+    int32 num{};
+    TCHAR const* name{};
+};
+
+#define SANDBOX_NAMED_NUM(value)                                                      \
+    ml::NamedNum {                                                                    \
+        ml::NumTraits<std::remove_cvref_t<decltype(value)>>::num(value), TEXT(#value) \
+    }
+
+void SANDBOXCORE_API fatal_if_nums_not_equal(std::initializer_list<NamedNum> const values);
+}
