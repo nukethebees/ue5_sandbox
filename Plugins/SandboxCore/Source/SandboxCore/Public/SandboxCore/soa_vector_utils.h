1a133b1bd20f978f55639968f8c0adae5032bd5e
Matthew Love <matthewlove94@gmail.com>
2026-06-12 10:36:49 +0100

    Add more assignment functions

--- Plugins/SandboxCore/Source/SandboxCore/Public/SandboxCore/soa_vector_utils.h
+++ Plugins/SandboxCore/Source/SandboxCore/Public/SandboxCore/soa_vector_utils.h
@@ -1,20 +1,37 @@
 #pragma once
 
+#include <SandboxCore/soa_vectors.h>
+
 #include <HAL/Platform.h>
 #include <Math/MathFwd.h>
 
-struct FVectors3f;
-
 namespace ml {
-void assign_from(FVectors3f& dst, FVectors3f const& src);
+void SANDBOXCORE_API assign_from(FVectors3f& dst, FVectors3f const& src);
+inline void
+    assign_from(FVectors3f& dst, int32 const dst_i, FVectors3f const& src, int32 const src_i) {
+    dst.xs[dst_i] = src.xs[src_i];
+    dst.ys[dst_i] = src.ys[src_i];
+    dst.zs[dst_i] = src.zs[src_i];
+}
 
-void multiply_in_place(FVectors3f& dst, float const value);
+inline void assign(FVectors3f& vector, int32 const i, float const value) {
+    vector.xs[i] = value;
+    vector.ys[i] = value;
+    vector.zs[i] = value;
+}
+inline void assign(FVectors3f& vector, int32 const i, FVector const& value) {
+    vector.xs[i] = static_cast<float>(value.X);
+    vector.ys[i] = static_cast<float>(value.Y);
+    vector.zs[i] = static_cast<float>(value.Z);
+}
 
-auto get_fvector(FVectors3f const& src, int32 i) -> FVector {
+void SANDBOXCORE_API multiply_in_place(FVectors3f& dst, float const value);
+
+inline auto get_fvector(FVectors3f const& src, int32 i) -> FVector {
     return {src.xs[i], src.ys[i], src.zs[i]};
 }
 
-auto scaled_fvector(FVectors3f const& vectors, int32 const i, float const scale) -> FVector {
+inline auto scaled_fvector(FVectors3f const& vectors, int32 const i, float const scale) -> FVector {
     return FVector{
         vectors.xs[i] * scale,
         vectors.ys[i] * scale,
@@ -23,7 +40,7 @@ auto scaled_fvector(FVectors3f const& vectors, int32 const i, float const scale)
 }
 }
 
-auto operator*=(FVectors3f& vectors, float const scale) -> FVectors3f& {
+inline auto operator*=(FVectors3f& vectors, float const scale) -> FVectors3f& {
     ml::multiply_in_place(vectors, scale);
     return vectors;
 }
