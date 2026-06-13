1ddcc18a8212ee8a1fb8408746a21fa08e64a3a0
Matthew Love <matthewlove94@gmail.com>
2026-06-13 18:52:58 +0100

    Remove assertions

--- Plugins/SandboxCore/Tests/Private/array_math_tests_add_in_place.cpp
+++ Plugins/SandboxCore/Tests/Private/array_math_tests_add_in_place.cpp
@@ -7,7 +7,6 @@ TEST_CASE("SandboxCore.Array.add_in_place.Zero") {
     TArray<int32> array{0, 1, 2};
     TArray<int32> expected{0, 1, 2};
 
-    REQUIRE(array == expected);
     ml::add_in_place(array, 0);
     REQUIRE(array == expected);
 }
@@ -16,7 +15,6 @@ TEST_CASE("SandboxCore.Array.add_in_place.One") {
     TArray<int32> array{0, 1, 2};
     TArray<int32> expected{1, 2, 3};
 
-    REQUIRE(array != expected);
     ml::add_in_place(array, 1);
     REQUIRE(array == expected);
 }
