bda0e475cd672a9f981620fe0c38b3fdef58371d
Matthew Love <matthewlove94@gmail.com>
2026-06-13 19:04:06 +0100

    Add append_n tests

---
+++ Plugins/SandboxCore/Tests/Private/array_utils_tests_append_n.cpp
@@ -0,0 +1,37 @@
+#include <SandboxCore/array_utils.h>
+
+#include "CoreMinimal.h"
+#include "TestHarness.h"
+
+using A = TArray<int32>;
+
+TEST_CASE("SandboxCore.Array.append_n.Empty array, append 0*0") {
+    TArray<int32> array{};
+    ml::append_n(array, 0, 0);
+
+    REQUIRE(array == A{});
+}
+TEST_CASE("SandboxCore.Array.append_n.Empty array, append 0*1") {
+    TArray<int32> array{};
+    ml::append_n(array, 0, 1);
+
+    REQUIRE(array == A{0});
+}
+TEST_CASE("SandboxCore.Array.append_n.Empty array, append 1*1") {
+    TArray<int32> array{};
+    ml::append_n(array, 1, 1);
+
+    REQUIRE(array == A{1});
+}
+TEST_CASE("SandboxCore.Array.append_n.Empty array, append 2*1") {
+    TArray<int32> array{};
+    ml::append_n(array, 1, 2);
+
+    REQUIRE(array == A{1, 1});
+}
+TEST_CASE("SandboxCore.Array.append_n.Two value array, append 2*2") {
+    TArray<int32> array{3, 3};
+    ml::append_n(array, 2, 2);
+
+    REQUIRE(array == A{3, 3, 2, 2});
+}
