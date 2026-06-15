dd4f640d4c470db3cb53cef4ce99799c95a91050
Matthew Love <matthewlove94@gmail.com>
2026-06-15 18:15:00 +0100

    Remove * and , from test names

--- Plugins/SandboxCore/Tests/Private/array_utils_tests_append_n.cpp
+++ Plugins/SandboxCore/Tests/Private/array_utils_tests_append_n.cpp
@@ -5,31 +5,31 @@
 
 using A = TArray<int32>;
 
-TEST_CASE("SandboxCore.Array.append_n.Empty array, append 0*0") {
+TEST_CASE("SandboxCore.Array.append_n.Empty array append 0x0") {
     TArray<int32> array{};
     ml::append_n(array, 0, 0);
 
     REQUIRE(array == A{});
 }
-TEST_CASE("SandboxCore.Array.append_n.Empty array, append 0*1") {
+TEST_CASE("SandboxCore.Array.append_n.Empty array append 0x1") {
     TArray<int32> array{};
     ml::append_n(array, 0, 1);
 
     REQUIRE(array == A{0});
 }
-TEST_CASE("SandboxCore.Array.append_n.Empty array, append 1*1") {
+TEST_CASE("SandboxCore.Array.append_n.Empty array append 1x1") {
     TArray<int32> array{};
     ml::append_n(array, 1, 1);
 
     REQUIRE(array == A{1});
 }
-TEST_CASE("SandboxCore.Array.append_n.Empty array, append 2*1") {
+TEST_CASE("SandboxCore.Array.append_n.Empty array append 2x1") {
     TArray<int32> array{};
     ml::append_n(array, 1, 2);
 
     REQUIRE(array == A{1, 1});
 }
-TEST_CASE("SandboxCore.Array.append_n.Two value array, append 2*2") {
+TEST_CASE("SandboxCore.Array.append_n.Two value array append 2x2") {
     TArray<int32> array{3, 3};
     ml::append_n(array, 2, 2);
 
