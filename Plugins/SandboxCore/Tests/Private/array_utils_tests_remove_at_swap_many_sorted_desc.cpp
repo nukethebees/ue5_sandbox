3ebb2ae136e35513e4966284f5642abed5217b2f
Matthew Love <matthewlove94@gmail.com>
2026-06-15 18:15:07 +0100

    Make tests more readable

--- Plugins/SandboxCore/Tests/Private/array_utils_tests_remove_at_swap_many_sorted_desc.cpp
+++ Plugins/SandboxCore/Tests/Private/array_utils_tests_remove_at_swap_many_sorted_desc.cpp
@@ -2,35 +2,33 @@
 
 #include "TestHarness.h"
 
-TEST_CASE("SandboxCore.Array.remove_at_swap_many_sorted_desc.Removes from one int32 array",
-          "[SandboxCore][Array]") {
-    TArray<int32> values{10, 20, 30, 40, 50, 60};
+TEST_CASE("SandboxCore.Array.remove_at_swap_many_sorted_desc.Removes from one int32 array") {
+    TArray<int32> values{00, 10, 20, 30, 40, 50, 60};
     TArray<int32> const indices{4, 2, 0};
+
+    // 00, 10, 20, 30, 40, 50, 60
+    // 00, 10, 20, 30, 60, 50
+    // 00, 10, 50, 30, 60
+    // 60, 10, 50, 30
+
+    TArray<int32> expected{60, 10, 50, 30};
 
     ml::remove_at_swap_many_sorted_desc(indices, values);
 
-    REQUIRE(values.Num() == 3);
-    CHECK(values[0] == 40);
-    CHECK(values[1] == 20);
-    CHECK(values[2] == 60);
+    REQUIRE(values == expected);
 }
 
-TEST_CASE("SandboxCore.Array.remove_at_swap_many_sorted_desc.Removes from paired int32 and float arrays",
-          "[SandboxCore][Array]") {
-    TArray<int32> ids{10, 20, 30, 40, 50, 60};
-    TArray<float> weights{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
+TEST_CASE("SandboxCore.Array.remove_at_swap_many_sorted_desc.Removes from paired int32 and float arrays") {
+    TArray<int32> ids{00, 10, 20, 30, 40, 50, 60};
+    TArray<float> weights{0.f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
+
     TArray<int32> const indices{4, 2, 0};
+
+    TArray<int32> expected_ids{60, 10, 50, 30};
+    TArray<float> expected_weights{6.0f, 1.0f, 5.0f, 3.0f};
 
     ml::remove_at_swap_many_sorted_desc(indices, ids, weights);
 
-    REQUIRE(ids.Num() == 3);
-    REQUIRE(weights.Num() == 3);
-
-    CHECK(ids[0] == 40);
-    CHECK(ids[1] == 20);
-    CHECK(ids[2] == 60);
-
-    CHECK(weights[0] == 4.0f);
-    CHECK(weights[1] == 2.0f);
-    CHECK(weights[2] == 6.0f);
+    REQUIRE(ids == expected_ids);
+    REQUIRE(weights == expected_weights);
 }
