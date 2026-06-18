601624e942d3de2088108ed40fff792224e37a82
Matthew Love <matthewlove94@gmail.com>
2026-06-18 10:08:29 +0100

    Add more checks

--- Source/Sandbox/functional_tests/test_simple_batch.cpp
+++ Source/Sandbox/functional_tests/test_simple_batch.cpp
@@ -5,18 +5,72 @@
 void ATestSimpleBatch::StartTest() {
     Super::StartTest();
 
+    all_passed = true;
+
     if (!IsValid(entity_registry)) {
         FinishTest(EFunctionalTestResult::Failed, TEXT("Entity registry was not assigned"));
         return;
     }
+
+    update_kills();
 }
 void ATestSimpleBatch::Tick(float dt) {
     Super::Tick(dt);
 
-    auto const kills{entity_registry->get_total_kills()};
+    update_kills();
+    check_alive_matches_kills();
+
     if (kills == expected_kills) { FinishTest(EFunctionalTestResult::Succeeded, TEXT("Passed")); }
 
-    if (kills > expected_kills) {
-        FinishTest(EFunctionalTestResult::Failed, TEXT("Too many kills"));
+    all_passed &= AssertTrue(kills <= expected_kills, TEXT("Too many kills"));
+
+    if (!all_passed) {
+        FString msg{};
+
+        auto const& unique_entities{entity_registry->get_unique_entities()};
+        auto const n{unique_entities.num()};
+
+        msg += FString::Printf(TEXT("\nUnique entities: %d"), n);
+
+        msg += TEXT("\nActive entity data:");
+        auto const n_active_alive{entity_registry->get_num_alive_active_entities()};
+        msg += FString::Printf(TEXT("\nAlive: %d"), n_active_alive);
+
+        auto const n_active{entity_registry->get_num_elements()};
+
+        auto const& entity_data{entity_registry->get_entity_data()};
+        auto const generations{entity_registry->get_generations()};
+
+        for (int32 i{0}; i < n_active; ++i) {
+            msg += FString::Printf(TEXT("\nIndex: %d, Gen: %d, Health: %d, Alive: %d"),
+                                   i,
+                                   generations[i],
+                                   entity_data.healths[i],
+                                   entity_data.alive[i]);
+        }
+
+        msg += TEXT("\n\nGlobal id data:");
+        msg += FString::Printf(TEXT("\nAlive: %d"), entity_registry->get_total_alive());
+        msg += FString::Printf(TEXT("\nKills: %d"), kills);
+        for (int32 i{0}; i < n; ++i) {
+            msg += FString::Printf(TEXT("\nId: %d, Kills: %d, Alive: %d"),
+                                   i,
+                                   unique_entities.kills[i],
+                                   unique_entities.alive[i]);
+        }
+
+        FinishTest(EFunctionalTestResult::Failed, msg);
     }
+}
+
+void ATestSimpleBatch::check_alive_matches_kills() {
+    auto const n_alive{entity_registry->get_total_alive()};
+    auto const n_unique{entity_registry->get_num_unique_ids_issued()};
+
+    auto const n_alive_expected{n_unique - kills};
+
+    all_passed &= AssertEqual_Int(n_alive, n_alive_expected, TEXT("number of alive entities"));
+}
+void ATestSimpleBatch::update_kills() {
+    kills = entity_registry->get_total_kills();
 }
--- Source/Sandbox/functional_tests/test_simple_batch.h
+++ Source/Sandbox/functional_tests/test_simple_batch.h
@@ -13,9 +13,16 @@ class ATestSimpleBatch : public AFunctionalTest {
     void StartTest() override;
     void Tick(float dt) override;
   protected:
+    void check_alive_matches_kills();
+    void update_kills();
+
     UPROPERTY(EditAnywhere, Category = "Sandbox")
     ATestEntityRegistry* entity_registry{nullptr};
 
     UPROPERTY(EditAnywhere, Category = "Sandbox")
     int32 expected_kills{1};
+
+    int32 kills{0};
+
+    bool all_passed{true};
 };
