caafde572db4e25e5296b5ff1325bfa87426172c
Matthew Love <matthewlove94@gmail.com>
2026-06-20 10:15:37 +0100

    Add save game class

---
+++ Source/Sandbox/save/SpaceSaveGame.cpp
@@ -0,0 +1,1 @@
+#include "SpaceSaveGame.h"
---
+++ Source/Sandbox/save/SpaceSaveGame.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <Sandbox/batch_game/TestMissionMode.h>
+#include <Sandbox/batch_game/TestMissionState.h>
+
+#include <CoreMinimal.h>
+#include <GameFramework/SaveGame.h>
+
+#include "SpaceSaveGame.generated.h"
+
+USTRUCT(BlueprintType)
+struct FScoreRecord {
+    GENERATED_BODY()
+
+    UPROPERTY()
+    FDateTime date{};
+
+    UPROPERTY()
+    FName level_name{};
+
+    UPROPERTY()
+    ETestMissionMode mission_mode{ETestMissionMode::None};
+
+    UPROPERTY()
+    ETestMissionState end_state{ETestMissionState::NotStarted};
+
+    UPROPERTY()
+    int32 kills{};
+
+    UPROPERTY()
+    float time_seconds{};
+
+    // Objectives
+    UPROPERTY()
+    int32 kill_target{-1};
+
+    UPROPERTY()
+    float time_target{-1.f};
+};
+
+UCLASS()
+class SANDBOX_API USpaceSaveGame : public USaveGame {
+    GENERATED_BODY()
+  public:
+    static constexpr int32 current_save_version{1};
+
+    UPROPERTY()
+    int32 save_version{current_save_version};
+
+    UPROPERTY()
+    TArray<FScoreRecord> score_records;
+};
