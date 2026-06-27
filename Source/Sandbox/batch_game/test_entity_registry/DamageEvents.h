4617bce8f85f16af567b8f4919369972889dd9ca
Matthew Love <matthewlove94@gmail.com>
2026-06-27 13:39:02 +0100

    Use FSoAArrayMixin

--- Source/Sandbox/batch_game/test_entity_registry/DamageEvents.h
+++ Source/Sandbox/batch_game/test_entity_registry/DamageEvents.h
@@ -2,34 +2,40 @@
 
 #include <Sandbox/batch_game/test_entity_registry/RegistryEntityHandle.h>
 
+#include <SandboxCore/soa_array_mixin.h>
+
 #include <Containers/Array.h>
 #include <HAL/Platform.h>
 
 class AActor;
 class UActorComponent;
 
-struct UnresolvedDamageEvents {
+struct UnresolvedDamageEvents : public ml::FSoAArrayMixin {
     TArray<AActor*> damaged_actors;
     TArray<int32> damage_amounts;
     TArray<UActorComponent*> actor_components;
     TArray<int32> hit_items;
     TArray<FRegistryEntityHandle> instigators;
 
-    auto num() const -> int32;
-    void reset();
-    void validate_array_sizes() const;
+    template <typename TFunc>
+    auto apply_arrays(this auto&& self, TFunc&& func) -> decltype(auto) {
+        return std::forward<TFunc>(func)(self.damaged_actors,
+                                         self.damage_amounts,
+                                         self.actor_components,
+                                         self.hit_items,
+                                         self.instigators);
+    }
 };
 
-struct DamageEvents {
+struct DamageEvents : public ml::FSoAArrayMixin {
     TArray<int32> damage_amounts;
     TArray<UActorComponent*> actor_components;
     TArray<int32> hit_items;
     TArray<FRegistryEntityHandle> instigators;
 
-    auto num() const -> int32;
-    void reset();
-    void
-        remove_at_swap(int32 const index, int32 const count, EAllowShrinking const allow_shrinking);
-
-    void validate_array_sizes() const;
+    template <typename TFunc>
+    auto apply_arrays(this auto&& self, TFunc&& func) -> decltype(auto) {
+        return std::forward<TFunc>(func)(
+            self.damage_amounts, self.actor_components, self.hit_items, self.instigators);
+    }
 };
