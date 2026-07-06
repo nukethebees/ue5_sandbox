f0d235d3534563c75993b52f7fdb81aa68f7d01b
Matthew Love <matthewlove94@gmail.com>
2026-07-06 10:42:11 +0100

    Add operator<=>

--- Source/Sandbox/utilities/IndexSpan.h
+++ Source/Sandbox/utilities/IndexSpan.h
@@ -2,9 +2,13 @@
 
 #include <HAL/Platform.h>
 
+#include <compare>
+
 struct FIndexSpan {
     int32 offset{0};
     int32 count{0};
+
+    auto operator<=>(FIndexSpan const&) const noexcept = default;
 
     bool is_empty() const noexcept { return count == 0; }
     auto end() const noexcept { return offset + count; }
