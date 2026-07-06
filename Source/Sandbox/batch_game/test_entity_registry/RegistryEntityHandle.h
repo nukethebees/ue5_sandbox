389f733d96192e62e20f1ff77918ecbeb159d966
Matthew Love <matthewlove94@gmail.com>
2026-07-06 10:42:43 +0100

    Add hashing support

--- Source/Sandbox/batch_game/test_entity_registry/RegistryEntityHandle.h
+++ Source/Sandbox/batch_game/test_entity_registry/RegistryEntityHandle.h
@@ -1,8 +1,12 @@
 #pragma once
 
-#include "CoreMinimal.h"
+#include <CoreMinimal.h>
+#include <Templates/TypeHash.h>
 
+#include <bit>
 #include <compare>
+#include <type_traits>
+
 struct SANDBOX_API FRegistryEntityHandle {
     using index_type = int32;
     using generation_type = int32;
@@ -20,6 +24,13 @@ struct SANDBOX_API FRegistryEntityHandle {
     [[nodiscard]] auto is_null() const noexcept -> bool;
     auto to_string() const -> FString;
     void reset();
+
+    friend uint32 GetTypeHash(FRegistryEntityHandle const& handle) {
+        static_assert(sizeof(FRegistryEntityHandle) == sizeof(uint64));
+        static_assert(std::is_trivially_copyable_v<FRegistryEntityHandle>);
+
+        return GetTypeHash(std::bit_cast<uint64>(handle));
+    }
 
     index_type index{INDEX_NONE};
     generation_type generation{INDEX_NONE};
