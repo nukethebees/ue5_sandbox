87559c35432e8fae284ea67623449fdf5b1149cd
Matthew Love <matthewlove94@gmail.com>
2026-07-16 10:50:12 +0100

    Add is_valid

--- Source/Sandbox/batch_game/TestTeam.h
+++ Source/Sandbox/batch_game/TestTeam.h
@@ -14,3 +14,10 @@ enum class ETestTeam : uint8 {
     Yellow,
     COUNT UMETA(DisplayName = "Count", Hidden),
 };
+
+namespace ml {
+constexpr bool is_valid(ETestTeam const team) {
+    using T = uint8;
+    return static_cast<T>(team) < static_cast<T>(ETestTeam::COUNT);
+}
+}
