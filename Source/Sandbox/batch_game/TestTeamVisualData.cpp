1a879d123ba2cc7e92cd1ecd23b2ab0b02417862
Matthew Love <matthewlove94@gmail.com>
2026-07-16 10:50:26 +0100

    Prune invalid entries

--- Source/Sandbox/batch_game/TestTeamVisualData.cpp
+++ Source/Sandbox/batch_game/TestTeamVisualData.cpp
@@ -21,8 +21,6 @@ void UTestTeamVisualData::PostLoad() {
 auto UTestTeamVisualData::build_team_colour_cache() const -> FColourArray {
     FColourArray out{};
 
-    auto const n_colours{out.colours.Num()};
-
     for (auto const& [team, colour] : colours) {
         out[team] = colour;
     }
@@ -44,7 +42,13 @@ auto UTestTeamVisualData::build_team_colour_cache(UTestTeamVisualData const* con
 void UTestTeamVisualData::ensure_all_team_colours_exist() {
     constexpr auto count{std::to_underlying(ETestTeam::COUNT)};
 
-    for (int32 i = 0; i < count; ++i) {
+    for (int32 i{0}; i < count; ++i) {
         colours.FindOrAdd(static_cast<ETestTeam>(i), missing_colour);
+    }
+
+    TArray<ETestTeam> keys;
+    colours.GetKeys(keys);
+    for (auto k : keys) {
+        if (!ml::is_valid(k)) { colours.Remove(k); }
     }
 }
