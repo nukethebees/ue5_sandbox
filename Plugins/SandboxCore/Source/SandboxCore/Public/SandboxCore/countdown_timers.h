b4a77bb3370d5d192c13f721e028763516f8eebd
Matthew Love <matthewlove94@gmail.com>
2026-07-22 10:58:31 +0100

    Add view

--- Plugins/SandboxCore/Source/SandboxCore/Public/SandboxCore/countdown_timers.h
+++ Plugins/SandboxCore/Source/SandboxCore/Public/SandboxCore/countdown_timers.h
@@ -8,10 +8,26 @@ USTRUCT()
 struct SANDBOXCORE_API FCountdownTimers {
     GENERATED_BODY()
 
+    using View = TArrayView<float>;
+    using ConstView = TConstArrayView<float>;
+
     void tick(float const dt) noexcept;
 
     auto operator[](int32 const i) const -> float { return remaining_times[i]; }
     auto operator[](int32 const i) -> float& { return remaining_times[i]; }
+
+    auto get_view() -> View { return remaining_times; }
+    auto get_view(int32 const offset, int32 const count) -> View {
+        return get_view().Slice(offset, count);
+    }
+    auto get_view() const -> ConstView { return remaining_times; }
+    auto get_view(int32 const offset, int32 const count) const -> ConstView {
+        return get_view().Slice(offset, count);
+    }
+    auto get_const_view() const -> ConstView { return remaining_times; }
+    auto get_const_view(int32 const offset, int32 const count) const -> ConstView {
+        return get_const_view().Slice(offset, count);
+    }
 
     auto Num() const noexcept { return remaining_times.Num(); }
     auto Add(float t) { remaining_times.Add(t); }
