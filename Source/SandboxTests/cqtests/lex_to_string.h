cd7112d3f5f69ca0c3ec1101e5c356dc6ef8d730
Matthew Love <matthewlove94@gmail.com>
2026-07-25 17:01:16 +0100

    Add supports_lex_to_string

--- Source/SandboxTests/cqtests/lex_to_string.h
+++ Source/SandboxTests/cqtests/lex_to_string.h
@@ -3,3 +3,10 @@
 #include "CoreMinimal.h"
 
 auto LexToString(FVector3f const& vec) -> FString;
+
+namespace ml {
+template <typename T>
+concept supports_lex_to_string = requires(T const& t) {
+    { ::LexToString(t) } -> std::convertible_to<FString>;
+};
+}
