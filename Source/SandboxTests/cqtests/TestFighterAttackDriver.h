092a9598107858593ec3508a71b17b3e93d11586
Matthew Love <matthewlove94@gmail.com>
2026-07-25 17:53:55 +0100

    Add fight duration

--- Source/SandboxTests/cqtests/TestFighterAttackDriver.h
+++ Source/SandboxTests/cqtests/TestFighterAttackDriver.h
@@ -21,18 +21,26 @@ class ATestFighterAttackDriver : public AActor {
     auto get_enemy_team() const { return enemy_team; }
 
     auto get_setup_error() const { return setup_error; }
+    auto get_fight_duration() const { return fight_duration; }
   protected:
     void PostInitializeComponents() override;
     void BeginPlay() override;
 
+    // Hero
     UPROPERTY(EditAnywhere, Category = "Test")
     TObjectPtr<ATestCapitalShipProxy> hero{nullptr};
 
     ETestTeam hero_team;
 
+    // Enemy
     UPROPERTY(EditAnywhere, Category = "Test")
     TObjectPtr<ATestCapitalShipProxy> enemy{nullptr};
 
     ETestTeam enemy_team;
+
+    // Test setup
+    UPROPERTY(EditAnywhere, Category = "Test")
+    float fight_duration{5.f};
+
     FString setup_error{};
 };
