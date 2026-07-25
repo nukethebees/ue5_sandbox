a0dfd1bb60f886e6e6e6ae385545b240009febfd
Matthew Love <matthewlove94@gmail.com>
2026-07-25 17:01:05 +0100

    Add is_uenum

--- Source/Sandbox/utilities/enums.h
+++ Source/Sandbox/utilities/enums.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "CoreMinimal.h"
-#include "UObject/Class.h"
+#include <CoreMinimal.h>
+#include <Templates/IsUEnumClass.h>
+#include <UObject/Class.h>
 
 #include <type_traits>
 #include <utility>
@@ -10,13 +11,16 @@
 
 namespace ml {
 template <typename T>
+concept is_uenum = static_cast<bool>(TIsUEnumClass<std::remove_cvref_t<T>>::Value);
+
+template <typename T>
 struct EnumCountTrait {
     static constexpr T count{T::COUNT};
     static constexpr auto count_value{std::to_underlying(count)};
 };
 
 template <typename Enum>
-auto get_next(Enum current) -> Enum {
+auto get_next(Enum const current) -> Enum {
     using Underlying = std::underlying_type_t<Enum>;
     auto const next{std::to_underlying(current) + 1};
 
@@ -25,22 +29,20 @@ auto get_next(Enum current) -> Enum {
 }
 
 template <typename Enum>
-auto get_previous(Enum current) -> Enum {
+auto get_previous(Enum const current) -> Enum {
     return (current == static_cast<Enum>(0))
              ? static_cast<Enum>(EnumCountTrait<Enum>::count_value - 1)
              : static_cast<Enum>(std::to_underlying(current) - 1);
 }
 
-template <typename Enum>
-    requires std::is_scoped_enum_v<Enum>
-auto make_unhandled_enum_case_warning(Enum value) -> FString {
+template <is_uenum Enum>
+auto make_unhandled_enum_case_warning(Enum const value) -> FString {
     return FString::Printf(TEXT("Unhandled enum case: %s"), *UEnum::GetValueAsString(value));
 }
 
 // For the string Foo::Bar, return Bar
-template <typename Enum>
-    requires std::is_scoped_enum_v<Enum>
-auto to_string_without_type_prefix(Enum value) -> FString {
+template <is_uenum Enum>
+auto to_string_without_type_prefix(Enum const value) -> FString {
     auto const* const enum_ptr{StaticEnum<Enum>()};
     return enum_ptr->GetNameStringByValue(static_cast<int64>(value));
 }
