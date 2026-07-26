96bca25aa39aadef2a2f9b4f504b6e1039131023
Matthew Love <matthewlove94@gmail.com>
2026-07-26 20:24:20 +0100

    Support apply_array_pairs

--- Source/Sandbox/batch_game/test_entity_registry/DirectDamageEvents.h
+++ Source/Sandbox/batch_game/test_entity_registry/DirectDamageEvents.h
@@ -12,9 +12,11 @@ struct DirectDamageEvents : public ml::FSoAArrayMixin {
     TArray<int32> damage_amounts;
     TArray<FRegistryEntityHandle> instigators;
 
-    template <typename TFunc>
-    auto apply_arrays(this auto&& self, TFunc&& func) -> decltype(auto) {
-        return std::forward<TFunc>(func)(
-            self.damaged_entities, self.damage_amounts, self.instigators);
-    }
+#define SANDBOX_PACK(STAMPER, NON_FINAL) \
+    NON_FINAL(STAMPER(damaged_entities)) \
+    NON_FINAL(STAMPER(damage_amounts))   \
+    STAMPER(instigators)
+
+    SANDBOX_SOA_MAKE_APPLY_FNS(SANDBOX_PACK)
+#undef SANDBOX_PACK
 };
