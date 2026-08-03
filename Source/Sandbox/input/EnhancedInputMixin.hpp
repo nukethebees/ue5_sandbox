66276d6e40eb6be9009ef9f0525f0f929f28def8
Matthew Love <matthewlove94@gmail.com>
2026-08-03 13:59:14 +0100

    Change to UE_LOG

--- Source/Sandbox/input/EnhancedInputMixin.hpp
+++ Source/Sandbox/input/EnhancedInputMixin.hpp
@@ -1,20 +1,22 @@
 #pragma once
 
-#include <utility>
+#include <Sandbox/logging/SandboxLogCategories.h>
 
 #include "CoreMinimal.h"
 #include "EnhancedInputComponent.h"
 #include "InputTriggers.h"
 
+#include <utility>
+
 namespace ml {
 struct EnhancedInputMixin {
     template <typename Self>
-    decltype(auto) make_input_binder(this Self&& self, UEnhancedInputComponent* eic) {
+    auto make_input_binder(this Self&& self, UEnhancedInputComponent* eic) -> decltype(auto) {
         return [eic, owner = &self](auto* action, ETriggerEvent state, auto pmf) -> void {
             if (action) {
                 eic->BindAction(action, state, owner, pmf);
             } else {
-                owner->log_warning(TEXT("Binding action pointer missing."));
+                UE_LOG(LogSandbox, Warning, TEXT("Binding action pointer is nullptr."));
             }
         };
     }
