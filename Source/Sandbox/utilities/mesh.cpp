674957fc15d4466d024976a2052fe5edd509dd0a
Matthew Love <matthewlove94@gmail.com>
2026-08-04 14:00:02 +0100

    Split into two functions

--- Source/Sandbox/utilities/mesh.cpp
+++ Source/Sandbox/utilities/mesh.cpp
@@ -5,7 +5,12 @@
 #include <Engine/StaticMesh.h>
 
 namespace ml {
+auto get_mesh_sphere_bounds(UStaticMesh const& mesh) -> float {
+    return static_cast<float>(mesh.GetBounds().SphereRadius);
+}
 auto get_mesh_sphere_bounds(UStaticMeshComponent const& mesh) -> float {
-    return static_cast<float>(mesh.GetStaticMesh()->GetBounds().SphereRadius);
+    auto const static_mesh{mesh.GetStaticMesh()};
+    check(static_mesh);
+    return get_mesh_sphere_bounds(*static_mesh);
 }
 }
--- Source/Sandbox/utilities/mesh.h
+++ Source/Sandbox/utilities/mesh.h
@@ -1,7 +1,9 @@
 #pragma once
 
 class UStaticMeshComponent;
+class UStaticMesh;
 
 namespace ml {
+auto get_mesh_sphere_bounds(UStaticMesh const& mesh) -> float;
 auto get_mesh_sphere_bounds(UStaticMeshComponent const& mesh) -> float;
 }
