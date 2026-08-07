51e68e2e2f58ed73754a5a62e5d0ffdd387ced3c
Matthew Love <matthewlove94@gmail.com>
2026-08-07 22:46:56 +0100

    Add operator*

--- Plugins/SandboxCore/Source/SandboxCore/Public/SandboxCore/error_msg.h
+++ Plugins/SandboxCore/Source/SandboxCore/Public/SandboxCore/error_msg.h
@@ -4,6 +4,8 @@
 
 namespace ml {
 struct FErrorMsg {
+    auto operator*() const noexcept -> TCHAR const* { return *message; }
+
     explicit operator bool() const noexcept { return has_error(); }
 
     bool has_error() const noexcept { return !message.IsEmpty(); }
