6002219df490b9310f6cc955f113ea52bf290d36
Matthew Love <matthewlove94@gmail.com>
2026-08-07 15:46:48 +0100

    Add name functions

--- Source/Sandbox/batch_game/TestEntityType.h
+++ Source/Sandbox/batch_game/TestEntityType.h
@@ -13,3 +13,9 @@ enum class ETestEntityType : uint8 {
     TubeSpinner,
     COUNT UMETA(Hidden),
 };
+
+namespace ml {
+auto get_entity_display_name(ETestEntityType const type) -> FString const&;
+auto get_entity_class_name(ETestEntityType const type) -> FString const&;
+auto get_entity_short_name(ETestEntityType const type) -> FString const&;
+}
