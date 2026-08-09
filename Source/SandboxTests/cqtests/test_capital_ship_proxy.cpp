f4fc1de5d34991a90e42b7d8976cfff2c2d84f9f
Matthew Love <matthewlove94@gmail.com>
2026-08-09 16:58:52 +0100

    Formatting

--- Source/SandboxTests/cqtests/test_capital_ship_proxy.cpp
+++ Source/SandboxTests/cqtests/test_capital_ship_proxy.cpp
@@ -19,22 +19,22 @@ TEST_CLASS(TestCapitalShipProxy, "Sandbox.FunctionalTests")
 
     BEFORE_EACH()
     {
-        level_setup.setup(
-            TestCommandBuilder,
-            *TestRunner,
-            [this](UWorld& world, UTestSimulationConfig const& config) {
-                auto* const capital_config{config.simulation_config->capital_ships_config.Get()};
-                check(capital_config);
+        level_setup.setup(TestCommandBuilder,
+                          *TestRunner,
+                          [this](UWorld& world, UTestSimulationConfig const& config) {
+                              auto* const capital_config{
+                                  config.simulation_config->capital_ships_config.Get()};
+                              check(capital_config);
 
-                expected_health = capital_config->max_health;
+                              expected_health = capital_config->max_health;
 
-                auto* const proxy{world.SpawnActorDeferred<ATestCapitalShipProxy>(
-                    ATestCapitalShipProxy::StaticClass(), FTransform::Identity)};
-                check(proxy);
+                              auto* const proxy{world.SpawnActorDeferred<ATestCapitalShipProxy>(
+                                  ATestCapitalShipProxy::StaticClass(), FTransform::Identity)};
+                              check(proxy);
 
-                proxy->set_actor_config(capital_config);
-                UGameplayStatics::FinishSpawningActor(proxy, FTransform::Identity);
-            });
+                              proxy->set_actor_config(capital_config);
+                              UGameplayStatics::FinishSpawningActor(proxy, FTransform::Identity);
+                          });
     }
 
     AFTER_EACH()
@@ -46,17 +46,15 @@ TEST_CLASS(TestCapitalShipProxy, "Sandbox.FunctionalTests")
             auto test_driver{ml::TestSimulationDriver::from_world(level_setup.get_world())};
             auto const& capitals{test_driver.get_capital_ships()};
 
-            if (!TestRunner->TestEqual(
-                    TEXT("One capital ship is spawned from the proxy"),
-                    capitals.get_num_instances(),
-                    1)) {
+            if (!TestRunner->TestEqual(TEXT("One capital ship is spawned from the proxy"),
+                                       capitals.get_num_instances(),
+                                       1)) {
                 return;
             }
 
             auto const health{capitals.get_health(capitals.get_handle(0))};
-            TestRunner->TestEqual(TEXT("Capital ship health matches its config"),
-                                  health,
-                                  expected_health);
+            TestRunner->TestEqual(
+                TEXT("Capital ship health matches its config"), health, expected_health);
         });
     }
 };
