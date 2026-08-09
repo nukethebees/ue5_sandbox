61f874d7ea96916e5db5f6a08f0d980fd787ca85
Matthew Love <matthewlove94@gmail.com>
2026-08-09 16:48:37 +0100

    Don't require global namespace

--- Source/SandboxTests/cqtests/lex_to_string.h
+++ Source/SandboxTests/cqtests/lex_to_string.h
@@ -7,6 +7,6 @@ auto LexToString(FVector3f const& vec) -> FString;
 namespace ml {
 template <typename T>
 concept supports_lex_to_string = requires(T const& t) {
-    { ::LexToString(t) } -> std::convertible_to<FString>;
+    { LexToString(t) } -> std::convertible_to<FString>;
 };
 }
