82179beb92ef1002c0a629d572bcd119925a6007
Matthew Love <matthewlove94@gmail.com>
2026-08-15 14:12:01 +0100

    Add set_team

--- Source/Sandbox/batch_game/TestCapitalShipProxy.h
+++ Source/Sandbox/batch_game/TestCapitalShipProxy.h
@@ -43,6 +43,7 @@ class ATestCapitalShipProxy
     auto get_initial_spawn_delay() const noexcept { return initial_spawn_delay; }
     auto get_spawn_cooldown() const noexcept { return spawn_cooldown; }
 
+    void set_team(ETestTeam const new_team) noexcept { team = new_team; }
     void set_health(TOptional<int32> const new_health) noexcept { health = new_health; }
 
     void set_test_name(FName const new_test_name) noexcept { test_name = new_test_name; }
