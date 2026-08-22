e20a51008ab8a512da4db72705f5711868d93ec6
Matthew Love <matthewlove94@gmail.com>
2026-08-22 11:48:14 +0100

    Add benchmark support

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -1,9 +1,15 @@
 function(add_low_level_test_suite target_name unreal_target test_name_prefix test_label)
   add_unreal_target(${target_name} ${unreal_target})
 
-  set(test_executable
-    "${CMAKE_CURRENT_SOURCE_DIR}/Binaries/${UE_PLATFORM}/${unreal_target}/${unreal_target}-${UE_PLATFORM}-${UE_CONFIGURATION}.exe"
-  )
+  if(UE_CONFIGURATION STREQUAL "Development")
+    set(test_executable
+      "${CMAKE_CURRENT_SOURCE_DIR}/Binaries/${UE_PLATFORM}/${unreal_target}/${unreal_target}.exe"
+    )
+  else()
+    set(test_executable
+      "${CMAKE_CURRENT_SOURCE_DIR}/Binaries/${UE_PLATFORM}/${unreal_target}/${unreal_target}-${UE_PLATFORM}-${UE_CONFIGURATION}.exe"
+    )
+  endif()
 
   discover_tests(
     COMMAND "${test_executable}"
