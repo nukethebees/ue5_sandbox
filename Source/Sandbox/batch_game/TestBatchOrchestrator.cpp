754139a369be1e61e78238cea1c113a1c7cb2c3b
Matthew Love <matthewlove94@gmail.com>
2026-08-23 17:01:14 +0100

    Reset ticks in reset_for_new_level

--- Source/Sandbox/batch_game/TestBatchOrchestrator.cpp
+++ Source/Sandbox/batch_game/TestBatchOrchestrator.cpp
@@ -267,6 +267,7 @@ void ATestBatchOrchestrator::reset_for_new_level() {
     }
 
     mission_manager.reset_runtime_state();
+    completed_ticks = 0;
     state = EOrchestratorState::Uninitialised;
     if (should_initialise_in_begin_play()) {
         begin_play();
