1bb20c1d6f1389a902c302aa66fa2b896d2c421a
Matthew Love <matthewlove94@gmail.com>
2026-08-30 18:33:59 +0100

    Add world_aabbs_

--- Plugins/SpaceGame/Source/SpaceGame/Public/SpaceGame/simulation/CollisionSystem.h
+++ Plugins/SpaceGame/Source/SpaceGame/Public/SpaceGame/simulation/CollisionSystem.h
@@ -26,6 +26,7 @@ struct SPACEGAME_API FCollisionSystem {
     auto get_cell_dims() const noexcept -> FVector3f { return cell_dims_; }
     void set_cell_dims(FVector3f const cell_dims) noexcept { cell_dims_ = cell_dims; }
   private:
+    WorldAABBs world_aabbs_{};
     FEntityAABBs entity_aabbs_{};
     FIntVector3 grid_dims_{FIntVector3::ZeroValue};
     FVector3f cell_dims_{FVector3f::ZeroVector};
