0ba048f0c4a72db2f97d72e9b1dbd78273cad850
Matthew Love <matthewlove94@gmail.com>
2026-08-30 14:38:13 +0100

    Add SGCollision module

---
+++ Plugins/SpaceGame/Source/SGCollision/SGCollision.Build.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+
+public class SGCollision : ModuleRules
+{
+    public SGCollision(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        CppStandard = CppStandardVersion.Latest;
+        MinCpuArchX64 = MinimumCpuArchitectureX64.AVX2;
+
+        PublicDependencyModuleNames.Add("Core");
+    }
+}
