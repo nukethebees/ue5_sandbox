1b077195379a639a1d56e5c0dadc612dc847a791
Matthew Love <matthewlove94@gmail.com>
2026-09-04 13:21:53 +0100

    Make player flight mode explicit in level test

--- Plugins/SpaceGame/Source/SpaceGame/Public/SpaceGame/ships/player/TestSpaceShip.h
+++ Plugins/SpaceGame/Source/SpaceGame/Public/SpaceGame/ships/player/TestSpaceShip.h
@@ -122,6 +122,7 @@ class SPACEGAME_API ATestSpaceShip
     auto get_move_input() const { return planar_movement_direction; }
     auto get_control_mode() const { return control_mode; }
     auto get_flight_mode() const { return flight_mode; }
+    void set_flight_mode(ETestSpaceShipFlightMode new_flight_mode) noexcept;
     auto get_target_local_planar_velocity_scale() const {
         return target_local_planar_velocity_scale;
     }
