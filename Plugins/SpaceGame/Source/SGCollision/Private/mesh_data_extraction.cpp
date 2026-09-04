56a72dee7b68bbc0eed54e7dd62fe13cec7e054f
Matthew Love <matthewlove94@gmail.com>
2026-09-04 12:23:05 +0100

    Add get_aabb overload

--- Plugins/SpaceGame/Source/SGCollision/Private/mesh_data_extraction.cpp
+++ Plugins/SpaceGame/Source/SGCollision/Private/mesh_data_extraction.cpp
@@ -1,10 +1,62 @@
 #include "SGCollision/mesh_data_extraction.h"
 
+#include <Engine/EngineTypes.h>
 #include <Engine/StaticMesh.h>
 #include <Math/Transform.h>
+#include <PhysicsEngine/AggregateGeom.h>
 #include <PhysicsEngine/BodySetup.h>
 
 namespace ml {
+auto get_aabb(FKAggregateGeom const& geometry, FTransform const& local_to_world) -> FBox {
+    auto const scale{local_to_world.GetScale3D()};
+    auto world_transform{local_to_world};
+    world_transform.RemoveScaling();
+
+    FBox aabb{ForceInit};
+
+    for (auto const& collision_sphere : geometry.SphereElems) {
+        if (!CollisionEnabledHasQuery(collision_sphere.GetCollisionEnabled())) {
+            continue;
+        }
+
+        auto const scaled_sphere{collision_sphere.GetFinalScaled(scale, FTransform::Identity)};
+        auto const centre{world_transform.TransformPosition(scaled_sphere.Center)};
+        auto const extent{FVector{scaled_sphere.Radius}};
+        aabb += FBox{centre - extent, centre + extent};
+    }
+
+    for (auto const& collision_box : geometry.BoxElems) {
+        if (!CollisionEnabledHasQuery(collision_box.GetCollisionEnabled())) {
+            continue;
+        }
+
+        auto scaled_box{collision_box.GetFinalScaled(scale, FTransform::Identity)};
+        auto box_transform{scaled_box.GetTransform()};
+        box_transform.SetScale3D(FVector::OneVector);
+        scaled_box.SetTransform(box_transform);
+        aabb += scaled_box.CalcAABB(world_transform, 1.f);
+    }
+
+    for (auto const& collision_capsule : geometry.SphylElems) {
+        if (!CollisionEnabledHasQuery(collision_capsule.GetCollisionEnabled())) {
+            continue;
+        }
+
+        auto const scaled_capsule{collision_capsule.GetFinalScaled(scale, FTransform::Identity)};
+        aabb += scaled_capsule.CalcAABB(world_transform, 1.f);
+    }
+
+    for (auto const& collision_convex : geometry.ConvexElems) {
+        if (!CollisionEnabledHasQuery(collision_convex.GetCollisionEnabled())) {
+            continue;
+        }
+
+        aabb += collision_convex.CalcAABB(world_transform, scale);
+    }
+
+    return aabb;
+}
+
 auto get_aabb(UStaticMesh const& mesh) -> FBox {
     auto const* body_setup{mesh.GetBodySetup()};
     if (body_setup == nullptr) {
@@ -15,20 +67,6 @@ auto get_aabb(UStaticMesh const& mesh) -> FBox {
         return FBox{ForceInit};
     }
 
-    FBox aabb{ForceInit};
-
-    for (auto const& collision_box : body_setup->AggGeom.BoxElems) {
-        aabb += collision_box.CalcAABB(FTransform::Identity, 1.0f);
-    }
-
-    for (auto const& collision_sphere : body_setup->AggGeom.SphereElems) {
-        aabb += collision_sphere.CalcAABB(FTransform::Identity, 1.0f);
-    }
-
-    for (auto const& collision_capsule : body_setup->AggGeom.SphylElems) {
-        aabb += collision_capsule.CalcAABB(FTransform::Identity, 1.0f);
-    }
-
-    return aabb;
+    return get_aabb(body_setup->AggGeom, FTransform::Identity);
 }
 }
--- Plugins/SpaceGame/Source/SGCollision/Public/SGCollision/mesh_data_extraction.h
+++ Plugins/SpaceGame/Source/SGCollision/Public/SGCollision/mesh_data_extraction.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <Math/Box.h>
+#include <Math/Transform.h>
 
 class UStaticMesh;
+struct FKAggregateGeom;
 
 namespace ml {
+auto SGCOLLISION_API get_aabb(FKAggregateGeom const& geometry, FTransform const& local_to_world)
+    -> FBox;
 auto SGCOLLISION_API get_aabb(UStaticMesh const& mesh) -> FBox;
 }
