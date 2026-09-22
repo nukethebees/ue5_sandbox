658bb8e8a866c42b4ed0c0c44d40b834571e329e
Matthew Love <matthewlove94@gmail.com>
2026-09-22 10:10:39 +0100

    Import orchestrator mission into S7 level editor

--- Source/SandboxEditor/levels/S7LevelAuthoringMode.h
+++ Source/SandboxEditor/levels/S7LevelAuthoringMode.h
@@ -59,6 +59,7 @@ class SANDBOXEDITOR_API US7LevelAuthoringMode final : public UBaseLegacyWidgetEd
     void assign_selected_must_survive();
     void assign_selected_required_kills();
     void clear_selected_objectives();
+    void import_orchestrator_mission();
     void load_s7();
     void reload_s7();
     void set_source_buffer(FString source);
