88c8dfdbd6c70986b4def352de6b1f9d2378d3d7
Matthew Love <matthewlove94@gmail.com>
2026-09-24 14:31:16 +0100

    Make projectile validation buffer parameter read-only

--- native/core/src/projectile_intercept.cpp
+++ native/core/src/projectile_intercept.cpp
@@ -18,7 +18,7 @@ auto at(ml::Vector3fSoAConstView const values, std::int32_t const index) noexcep
     return values[index];
 }
 
-auto validate_batch([[maybe_unused]] float* const out_intercept_times,
+auto validate_batch([[maybe_unused]] float const* const out_intercept_times,
                     ml::Vector3fSoAConstView const shooter_positions,
                     [[maybe_unused]] ml::Vector3fSoAConstView const target_positions,
                     [[maybe_unused]] ml::Vector3fSoAConstView const target_velocities) noexcept
